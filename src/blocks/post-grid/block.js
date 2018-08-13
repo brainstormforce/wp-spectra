5e9795bdd958dc82479182d684051f3d7c1bd144
Vrunda Kansara <vrundak@bsf.io>
2018-08-13 18:48:55 +0530

    post grid query changes

--- src/blocks/post-grid/block.js
+++ src/blocks/post-grid/block.js
@@ -2,382 +2,57 @@
  * BLOCK: post-grid
  */
 
-// Import block dependencies and components.
+/**
+ * BLOCK: Atomic Blocks Page Grid
+ */
+
+// Import block dependencies and components
 import classnames from 'classnames';
+import edit from './edit';
 
 //  Import CSS.
 import './style.scss'
 import './editor.scss';
 
-// Import __() from wp.i18n
-const { __ } = wp.i18n;
-
-// Import registerBlockType() from wp.blocks
-const {
-	registerBlockType,
-} = wp.blocks;
-
-const {
-	AlignmentToolbar,
-	BlockControls,
-	ColorPalette,
-	InspectorControls,
-	RichText,
-} = wp.editor
-
-const {
-    PanelBody,
-    PanelColor,
-    SelectControl,
-    RangeControl,
-} = wp.components;
+// Components
+const { __ } = wp.i18n; 
 
 // Extend component
 const { Component } = wp.element;
 
-class UAGBPostGrid extends Component {
-	render() {
+// Register block controls
+const { 
+	registerBlockType,
+} = wp.blocks;
 
-		// Setup the attributes
-		const {
-			isSelected,
-			className,
-			setAttributes,
-			attributes: {
-				headingTitle,
-				headingDesc,
-				headingAlign,
-				headingColor,
-				subHeadingColor,
-				separatorColor,
-				headingTag,
-				headFontSize,
-				subHeadFontSize,
-				separatorWidth,
-				separatorHeight,
-				headSpace,
-				separatorSpace,
-				subHeadSpace,
-			},
-		} = this.props;
+// Register alignments
+const validAlignments = [ 'center', 'wide' ];
 
-		return [
+export const name = 'core/latest-posts';
 
-			isSelected && (
-				<BlockControls key='controls'>
-					<AlignmentToolbar
-						value={ headingAlign }
-						onChange={ ( value ) => setAttributes( { headingAlign: value } ) }
-					/>
-				</BlockControls>
-			),
-
-			isSelected && (
-				<InspectorControls>
-				<PanelBody 
-                	title={ __( 'Typography' ) }
-                	initialOpen={ false }
-                >
-                	<SelectControl
-                        label={ __( 'Tag' ) }
-                        value={ headingTag }
-                        onChange={ ( value ) => setAttributes( { headingTag: value } ) }
-                        options={ [
-                            { value: 'h1', label: __( 'H1' ) },
-                            { value: 'h2', label: __( 'H2' ) },
-                            { value: 'h3', label: __( 'H3' ) },
-                            { value: 'h4', label: __( 'H4' ) },
-                            { value: 'h5', label: __( 'H5' ) },
-                            { value: 'h6', label: __( 'H6' ) },
-                        ] }
-                    />
-					<RangeControl
-                        label={ __( 'Heading Font Size' ) }
-                        value={ headFontSize }
-                        onChange={ ( value ) => setAttributes( { headFontSize: value } ) }
-                        min={ 10 }
-                        max={ 200 }
-                        beforeIcon="editor-textcolor"
-                        allowReset
-                    />
-                    <RangeControl
-                        label={ __( 'Sub-Heading Font Size' ) }
-                        value={ subHeadFontSize }
-                        onChange={ ( value ) => setAttributes( { subHeadFontSize: value } ) }
-                        min={ 10 }
-                        max={ 200 }
-                        beforeIcon="editor-textcolor"
-                        allowReset
-                    />
-				</PanelBody>
-                <PanelBody 
-                	title={ __( 'Colors' ) }
-                	initialOpen={ false }
-                >
-                    <PanelColor
-                        title={ __( 'Heading Color' ) }
-                        colorValue={ headingColor }
-                        initialOpen={ false }
-                    >
-                        <ColorPalette
-                            value={ headingColor }
-                            onChange={ ( colorValue ) => setAttributes( { headingColor: colorValue } ) }
-                            allowReset
-                        />
-                    </PanelColor>
-                    <PanelColor
-                        title={ __( 'Sub-Heading Color' ) }
-                        colorValue={ subHeadingColor }
-                        initialOpen={ false }
-                    >
-                        <ColorPalette
-                            value={ subHeadingColor }
-                            onChange={ ( colorValue ) => setAttributes( { subHeadingColor: colorValue } ) }
-                            allowReset
-                        />
-                    </PanelColor>
-                    <PanelColor
-                        title={ __( 'Separator Color' ) }
-                        colorValue={ separatorColor }
-                        initialOpen={ false }
-                    >
-                        <ColorPalette
-                            value={ separatorColor }
-                            onChange={ ( colorValue ) => setAttributes( { separatorColor: colorValue } ) }
-                            allowReset
-                        />
-                    </PanelColor>
-				</PanelBody>
-				<PanelBody 
-                	title={ __( 'Additional Options' ) }
-                	initialOpen={ false }
-                >
-                	<RangeControl
-                        label={ __( 'Separator Height' ) }
-                        value={ separatorHeight }
-                        onChange={ ( value ) => setAttributes( { separatorHeight: value } ) }
-                        min={ 0 }
-                        max={ 50 }
-                        beforeIcon="editor-textcolor"
-                        allowReset
-                    />
-                	<RangeControl
-                        label={ __( 'Separator Width' ) }
-                        value={ separatorWidth }
-                        onChange={ ( value ) => setAttributes( { separatorWidth: value } ) }
-                        min={ 0 }
-                        max={ 100 }
-                        beforeIcon="editor-textcolor"
-                        allowReset
-                    />
-					<RangeControl
-                        label={ __( 'Heading Spacing' ) }
-                        value={ headSpace }
-                        onChange={ ( value ) => setAttributes( { headSpace: value } ) }
-                        min={ 0 }
-                        max={ 50 }
-                        beforeIcon="editor-textcolor"
-                        allowReset
-                    />
-                    <RangeControl
-                        label={ __( 'Separator Spacing' ) }
-                        value={ separatorSpace }
-                        onChange={ ( value ) => setAttributes( { separatorSpace: value } ) }
-                        min={ 0 }
-                        max={ 50 }
-                        beforeIcon="editor-textcolor"
-                        allowReset
-                    />
-                    <RangeControl
-                        label={ __( 'Sub-Heading Spacing' ) }
-                        value={ subHeadSpace }
-                        onChange={ ( value ) => setAttributes( { subHeadSpace: value } ) }
-                        min={ 0 }
-                        max={ 50 }
-                        beforeIcon="editor-textcolor"
-                        allowReset
-                    />
-				</PanelBody>
-                </InspectorControls>
-            ),
-
-			<div className={ className }>
-				<RichText
-					tagName={ headingTag }
-					placeholder={ __( 'Write a Heading' ) }
-					value={ headingTitle }
-					className='uagb-heading-text'
-					onChange={ ( value ) => setAttributes( { headingTitle: value } ) }
-					style={{ 
-						textAlign: headingAlign,
-						fontSize: headFontSize + 'px',
-						color: headingColor,
-						marginBottom: headSpace + 'px',
-					}}
-				/>
-				<div
-					className="uagb-separator-wrap"
-					style={{ textAlign: headingAlign }}
-				><div className="uagb-separator" style={{ borderTopWidth: separatorHeight + 'px', width: separatorWidth + '%', borderColor: separatorColor, marginBottom: separatorSpace + 'px', }}></div></div>
-				<RichText
-					tagName="p"
-					placeholder={ __( 'Write a Description' ) }
-					value={ headingDesc }
-					className='uagb-desc-text'
-					onChange={ ( value ) => setAttributes( { headingDesc: value } ) }
-					style={{
-						textAlign: headingAlign,
-						fontSize: subHeadFontSize + 'px',
-						color: subHeadingColor,
-						marginBottom: subHeadSpace + 'px',
-					}}
-				/>
-			</div>
-		];
-	}
-}
-
-/**
- * Register: as Gutenberg Block.
- *
- * Registers a new block provided a unique name and an object defining its
- * behavior.
- *
- * @link https://wordpress.org/gutenberg/handbook/block-api/
- * @param  {string}   name     Block name.
- * @param  {Object}   settings Block settings.
- * @return {?WPBlock}          The block, if it has been successfully
- *                             registered; otherwise `undefined`.
- */
+// Register the block
 registerBlockType( 'uagb/post-grid', {
-
-	// Block name. Block names must be string that contains a namespace prefix. Example: my-plugin/my-custom-block.
-	title: __( 'Post Grid - UAGB' ), // Block title.
-	description: __( 'Add Post Grid block.' ), // Block description.
-	icon: 'editor-table', // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
-	category: 'common', // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
+	title: __( 'Vrunda Post Grid' ),
+	description: __( 'Add a grid or list of customizable posts to your page.' ),
+	icon: 'grid-view',
+	category: 'atomic-blocks',
 	keywords: [
-		__( 'post grid' ),
-		__( 'uagb' ),
+		__( 'post' ),
+		__( 'grid' ),
+		__( 'atomic' ),
 	],
-	category: 'formatting',
 
-	attributes: {
-		headingTitle: {
-			type: 'string',
-		},
-		headingDesc: {
-			type: 'string',
-		},
-		headingAlign: {
-			type: 'string',
-			default: 'center',
-		},
-		headingColor: {
-            type: 'string',
-        },
-        subHeadingColor: {
-            type: 'string',
-        },
-        separatorColor: {
-        	type: 'string',
-        },
-        headingTag: {
-        	type: 'string',
-        	default: 'h1'
-        },
-        separatorHeight: {
-        	type: 'number'
-        },
-        separatorWidth: {
-        	type: 'number'
-        },
-        headFontSize: {
-            type: 'number',
-        },
-        subHeadFontSize: {
-            type: 'number',
-        },
-        headSpace: {
-            type: 'number',
-        },
-		separatorSpace: {
-            type: 'number',
-        },
-		subHeadSpace: {
-            type: 'number',
-        },
+	getEditWrapperProps( attributes ) {
+		const { align } = attributes;
+		if ( -1 !== validAlignments.indexOf( align ) ) {
+			return { 'data-align': align };
+		}
 	},
-	/**
-	 * The edit function describes the structure of your block in the context of the editor.
-	 * This represents what the editor will render when the block is used.
-	 *
-	 * The "edit" property must be a valid function.
-	 *
-	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
-	 */
-	edit: UAGBPostGrid,
 
-	/*function( props ) {
+	edit,
 
-		console.log( 'Edit props' );
-		console.log( props );
-
-		const { headingTitle } = props.attributes;
-
-		return (
-			<div className={ props.className }>
-				<p>Ultimate Addons For Gutenberg!</p>
-			</div>
-		);
-	},*/
-
-	/**
-	 * The save function defines the way in which the different attributes should be combined
-	 * into the final markup, which is then serialized by Gutenberg into post_content.
-	 *
-	 * The "save" property must be specified and must be a valid function.
-	 *
-	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
-	 */
-	save: function( props ) {
-		
-		console.log( 'Save props' );
-		console.log( props );
-
-		const {
-			headingTitle,
-			headingDesc,
-			headingAlign,
-			headingColor,
-			subHeadingColor,
-			separatorColor,
-			headingTag,
-			separatorWidth,
-			separatorHeight,
-			headFontSize,
-			subHeadFontSize,
-			headSpace,
-			separatorSpace,
-			subHeadSpace,
-		} = props.attributes;
-
-		return (
-			<div className={ props.className }>
-				<RichText.Content
-					tagName={ headingTag }
-					value={ headingTitle }
-					className='uagb-heading-text'
-					style={{ 
-						textAlign: headingAlign,
-						fontSize: headFontSize + 'px',
-						color: headingColor,
-						marginBottom: headSpace + 'px',
-					}}
-				/>
-				<div className="uagb-separator-wrap" style={{ textAlign: headingAlign }}><div className="uagb-separator" style={{ borderTopWidth: separatorHeight + 'px', width: separatorWidth + '%', borderColor: separatorColor, marginBottom: separatorSpace + 'px', }}></div></div>
-				<p className="uagb-desc-text" style={{ textAlign: headingAlign, fontSize: subHeadFontSize + 'px', color: subHeadingColor, marginBottom: subHeadSpace + 'px', }}>{ headingDesc }</p>
-			</div>
-		);
-	}
+	// Render via PHP
+	save() {
+		return null;
+	},
 } );
