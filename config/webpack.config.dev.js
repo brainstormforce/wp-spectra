10c100286306cda73d3b3d11f4ae016c47a7d02b
Vrunda Kansara <vrundak@bsf.io>
2018-09-26 17:00:06 +0530

    default fixes

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -20,77 +20,77 @@
  * @since 1.0.0
  */
 
-const paths = require( './paths' );
-const autoprefixer = require( 'autoprefixer' );
-const ExtractTextPlugin = require( 'extract-text-webpack-plugin' );
+const paths = require( "./paths" )
+const autoprefixer = require( "autoprefixer" )
+const ExtractTextPlugin = require( "extract-text-webpack-plugin" )
 
 // Extract style.css for both editor and frontend styles.
 const blocksCSSPlugin = new ExtractTextPlugin( {
-	filename: './dist/blocks.style.build.css',
-} );
+	filename: "./dist/blocks.style.build.css",
+} )
 
 // Extract editor.css for editor styles.
 const editBlocksCSSPlugin = new ExtractTextPlugin( {
-	filename: './dist/blocks.editor.build.css',
-} );
+	filename: "./dist/blocks.editor.build.css",
+} )
 
 // Configuration for the ExtractTextPlugin — DRY rule.
 const extractConfig = {
 	use: [
 		// "postcss" loader applies autoprefixer to our CSS.
-		{ loader: 'raw-loader' },
+		{ loader: "raw-loader" },
 		{
-			loader: 'postcss-loader',
+			loader: "postcss-loader",
 			options: {
-				ident: 'postcss',
+				ident: "postcss",
 				plugins: [
 					autoprefixer( {
 						browsers: [
-							'>1%',
-							'last 4 versions',
-							'Firefox ESR',
-							'not ie < 9', // React doesn't support IE8 anyway
+							">1%",
+							"last 4 versions",
+							"Firefox ESR",
+							"not ie < 9", // React doesn't support IE8 anyway
 						],
-						flexbox: 'no-2009',
+						flexbox: "no-2009",
 					} ),
 				],
 			},
 		},
 		// "sass" loader converst SCSS to CSS.
 		{
-			loader: 'sass-loader',
+			loader: "sass-loader",
 			options: {
 				// Add common CSS file for variables and mixins.
-				data: '@import "./src/common.scss";\n',
-				outputStyle: 'nested',
+				data: "@import \"./src/common.scss\";\n",
+				outputStyle: "nested",
 			},
 		},
 	],
-};
+}
 
 // console.log( paths );
 
 // Export configuration.
 module.exports = {
 	entry: {
-		'./dist/blocks.build': paths.pluginBlocksJs, // 'name' : 'path/file.ext'.
+		"./dist/blocks.build": paths.pluginBlocksJs, // 'name' : 'path/file.ext'.
 	},
 	output: {
 		// Add * filename * comments to generated require()s in the output.
 		pathinfo: true,
 		// The dist folder.
 		path: paths.pluginDist,
-		filename: '[name].js', // [name] = './dist/blocks.build' as defined above.
+		filename: "[name].js", // [name] = './dist/blocks.build' as defined above.
 	},
 	// You may want 'eval' instead if you prefer to see the compiled output in DevTools.
-	devtool: 'cheap-eval-source-map',
+	devtool: "cheap-eval-source-map",
 	module: {
 		rules: [
 			{
 				test: /\.(js|jsx|mjs)$/,
 				exclude: /(node_modules|bower_components)/,
 				use: {
-					loader: 'babel-loader',
+					loader: "babel-loader",
 					options: {
 						
 						// This is a feature of `babel-loader` for webpack (not Babel itself).
@@ -114,6 +114,6 @@ module.exports = {
 	},
 	// Add plugins.
 	plugins: [ blocksCSSPlugin, editBlocksCSSPlugin ],
-	stats: 'minimal',
+	stats: "minimal",
 	// stats: 'errors-only',
-};
+}
--- scripts/build.js
+++ scripts/build.js
@@ -4,34 +4,34 @@
  * The create-guten-block CLI builds here.
  */
 
-'use strict';
+"use strict"
 
 // Do this as the first thing so that any code reading it knows the right env.
-process.env.BABEL_ENV = 'production';
-process.env.NODE_ENV = 'production';
+process.env.BABEL_ENV = "production"
+process.env.NODE_ENV = "production"
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
 // terminate the Node.js process with a non-zero exit code.
-process.on( 'unhandledRejection', err => {
-	throw err;
-} );
+process.on( "unhandledRejection", err => {
+	throw err
+} )
 
 // Modules.
-const fs = require( 'fs' );
-const ora = require( 'ora' );
-const path = require( 'path' );
-const chalk = require( 'chalk' );
-const webpack = require( 'webpack' );
-const fileSize = require( 'filesize' );
-const gzipSize = require( 'gzip-size' );
-const config = require( '../config/webpack.config.prod' );
+const fs = require( "fs" )
+const ora = require( "ora" )
+const path = require( "path" )
+const chalk = require( "chalk" )
+const webpack = require( "webpack" )
+const fileSize = require( "filesize" )
+const gzipSize = require( "gzip-size" )
+const config = require( "../config/webpack.config.prod" )
 
 // Build file paths.
-const theCWD = process.cwd();
-const fileBuildJS = path.resolve( theCWD, './dist/blocks.build.js' );
-const fileEditorCSS = path.resolve( theCWD, './dist/blocks.editor.build.css' );
-const fileStyleCSS = path.resolve( theCWD, './dist/blocks.style.build.css' );
+const theCWD = process.cwd()
+const fileBuildJS = path.resolve( theCWD, "./dist/blocks.build.js" )
+const fileEditorCSS = path.resolve( theCWD, "./dist/blocks.editor.build.css" )
+const fileStyleCSS = path.resolve( theCWD, "./dist/blocks.style.build.css" )
 
 /**
  * Get File Size
@@ -42,13 +42,13 @@ const fileStyleCSS = path.resolve( theCWD, './dist/blocks.style.build.css' );
  * @returns {string} then size result.
  */
 const getFileSize = filePath => {
-	return fileSize( gzipSize.sync( fs.readFileSync( filePath ) ) );
-};
+	return fileSize( gzipSize.sync( fs.readFileSync( filePath ) ) )
+}
 
 // clearConsole();
 
 // Init the spinner.
-const spinner = new ora( { text: '' } );
+const spinner = new ora( { text: "" } )
 
 /**
  * Build function
@@ -60,7 +60,7 @@ const spinner = new ora( { text: '' } );
 async function build( webpackConfig ) {
 
 	// Compiler Instance.
-	const compiler = await webpack( webpackConfig );
+	const compiler = await webpack( webpackConfig )
 
 	// Run the compiler.
 	compiler.run( ( err, stats ) => {
@@ -70,10 +70,10 @@ async function build( webpackConfig ) {
 		// 	return console.log( err );
 		// }
 
-		var stats_json = stats.toJson( {}, true );
+		var stats_json = stats.toJson( {}, true )
 
-		console.log( stats_json.errors );
-		console.log( stats_json.warnings );
+		console.log( stats_json.errors )
+		console.log( stats_json.warnings )
 
 		// Get the messages formatted.
 		/*const messages = formatWebpackMessages( stats.toJson( {}, true ) );
@@ -111,31 +111,31 @@ async function build( webpackConfig ) {
 		}*/
 
 		// Start the build.
-		console.log( `\n ${ chalk.dim( 'Let\'s build and compile the files...' ) }` );
-		console.log( '\n✅ ', chalk.black.bgGreen( ' Built successfully! \n' ) );
+		console.log( `\n ${ chalk.dim( "Let's build and compile the files..." ) }` )
+		console.log( "\n✅ ", chalk.black.bgGreen( " Built successfully! \n" ) )
 
 		console.log(
-			'\n\n',
-			'File sizes after gzip:',
-			'\n\n',
+			"\n\n",
+			"File sizes after gzip:",
+			"\n\n",
 			getFileSize( fileBuildJS ),
-			`${ chalk.dim( '— ./dist/' ) }`,
-			`${ chalk.green( 'blocks.build.js' ) }`,
-			'\n',
+			`${ chalk.dim( "— ./dist/" ) }`,
+			`${ chalk.green( "blocks.build.js" ) }`,
+			"\n",
 			getFileSize( fileEditorCSS ),
-			`${ chalk.dim( '— ./dist/' ) }`,
-			`${ chalk.green( 'blocks.editor.build.css' ) }`,
+			`${ chalk.dim( "— ./dist/" ) }`,
+			`${ chalk.green( "blocks.editor.build.css" ) }`,
 
-			'\n',
+			"\n",
 			getFileSize( fileStyleCSS ),
-			`${ chalk.dim( '— ./dist/' ) }`,
-			`${ chalk.green( 'blocks.style.build.css' ) }`,
+			`${ chalk.dim( "— ./dist/" ) }`,
+			`${ chalk.green( "blocks.style.build.css" ) }`,
 
-			'\n\n'
-		);
+			"\n\n"
+		)
 
-		return true;
-	} );
+		return true
+	} )
 }
 
-build( config );
+build( config )
