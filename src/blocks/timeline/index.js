7f3fc4524ab9c32b23d728e1df3d91fe382a48e3
prajakta <prajaktag@bsf.io>
2018-10-03 19:26:33 +0530

    Fixed:Comment issue

--- src/blocks/timeline/index.js
+++ src/blocks/timeline/index.js
@@ -1,19 +1,19 @@
 /**
- * BLOCK: Atomic Blocks Page Grid
+ * BLOCK: Timeline Js.
  */
 
-// Import block dependencies and components
+// Import block dependencies and components.
 import classnames from 'classnames';
 import edit from './block';
 
-// Import CSS
+// Import CSS.
 import './style.scss';
 import './editor.scss';
 
-// Components
+// Components.
 const { __ } = wp.i18n; 
 
-// Extend component
+// Extend component.
 const { Component } = wp.element;
 
 const {
@@ -34,7 +34,7 @@ const iconEl = el('svg', { width: 20, height: 20 },
   el('path', { d: "M1.661 5.463v3.706h13.448l1.16-1.159c0.365-0.365 0.962-0.365 1.327 0l1.16 1.159h1.245v1.661h-1.246l-1.158 1.158c-0.365 0.365-0.962 0.365-1.327 0l-1.159-1.158h-13.449v3.706h1.18l1.16-1.16c0.365-0.365 0.962-0.365 1.327 0l1.16 1.16h13.512v1.661h-13.514l-1.159 1.158c-0.365 0.365-0.961 0.365-1.326 0l-1.159-1.158h-1.181v3.77h-1.661v-19.936h1.661v3.77h5.78l1.16-1.16c0.365-0.365 0.962-0.365 1.327 0l1.16 1.16h8.912v1.661h-8.913l-1.159 1.158c-0.365 0.365-0.961 0.365-1.326 0l-1.16-1.158h-5.781z" } )
 );
 
-// Register block controls
+// Register block controls.
 const { 
     registerBlockType,
 } = wp.blocks;
@@ -46,10 +46,10 @@ const {
 
 export const name = 'core/latest-posts';
 
-// Register alignments
+// Register alignments.
 const validAlignments = [ 'center', 'left', 'right' ];
 
-// Register the block
+// Register the block.
 registerBlockType( 'uagb/timeline', {
     title: __( 'Timeline - UAGB' ),
     description: __( 'Add a timeline to your page.' ),
