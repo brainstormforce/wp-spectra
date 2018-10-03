adfcafd65982cce08175516169aa369b6b590224
prajakta <prajaktag@bsf.io>
2018-10-03 18:51:47 +0530

    Fixed:advanced heading issue

--- src/blocks.js
+++ src/blocks.js
@@ -13,4 +13,3 @@ import './blocks/advanced-heading/block.js';
 import './blocks/timeline/index.js';
 import './blocks/post/block.js';
 import './blocks/content-timeline/block.js';
-import "./blocks/advanced-heading/block.js";
