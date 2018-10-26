b6c63bc9dec6057b8d520843b9b6911be3bc3363
Vrunda Kansara <vrundak@bsf.io>
2018-10-26 20:46:26 +0530

    unwanted code removed

--- classes/class-uagb-init-blocks.php
+++ classes/class-uagb-init-blocks.php
@@ -83,63 +83,12 @@ class UAGB_Init_Blocks {
 			UAGB_VER
 		);
 
-		// Scripts.
-		wp_enqueue_script(
-			'uagb-masonry', // Handle.
-			UAGB_URL . 'assets/lib/isotope.min.js',
-			array( 'jquery' ), // Dependencies, defined above.
-			UAGB_VER,
-			false // Enqueue the script in the footer.
-		);
-
-		// Scripts.
-		wp_enqueue_script(
-			'uagb-slick-js', // Handle.
-			UAGB_URL . 'assets/lib/slick.min.js',
-			array( 'jquery' ), // Dependencies, defined above.
-			UAGB_VER,
-			false // Enqueue the script in the footer.
-		);
-
-		// Styles.
-		wp_enqueue_style(
-			'uagb-slick-css', // Handle.
-			UAGB_URL . 'assets/css/slick.css', // Block style CSS.
-			array( 'wp-blocks' ), // Dependency to include the CSS after it.
-			UAGB_VER
-		);
-
 		// Font Awsome.
 		wp_enqueue_style(
 			'uagb-fontawesome-css', // Handle.
 			'https://use.fontawesome.com/releases/v5.0.9/css/all.css', // Block style CSS.
 			array( 'wp-blocks' ), // Dependency to include the CSS after it.
 			UAGB_VER
-		);
-
-		// Google Map Script.
-		wp_enqueue_script(
-			'uagb-google-map', // Handle.
-			'https://maps.googleapis.com/maps/api/js?key=AIzaSyDBEoty6odpRzDD7dcvxdCKz6KOilnTiec',
-			array( 'jquery' ), // Dependencies, defined above.
-			UAGB_VER,
-			false // Enqueue the script in the footer.
-		);
-
-		wp_enqueue_script(
-			'uagb-google-map-cluster', // Handle.
-			'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js',
-			array( 'jquery' ), // Dependencies, defined above.
-			UAGB_VER,
-			false // Enqueue the script in the footer.
-		);
-
-		wp_enqueue_script(
-			'uagb-google-map-handler', // Handle.
-			UAGB_URL . 'assets/js/google-map.js',
-			array( 'jquery', 'uagb-google-map', 'uagb-google-map-cluster' ), // Dependencies, defined above.
-			UAGB_VER,
-			false // Enqueue the script in the footer.
 		);
 
 	} // End function editor_assets().
