a87b980d409840d0d95e469732ae33f766b4fc5a
prajakta <prajaktag@bsf.io>
2018-11-06 14:28:45 +0530

    WIP on menu

---
+++ src/blocks/rest-menu/classes.js
@@ -0,0 +1,30 @@
+/**
+ * Set inline CSS class.
+ * @param {object} props - The block object.
+ * @return {array} The inline CSS class.
+ */
+
+function PositionClasses( attributes ) {    
+
+    var iconimgStyle_class = '';
+    iconimgStyle_class += " uagb-tm__imgicon-style-"+attributes.iconimgStyle+ " ";   
+    iconimgStyle_class += "uagb-tm__image-position-"+attributes.imagePosition+ " ";
+
+    if( attributes.imagePosition == 'left' || attributes.imagePosition == 'right'){
+    	iconimgStyle_class += "uagb-tm__image-aligned-"+attributes.imageAlignment+ " ";
+        if( attributes.stack !== "none" ){
+            iconimgStyle_class +="uagb-tm-stacked-"+attributes.stack+ " "
+            if( attributes.imagePosition === "right" ){
+                iconimgStyle_class += "uagb-tm-reverse-order-"+attributes.stack+ " "
+            }
+        }
+    }
+
+    iconimgStyle_class += "uagb-tm__bg-type-"+attributes.backgroundType+ " ";
+
+    return [
+        iconimgStyle_class        
+    ]
+}
+
+export default PositionClasses
---
+++ src/blocks/rest-menu/inline-styles.js
@@ -0,0 +1,139 @@
+/**
+ * Set inline styles.
+ * @param  {object} props - The block object.
+ * @return {object} The inline background type CSS.
+ */
+function TestimonialStyle( props ) {
+    const {
+            headingAlign,
+            companyColor,
+            descColor,
+            authorColor,
+            nameFontSize,
+            companyFontSize,
+            descFontSize,
+            descSpace,
+            block_id,
+            nameSpace,
+            imgVrPadding,
+            imgHrPadding,
+            imageWidth,  
+            rowGap,
+            columnGap,
+            contentPadding,
+            backgroundColor,
+            backgroundImage,
+            backgroundPosition,
+            backgroundSize,
+            backgroundRepeat,
+            backgroundAttachment,
+            backgroundImageColor,
+            backgroundOpacity,
+            borderStyle,
+            borderWidth ,
+            borderRadius,
+            borderColor         
+        } = props.attributes;        
+
+        if( props.clientId ){
+            var clientId = 'uagb-testimonial-'+props.clientId;
+        }else{
+            var clientId = 'uagb-testimonial-'+block_id;
+        }
+
+        var selectors = {};
+
+            selectors['.uagb-testomonial__wrap'] = {
+                    'padding-left' : rowGap/2+'px',
+                    'padding-right' : rowGap/2+'px',                                
+                    'margin-bottom' : columnGap+'px',
+                }; 
+            
+            selectors['.uagb-testomonial__wrap .uagb-tm__image-content'] = {
+                    'padding-left' : imgHrPadding+'px',
+                    'padding-right' : imgHrPadding+'px',                                
+                    'padding-top' : imgVrPadding+'px',
+                    'padding-bottom' : imgVrPadding+'px',
+                }; 
+
+            // Image
+            selectors['.uagb-tm__image img'] = {
+                    'width': imageWidth+'px',
+                    'max-width': imageWidth+'px',
+                }; 
+            
+            selectors['.uagb-tm__content'] = {                    
+                    'text-align' : headingAlign, 
+                    'padding' : contentPadding+'px',                  
+                };                       
+
+            // Prefix Style
+            selectors['.uagb-tm__author-name'] = {
+                    'font-size' : nameFontSize+'px',
+                    'color': authorColor,
+                    'margin-bottom': nameSpace+'px',
+                };
+
+            // Title Style
+            selectors['.uagb-tm__company'] = {
+                    'font-size' : companyFontSize+'px',
+                    'color': companyColor,
+                };
+
+            // Description Style
+            selectors['.uagb-tm__desc'] = {
+                    'font-size' : descFontSize+'px',
+                    'color': descColor,
+                    'margin-bottom': descSpace+'px',
+                };     
+
+            selectors['.uagb-testomonial__wrap.uagb-tm__bg-type-color .uagb-tm__content'] = {
+                    'background-color': backgroundColor,
+                };  
+
+            var position = backgroundPosition.replace( "-", " " )
+            selectors['.uagb-testomonial__wrap.uagb-tm__bg-type-image .uagb-tm__content'] = {
+                    'background-image': ( backgroundImage ) ? `url(${ backgroundImage.url })` : null,
+                    'background-position':position,
+                    'background-attachment':backgroundAttachment,
+                    'background-repeat':backgroundRepeat,
+                    'background-size':backgroundSize,
+                }; 
+
+            selectors['.uagb-testomonial__wrap.uagb-tm__bg-type-image .uagb-tm__overlay'] = {
+                    'background-color':backgroundImageColor,
+                    'opacity':'0.'+backgroundOpacity,
+                };                              
+               
+            if ( borderStyle != "none" ) {
+                selectors['.uagb-testomonial__wrap .uagb-tm__content'] = {
+                    'border-color': borderColor,
+                    'border-style':borderStyle,
+                    'border-width':borderWidth + "px",
+                    'border-radius':borderRadius + "px",                    
+                };                 
+            }
+
+        var styling_css = '';
+
+        for( var i in selectors ) {
+           
+            styling_css += '#wpwrap #'+clientId+' '+i + ' { ';
+            
+            
+            var sel = selectors[i];
+            var css = '';
+
+            for( var j in sel ) {
+
+                css += j + ': ' + sel[j] + ';';
+            }
+
+            styling_css += css + ' } ';
+        }       
+
+        return styling_css;
+
+}
+
+export default TestimonialStyle;
