612d39e54454292ee1e93b4428b661f1eb97d7d9
prajakta <prajaktag@bsf.io>
2018-11-14 11:51:51 +0530

    Fixed: Image issue

--- src/blocks/info-box/edit.js
+++ src/blocks/info-box/edit.js
@@ -81,6 +81,12 @@ class UAGBinfoBox extends Component {
 			setAttributes( { iconImage: null } )
 			return
 		}
+
+		if ( ! media.type || 'image' !== media.type ) {
+			setAttributes( { iconImage: null } )
+			return
+		}
+
 		setAttributes( { iconImage: media } )
 	}
 
@@ -668,7 +674,7 @@ class UAGBinfoBox extends Component {
 					<MediaUpload
 						title={ __( "Select Image" ) }
 						onSelect={ this.onSelectImage }
-						allowedTypes= "image"
+						allowedTypes= { [ "image" ] }
 						type="image"
 						value={ iconImage }
 						render={ ( { open } ) => (
