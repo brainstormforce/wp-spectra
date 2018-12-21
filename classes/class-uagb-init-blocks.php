b06dd885ffb78d75288fa89af5d04baf95df795d
prajakta <prajaktag@bsf.io>
2018-12-21 15:12:14 +0530

    Removed testimonial.js file

--- classes/class-uagb-init-blocks.php
+++ classes/class-uagb-init-blocks.php
@@ -113,17 +113,7 @@ class UAGB_Init_Blocks {
 			UAGB_URL . 'assets/css/slick.css', // Block style CSS.
 			UAGB_VER
 		);
-
-		// Testimonial Scripts.
-		/*
-		wp_enqueue_script(
-			'uabg-testimonial-js', // Handle.
-			UAGB_URL . 'assets/js/testimonial.js',
-			array( 'jquery' ),
-			UAGB_VER,
-			true // Enqueue the script in the footer.
-		);*/
-
+		
 		// Timeline js.
 		wp_enqueue_script(
 			'uabg-timeline-js', // Handle.
