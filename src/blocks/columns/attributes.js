faf70404f09f3a204a0198542fab06acfd9bb0eb
Vrunda Kansara <vrundak@bsf.io>
2018-12-26 19:21:17 +0530

    wip

---
+++ src/blocks/columns/attributes.js
@@ -0,0 +1,144 @@
+/**
+ * BLOCK: UAGB Section Attributes
+ */
+const attributes = {
+	align: {
+		type: "string",
+		default: "center"
+	},
+	block_id: {
+		type: "string",
+	},
+	topPadding: {
+		type: "number",
+		default: 20
+	},
+	bottomPadding: {
+		type: "number",
+		default: 20
+	},
+	leftPadding: {
+		type: "number",
+		default: 20
+	},
+	rightPadding: {
+		type: "number",
+		default: 20
+	},
+	topMargin: {
+		type: "number",
+		default: 0
+	},
+	bottomMargin: {
+		type: "number",
+		default: 0
+	},
+	leftMargin: {
+		type: "number",
+		default: 0
+	},
+	rightMargin: {
+		type: "number",
+		default: 0
+	},
+	contentWidth: {
+		type: "string",
+		default: "boxed"
+	},
+	width: {
+		type: "number",
+		default: 900
+	},
+	innerWidth: {
+		type: "number",
+		default: 1140
+	},
+	themeWidth: {
+		type: "boolean",
+		default: false
+	},
+	tag: {
+		type: "string",
+		default: "section"
+	},
+	backgroundType: {
+		type: "string",
+	},
+	backgroundImage: {
+		type: "object",
+	},
+	backgroundPosition: {
+		type: "string",
+		default: "center-center"
+	},
+	backgroundSize: {
+		type: "string",
+		default: "cover"
+	},
+	backgroundRepeat: {
+		type: "string",
+		default: "no-repeat"
+	},
+	backgroundAttachment: {
+		type: "string",
+		default: "scroll"
+	},
+	backgroundVideo: {
+		type: "object",
+	},
+	backgroundColor: {
+		type: "string",
+	},
+	gradientColor1: {
+		type: "string",
+	},
+	gradientColor2: {
+		type: "string",
+	},
+	gradientType: {
+		type: "string",
+		default: "linear"
+	},
+	gradientLocation1: {
+		type: "number",
+		default: 0
+	},
+	gradientLocation2: {
+		type: "number",
+		default: 100
+	},
+	gradientAngle: {
+		type: "number",
+		default: 0
+	},
+	backgroundOpacity: {
+		type: "number",
+		default: 0
+	},
+	backgroundVideoOpacity: {
+		type: "number",
+		default: 50
+	},
+	backgroundVideoColor: {
+		type: "string",
+	},
+	backgroundImageColor: {
+		type: "string"
+	},
+	borderStyle : {
+		type: "string",
+		default: "none"
+	},
+	borderWidth : {
+		type: "number",
+		default: 1
+	},
+	borderRadius : {
+		type: "number"
+	},
+	borderColor : {
+		type: "string"
+	}
+}
+
+export default attributes
---
+++ src/blocks/columns/block.js
@@ -0,0 +1,106 @@
+/**
+ * BLOCK: Section
+ */
+
+import classnames from "classnames"
+import UAGB_Block_Icons from "../../../dist/blocks/uagb-controls/block-icons"
+
+//  Import CSS.
+import "./style.scss"
+import "./editor.scss"
+import attributes from "./attributes"
+import edit from "./edit"
+
+
+// Components
+const { __ } = wp.i18n
+
+// Register block controls
+const {
+	registerBlockType
+} = wp.blocks
+
+const {
+	InnerBlocks,
+} = wp.editor
+
+/**
+ * Register: as Gutenberg Block.
+ *
+ * Registers a new block provided a unique name and an object defining its
+ * behavior.
+ *
+ * @link https://wordpress.org/gutenberg/handbook/block-api/
+ * @param  {string}   name     Block name.
+ * @param  {Object}   settings Block settings.
+ * @return {?WPBlock}          The block, if it has been successfully
+ *                             registered; otherwise `undefined`.
+ */
+registerBlockType( "uagb/Columns", {
+	title: uagb_blocks_info.blocks["uagb/Columns"]["title"],
+	description: uagb_blocks_info.blocks["uagb/Columns"]["description"],
+	icon: UAGB_Block_Icons.section,
+	category: uagb_blocks_info.category,
+	keywords: [
+		__( "columns" ),
+		__( "uagb" ),
+	],
+	attributes,
+	edit,
+	getEditWrapperProps( attributes ) {
+		const { align } = attributes
+		if ( "center" === align ||"wide" === align || "full" === align ) {
+			return { "data-align": align }
+		}
+	},
+	save : function( props ) {
+		return null
+
+		const { attributes, className } = props
+
+		const {
+			block_id,
+			tag,
+			backgroundType,
+			backgroundVideo,
+			contentWidth,
+			align
+		} = props.attributes
+
+		let block_controls_class = ""
+
+		if ( "full_width" == contentWidth ) {
+
+			if ( align == "wide" || align == "full" ) {
+				block_controls_class = "align" + align
+			}
+		}
+
+		const CustomTag = `${tag}`
+
+		return (
+			<CustomTag
+				className={ classnames(
+					className,
+					"uagb-section__wrap",
+					`uagb-section__background-${backgroundType}`,
+					block_controls_class
+				) }
+				id={ `uagb-section-${block_id}` }
+			>
+				<div className="uagb-section__overlay"></div>
+				{ "video" == backgroundType &&
+					<div className="uagb-section__video-wrap">
+						{  backgroundVideo &&
+						<video src={ backgroundVideo.url } autoPlay loop muted></video>
+						}
+
+					</div>
+				}
+				<div className="uagb-section__inner-wrap">
+					<InnerBlocks.Content />
+				</div>
+			</CustomTag>
+		)
+	}
+} )
