e1d3ab8b7db8c3ea1d49f959ae184042640ff89d
Prajakta <prajaktag@bsf.io>
2019-01-16 19:21:41 +0530

    Renamed restaurant menu to Price List

--- classes/class-uagb-config.php
+++ classes/class-uagb-config.php
@@ -13,6 +13,7 @@ if ( ! class_exists( 'UAGB_Config' ) ) {
 	class UAGB_Config {
 
 
+
 		/**
 		 * Block Attributes
 		 *
@@ -501,8 +502,8 @@ if ( ! class_exists( 'UAGB_Config' ) ) {
 					),
 					'uagb/restaurant-menu'  => array(
 						'slug'        => '',
-						'title'       => __( 'Restaurant Menu', 'ultimate-addons-for-gutenberg' ),
-						'description' => __( 'This block allows you to add attractive Restaurant Menu.', 'ultimate-addons-for-gutenberg' ),
+						'title'       => __( 'Price List', 'ultimate-addons-for-gutenberg' ),
+						'description' => __( 'This block allows you to add attractive Price List.', 'ultimate-addons-for-gutenberg' ),
 						'default'     => true,
 						'attributes'  => array(
 							'block_id'           => '',
