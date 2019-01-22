88ec4efdec2ba5672a64bf78ab8cd30159d36cca
Prajakta <prajaktag@bsf.io>
2019-01-22 16:04:48 +0530

    Moved icon to UAGB icon file

--- src/blocks/blockquote/block.js
+++ src/blocks/blockquote/block.js
@@ -88,7 +88,7 @@ registerBlockType( "uagb/blockquote", {
 				) } >
 					
 					<blockquote className="uagb-blockquote">					  
-					{ skinStyle === "quotation" && <div className="uagb-quote__icon-wrap"><span className="uagb-quote__icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M7.031 14c3.866 0 7 3.134 7 7s-3.134 7-7 7-7-3.134-7-7l-0.031-1c0-7.732 6.268-14 14-14v4c-2.671 0-5.182 1.040-7.071 2.929-0.364 0.364-0.695 0.751-0.995 1.157 0.357-0.056 0.724-0.086 1.097-0.086zM25.031 14c3.866 0 7 3.134 7 7s-3.134 7-7 7-7-3.134-7-7l-0.031-1c0-7.732 6.268-14 14-14v4c-2.671 0-5.182 1.040-7.071 2.929-0.364 0.364-0.695 0.751-0.995 1.157 0.358-0.056 0.724-0.086 1.097-0.086z"></path></svg></span></div> }
+					{ skinStyle === "quotation" && <div className="uagb-quote__icon-wrap"><span className="uagb-quote__icon">{ UAGB_Block_Icons.quote_inline_icon }</span></div> }
 					<div className="uagb-blockquote__content-wrap">
 					   	{ description_text !=="" && <Description attributes={props.attributes} setAttributes = "not_set" props = { props }  /> }
 
