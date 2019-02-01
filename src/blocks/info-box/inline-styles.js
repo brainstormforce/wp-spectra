4d27187da9465b9069aa48492b013bd1316da635
Prajakta <prajaktag@bsf.io>
2019-02-01 12:41:45 +0530

    Add responsive generate CSS for infobox

--- src/blocks/info-box/inline-styles.js
+++ src/blocks/info-box/inline-styles.js
@@ -3,6 +3,8 @@
  * @param  {object} props - The block object.
  * @return {object} The inline background type CSS.
  */
+import generateCSS from "../../../dist/blocks/uagb-controls/generateCSS"
+
 function InfoBoxStyle( props ) {
 	const {
 		headingAlign,
@@ -62,8 +64,6 @@ function InfoBoxStyle( props ) {
 		prefixFontSizeTablet,
 		prefixFontSizeMobile,
 	} = props.attributes
-
-	var clientId = "uagb-infobox-"+props.clientId
 
 	var selectors = {
 		// Icon css
@@ -250,65 +250,13 @@ function InfoBoxStyle( props ) {
 			"width": ctaFontSizeMobile + ctaFontSizeType,
 		}
 	}
+	
+	var styling_css = generateCSS( selectors, `.block-editor-page #wpwrap #uagb-infobox-${ props.clientId }` )
 
-	var styling_css = ""
+	styling_css += generateCSS( tablet_selectors, `.block-editor-page #wpwrap #uagb-infobox-${ props.clientId }`, true, "tablet" )
 
-	for( var i in selectors ) {
-
-		styling_css += " .block-editor-page #wpwrap #"+clientId+i + " { "
-
-
-		var sel = selectors[i]
-		var css = ""
-
-		for( var j in sel ) {
-
-			css += j + ": " + sel[j] + ";"
-		}
-
-		styling_css += css + " } "
-	}
-
-	styling_css += "@media only screen and (max-width: 976px) {"
-
-	for( var i in tablet_selectors ) {
-
-		styling_css += " .block-editor-page #wpwrap #"+clientId+i + " { "
-
-		var sel = tablet_selectors[i]
-		var css = ""
-
-		for( var j in sel ) {
-
-			css += j + ": " + sel[j] + ";"
-		}
-
-		styling_css += css + " } "
-	}
-
-	styling_css += " }"
-
-	styling_css += "@media only screen and (max-width: 767px) {"
-
-	for( var i in mobile_selectors ) {
-
-		styling_css += " .block-editor-page #wpwrap #"+clientId+i + " { "
-
-		var sel = mobile_selectors[i]
-		var css = ""
-
-		for( var j in sel ) {
-
-			css += j + ": " + sel[j] + ";"
-		}
-
-		styling_css += css + " } "
-	}
-
-	styling_css += " }"
-
+	styling_css += generateCSS( mobile_selectors, `.block-editor-page #wpwrap #uagb-infobox-${ props.clientId }`, true, "mobile" )
 	return styling_css
-
 }
 
 export default InfoBoxStyle
