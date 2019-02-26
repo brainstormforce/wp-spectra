5411eed5c86be9a8a6208fb0a064abf861dadd2d
Prajakta <prajaktag@bsf.io>
2019-02-26 14:35:15 +0530

    Changed cf selection

--- src/blocks/cf7-styler/edit.js
+++ src/blocks/cf7-styler/edit.js
@@ -464,27 +464,19 @@ class UAGBCF7 extends Component {
 			</PanelBody>			
 		)
 
-		const generalControls = (
-			<InspectorControls>
-				<PanelBody title={ __( "General" ) }>
-					<SelectControl
-						label={ __( "Select Form" ) }
-						value={ formId }
-						onChange={ this.onSelectForm }
-						options={ uagb_blocks_info.cf7_forms }
-					/>
-				</PanelBody>			
-			</InspectorControls>
-		)
-
 		if ( formId == 0 ) {
 			return (
 				<Fragment>
-					{ generalControls }
+					
 					<Placeholder
 						icon="admin-post"
 						label={ __( "Select a Contact Form 7" ) }
 					>
+					<SelectControl				
+						value={ formId }
+						onChange={ this.onSelectForm }
+						options={ uagb_blocks_info.cf7_forms }
+					/>	
 					</Placeholder>
 				</Fragment>
 			)
