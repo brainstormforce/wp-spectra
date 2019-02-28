44c1e8af8cdcc711ddd66bb8310c2ab41840a225
Vrunda <vrundak@bsf.io>
2019-02-28 05:02:55 -0500

    Show Rating notice on condition - More than 5 pages built in UAG

--- classes/class-uagb-helper.php
+++ classes/class-uagb-helper.php
@@ -12,7 +12,6 @@ if ( ! class_exists( 'UAGB_Helper' ) ) {
 	 */
 	final class UAGB_Helper {
 
-
 		/**
 		 * Member Variable
 		 *
@@ -819,6 +818,50 @@ if ( ! class_exists( 'UAGB_Helper' ) ) {
 			$image_sizes = apply_filters( 'uagb_post_featured_image_sizes', $image_sizes );
 
 			return $image_sizes;
+		}
+
+		/**
+		 * Get flag if more than 5 pages are build using UAG.
+		 *
+		 * @since  x.x.x
+		 * @return boolean true/false Flag if more than 5 pages are build using UAG.
+		 */
+		public static function show_rating_notice() {
+
+			$posts_created_with_uag = get_option( 'posts-created-with-uagb' );
+
+			if ( false === $posts_created_with_uag ) {
+
+				$query_args = array(
+					'posts_per_page' => -1,
+					'post_status'    => 'publish',
+					'post_type'      => 'any',
+				);
+
+				$query = new WP_Query( $query_args );
+
+				$uag_post_count = 0;
+
+				if ( isset( $query->post_count ) && $query->post_count > 0 ) {
+					foreach ( $query->posts as $key => $post ) {
+						if ( $uag_post_count >= 5 ) {
+							break;
+						}
+
+						if ( false !== strpos( $post->post_content, '<!-- wp:uagb/' ) ) {
+							$uag_post_count++;
+						}
+					}
+				}
+
+				if ( $uag_post_count >= 5 ) {
+					update_option( 'posts-created-with-uagb', $uag_post_count );
+
+					$posts_created_with_uag = $post_count;
+				}
+			}
+
+			return ( $posts_created_with_uag >= 5 );
 		}
 	}
 
