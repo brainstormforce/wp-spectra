246934b809ca78412234edd2d6cae6f636dfc72e
Prajakta <prajaktag@bsf.io>
2019-03-13 15:18:10 +0530

    Temp

---
+++ assets/js/post_carousel.js
@@ -0,0 +1,21 @@
+( function( $ ) {
+
+	var scroll = true
+	var scroll_offset = 30
+	var scroll_delay = 800
+	var scroll_to_top = false
+	var scroll_element = null
+
+	UAGBPostCarousel = {
+		setCarouselHeight: function( e ) {
+			console.log("here");
+		},
+		
+	}
+
+	$( document ).ready(function() {
+		UAGBPostCarousel.setCarouselHeight()
+	})
+
+
+} )( jQuery )
--- classes/class-uagb-init-blocks.php
+++ classes/class-uagb-init-blocks.php
@@ -8,8 +8,8 @@
  * @package UAGB
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
+if (! defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
 }
 
 /**
@@ -17,296 +17,312 @@ if ( ! defined( 'ABSPATH' ) ) {
  *
  * @package UAGB
  */
-class UAGB_Init_Blocks {
+class UAGB_Init_Blocks
+{
 
-	/**
-	 * Member Variable
-	 *
-	 * @var instance
-	 */
-	private static $instance;
+    /**
+     * Member Variable
+     *
+     * @var instance
+     */
+    private static $instance;
 
-	/**
-	 *  Initiator
-	 */
-	public static function get_instance() {
-		if ( ! isset( self::$instance ) ) {
-			self::$instance = new self;
-		}
-		return self::$instance;
-	}
+    /**
+     *  Initiator
+     */
+    public static function get_instance()
+    {
+        if (! isset(self::$instance)) {
+            self::$instance = new self;
+        }
+        return self::$instance;
+    }
 
-	/**
-	 * Constructor
-	 */
-	public function __construct() {
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
 
-		// Hook: Frontend assets.
-		add_action( 'enqueue_block_assets', array( $this, 'block_assets' ) );
+        // Hook: Frontend assets.
+        add_action('enqueue_block_assets', array( $this, 'block_assets' ));
 
-		// Hook: Editor assets.
-		add_action( 'enqueue_block_editor_assets', array( $this, 'editor_assets' ) );
+        // Hook: Editor assets.
+        add_action('enqueue_block_editor_assets', array( $this, 'editor_assets' ));
 
-		add_filter( 'block_categories', array( $this, 'register_block_category' ), 10, 2 );
-	}
+        add_filter('block_categories', array( $this, 'register_block_category' ), 10, 2);
+    }
 
-	/**
-	 * Gutenberg block category for UAGB.
-	 *
-	 * @param array  $categories Block categories.
-	 * @param object $post Post object.
-	 * @since 1.0.0
-	 */
-	function register_block_category( $categories, $post ) {
-		return array_merge(
-			$categories,
-			array(
-				array(
-					'slug'  => 'uagb',
-					'title' => __( 'Ultimate Addons Blocks', 'ultimate-addons-for-gutenberg' ),
-				),
-			)
-		);
-	}
+    /**
+     * Gutenberg block category for UAGB.
+     *
+     * @param array  $categories Block categories.
+     * @param object $post Post object.
+     * @since 1.0.0
+     */
+    function register_block_category($categories, $post)
+    {
+        return array_merge(
+            $categories,
+            array(
+                array(
+                    'slug'  => 'uagb',
+                    'title' => __('Ultimate Addons Blocks', 'ultimate-addons-for-gutenberg'),
+                ),
+            )
+        );
+    }
 
-	/**
-	 * Enqueue Gutenberg block assets for both frontend + backend.
-	 *
-	 * @since 1.0.0
-	 */
-	function block_assets() {
-		// Styles.
-		wp_enqueue_style(
-			'uagb-block-css', // Handle.
-			UAGB_URL . 'dist/blocks.style.build.css', // Block style CSS.
-			UAGB_VER
-		);
+    /**
+     * Enqueue Gutenberg block assets for both frontend + backend.
+     *
+     * @since 1.0.0
+     */
+    function block_assets()
+    {
+        // Styles.
+        wp_enqueue_style(
+            'uagb-block-css', // Handle.
+            UAGB_URL . 'dist/blocks.style.build.css', // Block style CSS.
+            UAGB_VER
+        );
 
-		// Scripts.
-		wp_enqueue_script(
-			'uagb-masonry', // Handle.
-			UAGB_URL . 'assets/js/isotope.min.js',
-			array( 'jquery' ), // Dependencies, defined above.
-			UAGB_VER,
-			false // Enqueue the script in the footer.
-		);
+        // Scripts.
+        wp_enqueue_script(
+            'uagb-masonry', // Handle.
+            UAGB_URL . 'assets/js/isotope.min.js',
+            array( 'jquery' ), // Dependencies, defined above.
+            UAGB_VER,
+            false // Enqueue the script in the footer.
+        );
 
-		wp_enqueue_script(
-			'uagb-imagesloaded', // Handle.
-			UAGB_URL . 'assets/js/imagesloaded.min.js',
-			array( 'jquery' ), // Dependencies, defined above.
-			UAGB_VER,
-			false // Enqueue the script in the footer.
-		);
+        wp_enqueue_script(
+            'uagb-imagesloaded', // Handle.
+            UAGB_URL . 'assets/js/imagesloaded.min.js',
+            array( 'jquery' ), // Dependencies, defined above.
+            UAGB_VER,
+            false // Enqueue the script in the footer.
+        );
 
-		$value = true;
+        $value = true;
 
-		if ( did_action( 'elementor/loaded' ) ) {
-			$value = false;
-		}
+        if (did_action('elementor/loaded')) {
+            $value = false;
+        }
 
-		$enable_font_awesome = apply_filters( 'uagb_font_awesome_enable', $value );
+        $enable_font_awesome = apply_filters('uagb_font_awesome_enable', $value);
 
-		if ( $enable_font_awesome ) {
-			$font_awesome = apply_filters( 'uagb_font_awesome_url', 'https://use.fontawesome.com/releases/v5.6.0/css/all.css' );
-			// Font Awesome.
-			wp_enqueue_style(
-				'uagb-fontawesome-css', // Handle.
-				$font_awesome, // Block style CSS.
-				UAGB_VER
-			);
-		}
+        if ($enable_font_awesome) {
+            $font_awesome = apply_filters('uagb_font_awesome_url', 'https://use.fontawesome.com/releases/v5.6.0/css/all.css');
+            // Font Awesome.
+            wp_enqueue_style(
+                'uagb-fontawesome-css', // Handle.
+                $font_awesome, // Block style CSS.
+                UAGB_VER
+            );
+        }
 
-		// Scripts.
-		wp_enqueue_script(
-			'uagb-slick-js', // Handle.
-			UAGB_URL . 'assets/js/slick.min.js',
-			array( 'jquery' ), // Dependencies, defined above.
-			UAGB_VER,
-			false // Enqueue the script in the footer.
-		);
+        // Scripts.
+        wp_enqueue_script(
+            'uagb-slick-js', // Handle.
+            UAGB_URL . 'assets/js/slick.min.js',
+            array( 'jquery' ), // Dependencies, defined above.
+            UAGB_VER,
+            false // Enqueue the script in the footer.
+        );
 
-		// Styles.
-		wp_enqueue_style(
-			'uagb-slick-css', // Handle.
-			UAGB_URL . 'assets/css/slick.css', // Block style CSS.
-			UAGB_VER
-		);
+        // Styles.
+        wp_enqueue_style(
+            'uagb-slick-css', // Handle.
+            UAGB_URL . 'assets/css/slick.css', // Block style CSS.
+            UAGB_VER
+        );
 
-		// Timeline js.
-		wp_enqueue_script(
-			'uagb-timeline-js', // Handle.
-			UAGB_URL . 'assets/js/timeline.js',
-			array( 'jquery' ),
-			UAGB_VER,
-			true // Enqueue the script in the footer.
-		);
+        // Timeline js.
+        wp_enqueue_script(
+            'uagb-timeline-js', // Handle.
+            UAGB_URL . 'assets/js/timeline.js',
+            array( 'jquery' ),
+            UAGB_VER,
+            true // Enqueue the script in the footer.
+        );
 
-		if ( ! wp_script_is( 'jquery', 'enqueued' ) ) {
-			wp_enqueue_script( 'jquery' );
-		}
+        // Carousel js.
+        wp_enqueue_script(
+            'uagb-carousel-js', // Handle.
+            UAGB_URL . 'assets/js/post_carousel.js',
+            array( 'jquery' ),
+            UAGB_VER,
+            true // Enqueue the script in the footer.
+        );
 
-		if ( ! wp_script_is( 'contact-form-7', 'enqueued' ) ) {
-			wp_enqueue_script( 'contact-form-7' );
-		}
+        if (! wp_script_is('jquery', 'enqueued')) {
+            wp_enqueue_script('jquery');
+        }
 
-		if ( ! wp_script_is( ' wpcf7-admin', 'enqueued' ) ) {
-			wp_enqueue_script( ' wpcf7-admin' );
-		}
-	} // End function editor_assets().
+        if (! wp_script_is('contact-form-7', 'enqueued')) {
+            wp_enqueue_script('contact-form-7');
+        }
 
-	/**
-	 * Enqueue Gutenberg block assets for backend editor.
-	 *
-	 * @since 1.0.0
-	 */
-	function editor_assets() {
-		// Scripts.
-		wp_enqueue_script(
-			'uagb-block-editor-js', // Handle.
-			UAGB_URL . 'dist/blocks.build.js',
-			array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-components', 'wp-editor', 'wp-api-fetch' ), // Dependencies, defined above.
-			UAGB_VER,
-			true // Enqueue the script in the footer.
-		);
+        if (! wp_script_is(' wpcf7-admin', 'enqueued')) {
+            wp_enqueue_script(' wpcf7-admin');
+        }
+    } // End function editor_assets().
 
-		// Styles.
-		wp_enqueue_style(
-			'uagb-block-editor-css', // Handle.
-			UAGB_URL . 'dist/blocks.editor.build.css', // Block editor CSS.
-			array( 'wp-edit-blocks' ), // Dependency to include the CSS after it.
-			UAGB_VER
-		);
+    /**
+     * Enqueue Gutenberg block assets for backend editor.
+     *
+     * @since 1.0.0
+     */
+    function editor_assets()
+    {
+        // Scripts.
+        wp_enqueue_script(
+            'uagb-block-editor-js', // Handle.
+            UAGB_URL . 'dist/blocks.build.js',
+            array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-components', 'wp-editor', 'wp-api-fetch' ), // Dependencies, defined above.
+            UAGB_VER,
+            true // Enqueue the script in the footer.
+        );
 
-		// Common Editor style.
-		wp_enqueue_style(
-			'uagb-block-common-editor-css', // Handle.
-			UAGB_URL . 'dist/blocks.commoneditorstyle.build.css', // Block editor CSS.
-			array( 'wp-edit-blocks' ), // Dependency to include the CSS after it.
-			UAGB_VER
-		);
+        // Styles.
+        wp_enqueue_style(
+            'uagb-block-editor-css', // Handle.
+            UAGB_URL . 'dist/blocks.editor.build.css', // Block editor CSS.
+            array( 'wp-edit-blocks' ), // Dependency to include the CSS after it.
+            UAGB_VER
+        );
 
-		wp_enqueue_script( 'uagb-deactivate-block-js', UAGB_URL . 'dist/blocks-deactivate.js', array( 'wp-blocks' ), UAGB_VER, true );
+        // Common Editor style.
+        wp_enqueue_style(
+            'uagb-block-common-editor-css', // Handle.
+            UAGB_URL . 'dist/blocks.commoneditorstyle.build.css', // Block editor CSS.
+            array( 'wp-edit-blocks' ), // Dependency to include the CSS after it.
+            UAGB_VER
+        );
 
-		$blocks       = array();
-		$saved_blocks = UAGB_Helper::get_admin_settings_option( '_uagb_blocks' );
+        wp_enqueue_script('uagb-deactivate-block-js', UAGB_URL . 'dist/blocks-deactivate.js', array( 'wp-blocks' ), UAGB_VER, true);
 
-		if ( is_array( $saved_blocks ) ) {
-			foreach ( $saved_blocks as $slug => $data ) {
-				$_slug         = 'uagb/' . $slug;
-				$current_block = UAGB_Config::$block_attributes[ $_slug ];
+        $blocks       = array();
+        $saved_blocks = UAGB_Helper::get_admin_settings_option('_uagb_blocks');
 
-				if ( isset( $current_block['is_child'] ) && $current_block['is_child'] ) {
-					continue;
-				}
+        if (is_array($saved_blocks)) {
+            foreach ($saved_blocks as $slug => $data) {
+                $_slug         = 'uagb/' . $slug;
+                $current_block = UAGB_Config::$block_attributes[ $_slug ];
 
-				if ( isset( $saved_blocks[ $slug ] ) ) {
-					if ( 'disabled' === $saved_blocks[ $slug ] ) {
-						array_push( $blocks, $_slug );
-					}
-				}
-			}
-		}
+                if (isset($current_block['is_child']) && $current_block['is_child']) {
+                    continue;
+                }
 
-		wp_localize_script(
-			'uagb-deactivate-block-js',
-			'uagb_deactivate_blocks',
-			array(
-				'deactivated_blocks' => $blocks,
-			)
-		);
+                if (isset($saved_blocks[ $slug ])) {
+                    if ('disabled' === $saved_blocks[ $slug ]) {
+                        array_push($blocks, $_slug);
+                    }
+                }
+            }
+        }
 
-		wp_localize_script(
-			'uagb-block-editor-js',
-			'uagb_blocks_info',
-			array(
-				'blocks'            => UAGB_Config::get_block_attributes(),
-				'category'          => 'uagb',
-				'ajax_url'          => admin_url( 'admin-ajax.php' ),
-				'cf7_forms'         => $this->get_cf7_forms(),
-				'gf_forms'          => $this->get_gravity_forms(),
-				'tablet_breakpoint' => UAGB_TABLET_BREAKPOINT,
-				'mobile_breakpoint' => UAGB_MOBILE_BREAKPOINT,
-				'image_sizes'       => UAGB_Helper::get_image_sizes(),
-				'post_types'        => UAGB_Helper::get_post_types(),
-				'all_taxonomy'      => UAGB_Helper::get_related_taxonomy(),
-			)
-		);
-	} // End function editor_assets().
+        wp_localize_script(
+            'uagb-deactivate-block-js',
+            'uagb_deactivate_blocks',
+            array(
+                'deactivated_blocks' => $blocks,
+            )
+        );
+
+        wp_localize_script(
+            'uagb-block-editor-js',
+            'uagb_blocks_info',
+            array(
+                'blocks'            => UAGB_Config::get_block_attributes(),
+                'category'          => 'uagb',
+                'ajax_url'          => admin_url('admin-ajax.php'),
+                'cf7_forms'         => $this->get_cf7_forms(),
+                'gf_forms'          => $this->get_gravity_forms(),
+                'tablet_breakpoint' => UAGB_TABLET_BREAKPOINT,
+                'mobile_breakpoint' => UAGB_MOBILE_BREAKPOINT,
+                'image_sizes'       => UAGB_Helper::get_image_sizes(),
+                'post_types'        => UAGB_Helper::get_post_types(),
+                'all_taxonomy'      => UAGB_Helper::get_related_taxonomy(),
+            )
+        );
+    } // End function editor_assets().
 
 
-	/**
-	 * Function to integrate CF7 Forms.
-	 *
-	 * @since 1.10.0
-	 */
-	public function get_cf7_forms() {
+    /**
+     * Function to integrate CF7 Forms.
+     *
+     * @since 1.10.0
+     */
+    public function get_cf7_forms()
+    {
 
-		$field_options = array();
+        $field_options = array();
 
-		if ( class_exists( 'WPCF7_ContactForm' ) ) {
-			$args             = array(
-				'post_type'      => 'wpcf7_contact_form',
-				'posts_per_page' => -1,
-			);
-			$forms            = get_posts( $args );
-			$field_options[0] = array(
-				'value' => -1,
-				'label' => __( 'Select Form', 'ultimate-addons-for-gutenberg' ),
-			);
-			if ( $forms ) {
-				foreach ( $forms as $form ) {
-					$field_options[] = array(
-						'value' => $form->ID,
-						'label' => $form->post_title,
-					);
-				}
-			}
-		}
+        if (class_exists('WPCF7_ContactForm')) {
+            $args             = array(
+                'post_type'      => 'wpcf7_contact_form',
+                'posts_per_page' => -1,
+            );
+            $forms            = get_posts($args);
+            $field_options[0] = array(
+                'value' => -1,
+                'label' => __('Select Form', 'ultimate-addons-for-gutenberg'),
+            );
+            if ($forms) {
+                foreach ($forms as $form) {
+                    $field_options[] = array(
+                        'value' => $form->ID,
+                        'label' => $form->post_title,
+                    );
+                }
+            }
+        }
 
-		if ( empty( $field_options ) ) {
-			$field_options = array(
-				'-1' => __( 'You have not added any Contact Form 7 yet.', 'ultimate-addons-for-gutenberg' ),
-			);
-		}
-		return $field_options;
-	}
+        if (empty($field_options)) {
+            $field_options = array(
+                '-1' => __('You have not added any Contact Form 7 yet.', 'ultimate-addons-for-gutenberg'),
+            );
+        }
+        return $field_options;
+    }
 
-	/**
-	 * Returns all gravity forms with ids
-	 *
-	 * @since x.x.x
-	 * @return array Key Value paired array.
-	 */
-	public function get_gravity_forms() {
+    /**
+     * Returns all gravity forms with ids
+     *
+     * @since x.x.x
+     * @return array Key Value paired array.
+     */
+    public function get_gravity_forms()
+    {
 
-		$field_options = array();
+        $field_options = array();
 
-		if ( class_exists( 'GFForms' ) ) {
-			$forms            = RGFormsModel::get_forms( null, 'title' );
-			$field_options[0] = array(
-				'value' => -1,
-				'label' => __( 'Select Form', 'ultimate-addons-for-gutenberg' ),
-			);
-			if ( is_array( $forms ) ) {
-				foreach ( $forms as $form ) {
-					$field_options[] = array(
-						'value' => $form->id,
-						'label' => $form->title,
-					);
-				}
-			}
-		}
+        if (class_exists('GFForms')) {
+            $forms            = RGFormsModel::get_forms(null, 'title');
+            $field_options[0] = array(
+                'value' => -1,
+                'label' => __('Select Form', 'ultimate-addons-for-gutenberg'),
+            );
+            if (is_array($forms)) {
+                foreach ($forms as $form) {
+                    $field_options[] = array(
+                        'value' => $form->id,
+                        'label' => $form->title,
+                    );
+                }
+            }
+        }
 
-		if ( empty( $field_options ) ) {
-			$field_options = array(
-				'-1' => __( 'You have not added any Gravity Forms yet.', 'uael' ),
-			);
-		}
+        if (empty($field_options)) {
+            $field_options = array(
+                '-1' => __('You have not added any Gravity Forms yet.', 'uael'),
+            );
+        }
 
-		return $field_options;
-	}
-
+        return $field_options;
+    }
 }
 
 /**
