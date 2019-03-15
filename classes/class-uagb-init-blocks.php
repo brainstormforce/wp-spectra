896456a9a097b955f01dfc3a03985e89c81f4d25
Vrunda Kansara <vrundak@bsf.io>
2019-03-15 16:09:53 +0530

    JS assets loaded on condition for enabled block

--- classes/class-uagb-init-blocks.php
+++ classes/class-uagb-init-blocks.php
@@ -116,13 +116,16 @@ class UAGB_Init_Blocks {
 			);
 		}
 
-		wp_enqueue_script(
-			'uagb-table-of-contents', // Handle.
-			UAGB_URL . 'assets/js/table-of-contents.js',
-			array( 'jquery' ), // Dependencies, defined above.
-			UAGB_VER,
-			false // Enqueue the script in the footer.
-		);
+		if ( ! ( isset( $blocks['table-of-contents'] ) && 'disabled' == $blocks['table-of-contents'] ) ) {
+
+			wp_enqueue_script(
+				'uagb-table-of-contents', // Handle.
+				UAGB_URL . 'assets/js/table-of-contents.js',
+				array( 'jquery' ), // Dependencies, defined above.
+				UAGB_VER,
+				false // Enqueue the script in the footer.
+			);
+		}
 
 		$value = true;
 
