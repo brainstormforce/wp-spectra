fefcf4c9243c9f00cfef57ac68aaceac56864726
balachandark <balachandark@bsf.io>
2019-04-19 10:37:25 +0530

    wip

--- src/blocks/table-of-contents/components.js
+++ src/blocks/table-of-contents/components.js
@@ -111,6 +111,7 @@ class TableOfContents extends Component {
 		const makeHeaderArray = origHeaders => {
 			let arrays = [];
 
+			console.log( origHeaders );
 			origHeaders
 				.filter(header => mappingHeaders[header.level - 1])
 				.forEach(header => {
