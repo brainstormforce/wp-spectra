c9935081bc2245dbf9890ff2112b4907d69144bb
vrundak <vrundak@bsf.io>
2019-12-09 15:16:33 +0530

    wip

--- classes/class-uagb-block-helper.php
+++ classes/class-uagb-block-helper.php
@@ -543,11 +543,13 @@ if ( ! class_exists( 'UAGB_Block_Helper' ) ) {
 
 			// @codingStandardsIgnoreEnd
 
-			$desktop = UAGB_Helper::generate_css( $selectors, '#uagb-column-' . $id );
+			$base_selector = ( $attr['classMigrate'] ) ? '.uagb-block-' : '#uagb-column-';
 
-			$tablet = UAGB_Helper::generate_css( $t_selectors, '#uagb-column-' . $id );
+			$desktop = UAGB_Helper::generate_css( $selectors, $base_selector . $id );
 
-			$mobile = UAGB_Helper::generate_css( $m_selectors, '#uagb-column-' . $id );
+			$tablet = UAGB_Helper::generate_css( $t_selectors, $base_selector . $id );
+
+			$mobile = UAGB_Helper::generate_css( $m_selectors, $base_selector . $id );
 
 			$generated_css = array(
 				'desktop' => $desktop,
