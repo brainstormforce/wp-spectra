18a6bb71049a5dce330a139818ef2b13231efb0c
Vrunda Kansara <vrundak@bsf.io>
2020-04-03 15:30:23 +0900

    Design changes

--- src/components/box-shadow/index.js
+++ src/components/box-shadow/index.js
@@ -33,66 +33,68 @@ class BoxShadowControl extends Component {
         } = this.props
 
         return(
-            <div className="uag-box-shadow-options">
-                <Fragment>
-                <p className="uagb-setting-label">{ boxShadowColor.label }<span className="components-base-control__label"><span className="component-color-indicator" style={{ backgroundColor: boxShadowColor.value }} ></span></span></p>
-                <ColorPalette
-                    value={ boxShadowColor.value }
-                    onChange={ ( colorValue ) => setAttributes( { boxShadowColor: colorValue } ) }
-                    allowReset
-                />
-                </Fragment>
-                <Fragment>
-                <h2>{ boxShadowHOffset.label }</h2>
-                <RangeControl
-                    value={ boxShadowHOffset.value }
-                    onChange={ ( value ) => setAttributes( { boxShadowHOffset: value } ) }
-                    min={ 0 }
-                    max={ 100 }
-                    allowReset
-                />
-                </Fragment>
-                <Fragment>
-                <h2>{ boxShadowVOffset.label }</h2>
-                <RangeControl
-                    value={ boxShadowVOffset.value }
-                    onChange={ ( value ) => setAttributes( { boxShadowVOffset: value } ) }
-                    min={ 0 }
-                    max={ 100 }
-                    allowReset
-                />
-                </Fragment>
-                <Fragment>
-                <h2>{ boxShadowBlur.label }</h2>
-                <RangeControl
-                    value={ boxShadowBlur.value }
-                    onChange={ ( value ) => setAttributes( { boxShadowBlur: value } ) }
-                    min={ 0 }
-                    max={ 100 }
-                    allowReset
-                />
-                </Fragment>
-                <Fragment>
-                <h2>{ boxShadowSpread.label }</h2>
-                <RangeControl
-                    value={ boxShadowSpread.value }
-                    onChange={ ( value ) => setAttributes( { boxShadowSpread: value } ) }
-                    min={ 0 }
-                    max={ 100 }
-                    allowReset
-                />
-                </Fragment>
-                <Fragment>
-                <SelectControl
-                    label={ boxShadowPosition.label }
-                    value={ boxShadowPosition.value }
-                    onChange={ ( value ) => setAttributes( { boxShadowPosition: value } ) }
-                    options={ [
-                        { value: "inset", label: __( "Inset" ) },
-                        { value: "outset", label: __( "Outset" ) },
-                    ] }
-                />
-                </Fragment>
+            <div className="uagb-box-shadow-advanced">
+                <div className="uag-box-shadow-options">
+                    <Fragment>
+                    <p className="uagb-setting-label">{ boxShadowColor.label }<span className="components-base-control__label"><span className="component-color-indicator" style={{ backgroundColor: boxShadowColor.value }} ></span></span></p>
+                    <ColorPalette
+                        value={ boxShadowColor.value }
+                        onChange={ ( colorValue ) => setAttributes( { boxShadowColor: colorValue } ) }
+                        allowReset
+                    />
+                    </Fragment>
+                    <Fragment>
+                    <h2>{ boxShadowHOffset.label }</h2>
+                    <RangeControl
+                        value={ boxShadowHOffset.value }
+                        onChange={ ( value ) => setAttributes( { boxShadowHOffset: value } ) }
+                        min={ 0 }
+                        max={ 100 }
+                        allowReset
+                    />
+                    </Fragment>
+                    <Fragment>
+                    <h2>{ boxShadowVOffset.label }</h2>
+                    <RangeControl
+                        value={ boxShadowVOffset.value }
+                        onChange={ ( value ) => setAttributes( { boxShadowVOffset: value } ) }
+                        min={ 0 }
+                        max={ 100 }
+                        allowReset
+                    />
+                    </Fragment>
+                    <Fragment>
+                    <h2>{ boxShadowBlur.label }</h2>
+                    <RangeControl
+                        value={ boxShadowBlur.value }
+                        onChange={ ( value ) => setAttributes( { boxShadowBlur: value } ) }
+                        min={ 0 }
+                        max={ 100 }
+                        allowReset
+                    />
+                    </Fragment>
+                    <Fragment>
+                    <h2>{ boxShadowSpread.label }</h2>
+                    <RangeControl
+                        value={ boxShadowSpread.value }
+                        onChange={ ( value ) => setAttributes( { boxShadowSpread: value } ) }
+                        min={ 0 }
+                        max={ 100 }
+                        allowReset
+                    />
+                    </Fragment>
+                    <Fragment>
+                    <SelectControl
+                        label={ boxShadowPosition.label }
+                        value={ boxShadowPosition.value }
+                        onChange={ ( value ) => setAttributes( { boxShadowPosition: value } ) }
+                        options={ [
+                            { value: "inset", label: __( "Inset" ) },
+                            { value: "outset", label: __( "Outset" ) },
+                        ] }
+                    />
+                    </Fragment>
+                </div>
             </div>
         )
     }
