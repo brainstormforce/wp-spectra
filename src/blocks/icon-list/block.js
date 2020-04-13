d521d5f12bd2874c4cfdf2681e845aacbb50f031
Vrunda Kansara <vrundak@bsf.io>
2020-04-13 14:31:11 +0900

    WIP

--- src/blocks/icon-list/block.js
+++ src/blocks/icon-list/block.js
@@ -7,6 +7,7 @@ import UAGB_Block_Icons from "../../../dist/blocks/uagb-controls/block-icons"
 import attributes from "./attributes"
 import edit from "./edit"
 import save from "./save"
+import transform from "./transform"
 import deprecated from "./deprecated"
 import "./style.scss"
 import "./editor.scss"
@@ -32,8 +33,12 @@ registerBlockType( "uagb/icon-list", {
 		__( "image list" ),
 		__( "uag" ),
 	],
+	supports: {
+		anchor: true,
+	},
 	attributes,
 	edit,
 	save,
+	//transform,
 	deprecated
 } )
