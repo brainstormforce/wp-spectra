bb291e0d636b525a0a9409172dedb7ded612205d
Raj <rajkiranb@bsf.io>
2020-04-14 14:48:51 +0530

    initial commit

---
+++ src/blocks/buttons-child/block.js
@@ -0,0 +1,28 @@
+/**
+ * BLOCK: Buttons - Child
+ */
+
+import UAGB_Block_Icons from "../../../dist/blocks/uagb-controls/block-icons"
+import attributes from "./attributes"
+import edit from "./edit"
+import save from "./save"
+import "./style.scss"
+import "./editor.scss"
+
+
+const { __ } = wp.i18n
+
+const {
+	registerBlockType
+} = wp.blocks
+
+registerBlockType( "uagb/buttons-child", {
+	title: uagb_blocks_info.blocks["uagb/buttons-child"]["title"],
+	description: uagb_blocks_info.blocks["uagb/buttons-child"]["description"],
+	icon: UAGB_Block_Icons.icon_list,
+	category: uagb_blocks_info.category,
+	parent: [ "uagb/buttons" ],
+	attributes,
+	edit,
+	save,
+} )
