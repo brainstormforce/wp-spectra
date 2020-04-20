60707b4368f631509bdfe3f07f68b797b4775dec
AbhijitNage123 <abhijitn@bsf.io>
2020-04-20 10:41:18 +0530

    Scroll to top background color not working

--- classes/class-uagb-block-helper.php
+++ classes/class-uagb-block-helper.php
@@ -4680,7 +4680,7 @@ if ( ! class_exists( 'UAGB_Block_Helper' ) ) {
 			}
 
 			if ( '' !== $attr['scrollToTopBgColor'] ) {
-				$desktop .= '.uagb-toc__scroll-top { background: ' . $attr['scrollToTopBgColor'] . '; }';
+				$desktop .= '.uagb-toc__scroll-top.uagb-toc__show-scroll { background: ' . $attr['scrollToTopBgColor'] . '; }';
 			}
 
 			$generated_css = array(
