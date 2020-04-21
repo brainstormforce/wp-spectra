56a672da6a09031e5fea9e888e5eee063fe1fa00
Raj <rajkiranb@bsf.io>
2020-04-21 15:51:24 +0530

    resolved undef err

--- dist/blocks/post/index.php
+++ dist/blocks/post/index.php
@@ -271,7 +271,7 @@ function uagb_get_post_html( $attributes, $query, $layout ) {
 		?>
 		</div>
 		<?php
-		if ( $attributes['postPagination'] ) {
+		if ( isset( $attributes['postPagination'] ) && true === $attributes['postPagination'] ) {
 
 			echo '<div class="uagb-post-pagination-wrap">';
 			echo uagb_render_pagination( $query, $attributes );
