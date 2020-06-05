a53acd4c0a0842890b22faadb3e486aca89d09fc
Raj <rajkiranb@bsf.io>
2020-06-05 16:37:53 +0530

    updated URL

--- src/blocks/wp-search/edit.js
+++ src/blocks/wp-search/edit.js
@@ -476,7 +476,7 @@ class UAGBWpSearchEdit extends Component {
             if ( 'input-button' === layout || 'input' === layout ) {
                 
                 return (
-                    <form className="uagb-search-wrapper" role="search" action="https://wordpress.test" method="get">
+                    <form className="uagb-search-wrapper" role="search" action={ uagb_blocks_info.uagb_home_url } method="get">
                         <div className="uagb-search-form__container" role="tablist">
                             <input placeholder={ placeholder } 
                             className="uagb-search-form__input" type="search" name="s" title="Search"/>
