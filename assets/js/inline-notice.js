7b1fffc2c54aba24e8c551cf04c5f77599207752
AbhijitNage123 <abhijitn@bsf.io>
2020-06-15 14:16:32 +0530

    Fix the issue of Active class not applying to the block

--- assets/js/inline-notice.js
+++ assets/js/inline-notice.js
@@ -15,26 +15,22 @@
 	 $(id+" .uagb-notice-dismiss").click(function(){
 	 	if ( true === is_cookie && 'undefined' !== typeof current_cookie) {
 	 		 current_cookies = Cookies.set( 'uagb-notice-' + unique_id, true, { expires: cookies_days } );
-   			 $(".uagb-block-"+unique_id).addClass("active");
-   			 $(id).css('display' ,'none')
+         $(id).addClass("active").css('display' ,'none')
    		}
 
    		if( 'undefined' === typeof current_cookie && true === is_cookie ){
   	 		current_cookies = Cookies.set( 'uagb-notice-' + unique_id, true, { expires: cookies_days } );
-   			 $(".uagb-block-"+unique_id).addClass("active");
-   		 	$(id).css('display' ,'none')
+        $(id).addClass("active").css('display' ,'none')
   	 	}
 
   	 	if( false === is_cookie ){
-  	 		$(".uagb-block-"+unique_id).addClass("active");
-   			  $(id).css('display' ,'none')
+          $(id).addClass("active").css('display' ,'none')
    		}
   	 });
 
   	 $( window ).on( "load", function(){
   	 	if ( true === is_cookie && 'undefined' !== typeof current_cookie ) {
-   		 $(".uagb-block-"+unique_id).addClass("active");
-   		 $(id).css('display' ,'none')
+       $(id).addClass("active").css('display' ,'none')
    		}
 
    		if( 'undefined' === typeof current_cookie && true === is_cookie ){
