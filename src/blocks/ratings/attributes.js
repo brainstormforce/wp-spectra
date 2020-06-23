2a630eebcf4b24fdf9f19845e058c14e1e047b77
AbhijitNage123 <abhijitn@bsf.io>
2020-06-23 18:41:30 +0530

    added title,description,image and list of feature and related settings

--- src/blocks/ratings/attributes.js
+++ src/blocks/ratings/attributes.js
@@ -1,6 +1,15 @@
 /**
  * BLOCK: UAGB Inline Notice Block Attributes
  */
+  const { __ } = wp.i18n
+
+ const features = []
+
+ features.push(
+ 	{
+ 		"feature_name": __( "- Feature Name." ),
+ 	}
+ )
 
 
  const attributes = {
@@ -9,11 +18,41 @@
 	},
 	rTitle: {
 		type: "string",
-		default: "Notice Title",
+		default: "Ultimate Addons for Gutenberg",
 	},
 	rContent: {
 		type: "string",
-		default: "",
+		default: "The Ultimate Gutenberg Blocks Library. Supercharge the Gutenberg editor with beautiful and powerful blocks to design stunning websites in minutes!",
+	},
+	mainimage: {
+		type: "object",
+		default:{
+			"url": "",
+		}
+	},
+	imgSize:{
+		type: "string",
+		default: "thumbnail",
+	},
+	featuresTitle : {
+		type: "string",
+		default : __( "List Of Features:" ),
+	},
+	feature_count: {
+		type: "number",
+		default: 1
+	},
+	features: {
+		type: "array",
+		default : features,
+	},
+	showFeature: {
+		type: "boolean",
+		default: true
+	},
+	headingTag: {
+		type: "string",
+		default: "h2"
 	},
 }
 
--- src/blocks/ratings/edit.js
+++ src/blocks/ratings/edit.js
@@ -9,6 +9,7 @@ import renderSVG from "../../../dist/blocks/uagb-controls/renderIcon"
 import FontIconPicker from "@fonticonpicker/react-fonticonpicker"
 import UAGBIcon from "../../../dist/blocks/uagb-controls/UAGBIcon.json"
 import UAGB_Block_Icons from "../../../dist/blocks/uagb-controls/block-icons"
+import times from "lodash/times"
 
 // Import all of our Text Options requirements.
 import TypographyControl from "../../components/typography"
@@ -26,6 +27,7 @@ const {
 	InspectorControls,
 	RichText,
 	ColorPalette,
+	MediaUpload,
 } = wp.blockEditor
 
 const {
@@ -33,6 +35,7 @@ const {
 	RangeControl,
 	SelectControl,
 	ToggleControl,
+	Button,
 } = wp.components
 
 const {
@@ -42,11 +45,21 @@ const {
 
 let svg_icons = Object.keys( UAGBIcon )
 
+let imageSizeOptions = [
+	{ value: "thumbnail", label: __( "Thumbnail" ) },
+	{ value: "medium", label: __( "Medium" ) },
+	{ value: "full", label: __( "Large" ) }
+]
+
 class UAGBInlineNoticeEdit extends Component {
 
 	constructor() {
 
 		super( ...arguments )
+
+		this.onRemoveImage = this.onRemoveImage.bind( this )
+		this.onSelectImage = this.onSelectImage.bind( this )
+		this.getImageSize  = this.getImageSize.bind( this )
 	}
 
 	componentDidMount() {
@@ -67,25 +80,205 @@ class UAGBInlineNoticeEdit extends Component {
 		}
 	}
 
+	savefeatures( value, index ) {
+			const { attributes, setAttributes } = this.props
+			const { features } = attributes
+
+		const newItems = features.map( ( item, thisIndex ) => {
+			if ( index === thisIndex ) {
+				item = { ...item, ...value }
+			}
+
+			return item
+		} )
+
+			setAttributes( {
+				features: newItems,
+			} )
+		}
+
+	/*
+	 * Event to set Image as null while removing.
+	 */
+	onRemoveImage() {
+		const { setAttributes } = this.props
+		setAttributes( { mainimage: null } )
+	}
+
+	/*
+	 * Event to set Image as while adding.
+	 */
+	onSelectImage( media ) {
+		const { setAttributes } = this.props
+
+		if ( ! media || ! media.url ) {
+			setAttributes( { mainimage: null } )
+			return
+		}
+
+		if ( ! media.type ) {
+			return
+		}
+
+		setAttributes( { mainimage: media } )
+		if ( media["sizes"] ) {
+			var new_img = this.getImageSize(media["sizes"])
+			imageSizeOptions = new_img
+		}
+	}
+
+	getImageSize(sizes) {
+		var size_arr = []
+		$.each(sizes, function (index, item) {
+		  var name = index
+		  	var p = { "value" : name, "label": name }
+		  	size_arr.push(p)
+		})
+		return(size_arr)
+	}
+
 	render() {
 
 		// Setup the attributes
 		const {
 			attributes: {
 				block_id,
-				rTitel,
+				rTitle,
 				rContent,
+				mainimage,
+				imgSize,
+				showFeature,
+				feature_count,
+				featuresTitle,
+				features,
+				headingTag,
 			},
 			setAttributes,
 			className,
 			attributes,
+			mergeBlocks,
 		} = this.props;
+
+		if( mainimage && mainimage["sizes"] ){
+			imageSizeOptions = this.getImageSize(mainimage["sizes"])
+		}
+
+			let url_chk = ''
+			let title = ''
+			if( "undefined" !== typeof attributes.mainimage  && null !== attributes.mainimage && "" !== attributes.mainimage ){
+				url_chk = attributes.mainimage.url
+				title = attributes.mainimage.title
+			}
+			
+			let url = ''
+			if( '' !== url_chk ){
+				let size = attributes.mainimage.sizes
+				let imageSize = attributes.imgSize
+
+				if ( "undefined" !== typeof size && "undefined" !== typeof size[imageSize] ) {
+				  url = size[imageSize].url 
+				}else{
+				  url = url_chk 
+				}
+		}
+
+		let image_icon_html = ''
+
+		if ( mainimage && mainimage.url ) {
+
+			image_icon_html = <img className="uagb-howto__source-image" src={url} title={title}/>
+
+		}
 
 
 		const ratingGeneralSettings = () => {
 			return (
 				<PanelBody title={ __( "General" ) } initialOpen={ true }>
-					
+					<h2>{ __( "Image" ) }</h2>
+					<MediaUpload
+						title={ __( "Select Image" ) }
+						onSelect={ ( value ) => setAttributes( { mainimage: value } ) }
+						allowedTypes={ [ "image" ] }
+						value={ mainimage }
+						render={ ( { open } ) => (
+							<Button isDefault onClick={ open }>
+								{ ! mainimage.url ? __( "Select Image" ) : __( "Replace image" ) }
+							</Button>
+						) }
+					/>
+					{ mainimage.url &&
+						<Button
+							className="uagb-rm-btn"
+							onClick={ () => setAttributes( { mainimage: '' } ) }
+							isLink isDestructive>
+							{ __( "Remove Image" ) }
+						</Button>
+					}
+					{ mainimage.url &&
+						<SelectControl
+							label={ __( "Size" ) }
+							options={ imageSizeOptions }
+							value={ imgSize }
+							onChange={ ( value ) => setAttributes( { imgSize: value } ) }
+						/>
+					}
+					<h2>{ __( "Primary Heading" ) }</h2>
+						<SelectControl
+							label={ __( "Tag" ) }
+							value={ headingTag }
+							onChange={ ( value ) => setAttributes( { headingTag: value } ) }
+							options={ [
+								{ value: "h1", label: __( "H1" ) },
+								{ value: "h2", label: __( "H2" ) },
+								{ value: "h3", label: __( "H3" ) },
+								{ value: "h4", label: __( "H4" ) },
+								{ value: "h5", label: __( "H5" ) },
+								{ value: "h6", label: __( "H6" ) },
+							] }
+					/>
+					<hr className="uagb-editor__separator" />
+					<ToggleControl
+						label={ __( "Show features" ) }
+						checked={ showFeature }
+						onChange={ ( value ) => setAttributes( { showFeature: ! showFeature } ) }
+						help={ __( "Note: This is recommended field for schema.It should be ON" ) }
+					/>
+					{ showFeature &&
+						<RangeControl
+							label={ __( "Number of Feature" ) }
+							value={ feature_count }
+							onChange={ newCount => {
+
+								let cloneIcons = [ ...features ]
+
+								if ( cloneIcons.length < newCount ) {
+
+									const incAmount = Math.abs( newCount - cloneIcons.length )
+
+									{ times( incAmount, n => {
+
+										cloneIcons.push( {
+											"feature_name": "- Feature Name." + ( cloneIcons.length + 1 ),
+										} )
+
+									} ) }
+
+									setAttributes( { features: cloneIcons } )
+								}else{
+									const incAmount = Math.abs( newCount - cloneIcons.length )
+									let data_new = cloneIcons
+					            for( var i= 0; i < incAmount; i++ ){
+					                data_new.pop()
+					            }
+					            setAttributes({features:data_new})
+
+								}
+								setAttributes( { feature_count: newCount } )
+							} }
+							min={ 1 }
+							max={ 50 }
+						/>
+						}
 				</PanelBody>
 			)
 		}
@@ -95,14 +288,14 @@ class UAGBInlineNoticeEdit extends Component {
 				<InspectorControls>
 					{ ratingGeneralSettings() }
 				</InspectorControls>
-				<div className={ classnames(
-					className,
-					"uagb-ratings__outer-wrap",
-					`uagb-block-${ block_id }`
-					) }
+			<div className={ classnames(
+				className,
+				"uagb-ratings__outer-wrap",
+				`uagb-block-${ block_id }`
+				) }
 				>
 				<RichText
-					tagName="h4"
+					tagName={ headingTag }
 					placeholder={ __( 'Review Title', 'ultimate-addons-for-gutenberg' ) }
 					keepPlaceholderOnFocus
 					value={ rTitle }
@@ -111,8 +304,65 @@ class UAGBInlineNoticeEdit extends Component {
 						setAttributes( { rTitle: value } )
 					}
 				/>
-					
+				<RichText
+					tagName="p"
+					placeholder={ __( 'Review Description', 'ultimate-addons-for-gutenberg' ) }
+					keepPlaceholderOnFocus
+					value={ rContent }
+					className='uagb-rating-desc'
+					onChange={ ( value ) =>
+						setAttributes( { rContent: value } )
+					}
+				/>
+				<div className="uagb-rating__source-wrap">{image_icon_html}</div>
+				<div className="uagb-rating__wrap">
+					{ showFeature &&
+					<RichText
+						tagName="h4"
+						placeholder={ __( "List Of Features:" ) }
+						value={ featuresTitle }
+						className='uagb-rating-feature-text'
+						onChange={ ( value ) => setAttributes( { featuresTitle: value } ) }
+						onMerge={ mergeBlocks }
+						unstableOnSplit={ this.splitBlock }
+						onnRemove={ () => onReplace( [] ) }
+					/>
+					}
+					{ showFeature &&
+					<div className="uagb-ratings-feature">
+								{
+								features.map( ( features, index ) => {
+
+										return (
+											<div
+												className={ classnames(
+													`uagb-rating-feature-${index}`,
+													"uagb-rating-feature-child__wrapper",
+												) }
+												key={ index }
+											>
+												<div className="uagb-features">
+													<RichText
+														tagName="div"
+														placeholder={ __( "Requirements Features:" ) }
+														value={ features.features_name }
+														onChange={ value => {
+																	this.savefeatures( { features_name: value }, index )
+																} }
+														className='uagb-rating-feature__label'
+														placeholder={ __( "Description" ) }
+														multiline={false}
+														allowedFormats={[ 'core/bold', 'core/italic', 'core/strikethrough' ]}
+													/>
+												</div>
+											</div>
+										)
+									})
+								}
+					</div>
+					}
 				</div>
+			</div>
 			
 			</Fragment>
 		)
