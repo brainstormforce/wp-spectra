b4a9914bd607ecac679acde082e5875331baba9f
Amit <amith@bsf.io>
2020-07-21 15:16:03 +0530

    [update] deprecated file update

--- src/blocks/price-list/deprecated.js
+++ src/blocks/price-list/deprecated.js
@@ -28,6 +28,61 @@ const deprecated = [
 				<Fragment>
 					<div className={ classnames(
 						className,
+						"uagb-rest_menu__outer-wrap"
+					) }
+					id = { `uagb-rm-${block_id}` }
+					>
+						{ rest_menu_item_arr.map( ( test, index ) =>
+
+							<div className = { classnames(
+								"uagb-rest_menu__wrap",
+								...PositionClasses( props.attributes ),
+							) } key ={ "wrap-"+index } >
+								<div className = "uagb-rm__content" key ={ "tm_content-"+index }>
+									{ (imagePosition == "top" || imagePosition == "left" ) && <RestMenuImage  attributes={props.attributes}  index_value = {index} /> }
+
+									<div className ="uagb-rm__text-wrap">
+										{
+											<Fragment>
+												<div className = "uagb-rm-details" key={"tm_wraps-"+index}>
+													<div className = "uagb-rm__title-wrap" key={"rm_title__wraps-"+index}>
+														<Title attributes={props.attributes} setAttributes = "not_set" props = { props } index_value = {index}/>
+														<div className = "uagb-rest-menu-text-wrap" key={"text-wrap-"+index}>
+															<Description attributes={props.attributes} setAttributes = "not_set" props = { props }  index_value = {index}/>
+														</div>
+													</div>
+													<div className = "uagb-rm__price-wrap" key={"rm_price__wraps-"+index}>
+														<Price attributes={props.attributes} setAttributes = "not_set" props = { props }  index_value = {index}/>
+													</div>
+												</div>
+											</Fragment>
+										}
+									</div>
+									{ ( imagePosition == "right" ) && <RestMenuImage  attributes={props.attributes}  index_value = {index} /> }
+								</div>
+								<div className="uagb-rm__separator-parent"><div className="uagb-rm__separator"></div></div>
+							</div>
+						)}
+					</div>
+				</Fragment>
+			)
+		},
+	},
+
+	{
+		attributes,
+		save: function( props ) {
+			const {
+				block_id,
+				className,
+				rest_menu_item_arr,
+				imagePosition,
+			} = props.attributes
+		
+			return (
+				<Fragment>
+					<div className={ classnames(
+						className,
 						"uagb-rest_menu__outer-wrap",
 						`uagb-block-${block_id}`
 					) }
