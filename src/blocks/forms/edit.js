44696437a4376db500515ef035ec8d256f1e4241
Amit <amith@bsf.io>
2020-07-28 15:39:54 +0530

    minor changes

--- src/blocks/forms/edit.js
+++ src/blocks/forms/edit.js
@@ -36,7 +36,7 @@ const {
 
 const { __ } = wp.i18n
 
-const ALLOWED_BLOCKS = [ "uagb/forms-name", "uagb/forms-email", "uagb/forms-email", "uagb/forms-phone", "uagb/forms-textarea" ]
+const ALLOWED_BLOCKS = [ "uagb/forms-name", "uagb/forms-email", "uagb/forms-hidden", "uagb/forms-phone", "uagb/forms-textarea" ]
 
 class UAGBFormsEdit extends Component {
 
