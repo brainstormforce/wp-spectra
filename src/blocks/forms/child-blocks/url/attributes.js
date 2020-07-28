e2bbefd5ed2ff96778ef5d1dd2cb8a08cc6b67d3
Amit <amith@bsf.io>
2020-07-28 13:11:46 +0530

    [ADD] URL child block

---
+++ src/blocks/forms/child-blocks/url/attributes.js
@@ -0,0 +1,14 @@
+const attributes = {
+    block_id: {
+		type: "string"
+    },
+    urlName: {
+        type: "string",
+        default: "URL Name",
+	},
+	urlRequired : {
+        type: "boolean",
+        default: false
+    }
+}
+export default attributes
