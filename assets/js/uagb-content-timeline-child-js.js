6c6d235e16743db7455271487a4a11c269dad155
AbhijitNage123 <abhijitn@bsf.io>
2020-08-04 09:53:28 +0530

    left right content related changes

--- assets/js/uagb-content-timeline-child-js.js
+++ assets/js/uagb-content-timeline-child-js.js
@@ -2,24 +2,39 @@
  * Common js file for timeline.
  */
  
+// console.log(document.querySelectorAll(".uagb-timeline__main .block-editor-block-list__layout"))
+
+let x = document.querySelectorAll(".uagb-timeline__main .block-editor-block-list__layout")
+
+document.querySelectorAll(".uagb-timeline__main .block-editor-block-list__layout")
+
+console.log(document.querySelectorAll(".uagb-timeline__main .block-editor-block-list__layout"))
 
 
-window.addEventListener(
-    'load', function () {	
+if ( x.length ){
+	
+}
 
-	var x = document.getElementsByClassName("uagb-timeline__widget")
 
-	console.log(document.getElementsByClassName("uagb-timeline__widget").length)
 
-	for( var i = 0; i <= document.getElementsByClassName("uagb-timeline__widget").length; i++){
-		console.log(document.getElementsByClassName("uagb-timeline__widget")[i])
-		if( i % 2 == 0 ){
-			console.log("even"+i)
-			document.getElementsByClassName("uagb-timeline__widget")[i].classList.remove("uagb-timeline__right").classList.add("uagb-timeline__left")
-		}else{
-			console.log("odd"+i)
-			document.getElementsByClassName("uagb-timeline__widget")[i].classList.remove("uagb-timeline__left").classList.add("uagb-timeline__right")
-		}
-	}
+// window.addEventListener(
+//     'load', function () {	
 
-});
+// 	var x = document.getElementsByClassName("uagb-timeline__widget")
+
+// 	console.log(document.getElementsByClassName("uagb-timeline__widget:nth-child(even)"))
+
+// 	for( var i = 0; i <= document.getElementsByClassName("uagb-timeline__widget").length; i++){
+		
+// 		if( i % 2 == 0 ){
+// 			console.log("even"+i)
+			
+// 			// document.getElementsByClassName("uagb-timeline__widget")[i].classList.remove("uagb-timeline__right").classList.add("uagb-timeline__left")
+// 		}else{
+// 			console.log("odd"+i)
+			
+// 			// document.getElementsByClassName("uagb-timeline__widget")[i].classList.remove("uagb-timeline__left").classList.add("uagb-timeline__right")
+// 		}
+// 	}
+
+// });
