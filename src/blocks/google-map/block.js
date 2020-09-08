2037fc86c02f99b8d5bca1842564fef6db5304f0
Amit <amith@bsf.io>
2020-09-08 12:06:43 +0530

    deprecated files

--- src/blocks/google-map/block.js
+++ src/blocks/google-map/block.js
@@ -258,6 +258,48 @@ registerBlockType( "uagb/google-map", {
 				}
 			},			
 			save: function( props ) {
+
+				const {
+					block_id,
+					height,
+					zoom,
+					address
+				} = props.attributes
+		
+				let encoded_address = encodeURI( address )
+		
+				let url = `https://www.google.com/maps/embed/v1/place?key=${api_key}&q=${encoded_address}&zoom=${zoom}`
+		
+				return (
+					<div className={ classnames( props.className, "uagb-google-map__wrap", `uagb-block-${block_id}` ) }>
+						<iframe
+							className="uagb-google-map__iframe"
+							title = { __( "Google Map for " + address ) }
+							src={url}
+							style={{height: height}}></iframe>
+					</div>
+				)
+			},
+		},
+		{
+			attributes : {
+				block_id: {
+					type: "string"
+				},
+				address: {
+					type: "string",
+					default: "Brainstorm Force"
+				},
+				height: {
+					type: "number",
+					default: 300
+				},
+				zoom: {
+					type: "number",
+					default: 12
+				}
+			},			
+			save: function( props ) {
 				const {
 					block_id,
 					height,
