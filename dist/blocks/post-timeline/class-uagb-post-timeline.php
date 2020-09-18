c4d3638306cb4d465449af7faa649fb4b5bf4816
Abhijit Babasaheb Nage <abhijitn@bsf.io>
2020-09-18 15:54:25 +0530

    added filter for post timeline icon

--- dist/blocks/post-timeline/class-uagb-post-timeline.php
+++ dist/blocks/post-timeline/class-uagb-post-timeline.php
@@ -616,9 +616,10 @@ if ( ! class_exists( 'UAGB_Post_Timeline' ) ) {
 		 * @param  array $attributes attribute array.
 		 */
 		public function get_icon( $attributes ) {
+			$icon = apply_filters( 'uagb_timeline_attribute_filter', $attributes );
 			?>
 			<div class = "uagb-timeline__marker uagb-timeline__out-view-icon" >
-				<span class = "uagb-timeline__icon-new uagb-timeline__out-view-icon" ><?php UAGB_Helper::render_svg_html( $attributes['icon'] ); ?></span>
+				<span class = "uagb-timeline__icon-new uagb-timeline__out-view-icon" ><?php UAGB_Helper::render_svg_html( $icon['icon'] ); ?></span>
 			</div>
 			<?php
 		}
