d01cf4573653de08780420eee013a6403a1366d1
Abhijit Babasaheb Nage <abhijitn@bsf.io>
2020-10-30 11:21:09 +0530

    added deprecated file

--- src/blocks/social-share-child/deprecated.js
+++ src/blocks/social-share-child/deprecated.js
@@ -56,6 +56,46 @@ const deprecated =[
 						</div>
 					)
 			},
+			save: props => {
+	
+				const { attributes, className } = props
+			
+				const {
+					type,
+					image_icon,
+					icon,
+					image,
+					block_id,
+					link,
+				} = props.attributes
+			
+				let url = links[type]
+			
+				let image_icon_html = ""
+			
+				if ( image_icon == "icon" ) {
+					if ( icon ) {
+						image_icon_html = <span className="uagb-ss__source-icon">{ renderSVG(icon) }</span>
+					}
+				} else {
+					if ( image && image.url ) {
+						image_icon_html = <img className="uagb-ss__source-image" src={image.url} />
+					}
+				}
+				
+				return (
+					<div
+						className={ classnames(
+							`uagb-ss-repeater`,
+							"uagb-ss__wrapper",
+							className,
+							`uagb-block-${ block_id }`
+						) }
+					>
+						<a className="uagb-ss__link" data-href={url} rel ="noopener noreferrer"><span className="uagb-ss__source-wrap">{image_icon_html}</span></a>
+					</div>
+				)
+			}
 		}
 	]
 
