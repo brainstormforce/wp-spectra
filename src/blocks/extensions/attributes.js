14c4b44e54a96619a582f0562aa5056c7bafc97e
Mangesh <mangeshs@bsf.io>
2020-12-28 15:28:25 +0530

    Added Controlles

--- src/blocks/extensions/attributes.js
+++ src/blocks/extensions/attributes.js
@@ -2,6 +2,27 @@ function addAttributes( settings ) {
 
 	if ( settings.attributes ) {
 		settings.attributes = Object.assign( settings.attributes, {
+			UAGBrowser: {
+				type:"string"
+			},
+			UAGSystem: {
+				type: "string"
+			},
+			DisplayConditions: {
+				type: "string"
+			},
+			UAGHideDesktop:{
+				type: "boolean",
+		        default: false
+			},
+        	UAGHideMob:{
+				type: "boolean",
+		        default: false
+			},
+        	UAGHideTab:{
+				type: "boolean",
+		        default: false
+			},
 			UAGloggedIn: {
 				type: "boolean",
 		        default: false
--- src/blocks/extensions/condition-block/index.js
+++ src/blocks/extensions/condition-block/index.js
@@ -1,5 +1,6 @@
 const {
-    ToggleControl
+    ToggleControl,
+    SelectControl
 } = wp.components
 
 
@@ -11,23 +12,104 @@ const UserConditionOptions = ( props ) => {
     const { Fragment } = wp.element;
     const { 
         UAGloggedIn,
-        UAGloggedOut
+        UAGloggedOut,
+        DisplayConditions,
+        UAGHideDesktop,
+        UAGHideMob,
+        UAGHideTab,
+        UAGSystem,
+        UAGBrowser
     } = attributes;
 
     return(
         <Fragment>
-            <ToggleControl
-				label={ __('Hide From Logged In Users')}
-				checked={UAGloggedIn}
-                help={ __( "This settings will only take effect once you are on the live page, and not while you're editing in Gutenberg." ) }
-                onChange={(value) => setAttributes({ UAGloggedIn: !attributes.UAGloggedIn })}
-			/>
-            <ToggleControl
-                label={ __('Hide From Logged Out Users' ) }
-                checked={UAGloggedOut}
-                help={ __( "This settings will only take effect once you are on the live page, and not while you're editing in Gutenberg." ) }
-                onChange={(value) => setAttributes({ UAGloggedOut: !attributes.UAGloggedOut })}
-            />
+            <SelectControl
+				label={ __( "Display Conditions" ) }
+				value={ DisplayConditions }
+				onChange={ ( value ) => setAttributes( { DisplayConditions: value } ) }
+				options={ [
+                    { value: "none", label: __( "None" ) },
+                    { value: "userstate", label: __( "User State" ) },
+                    { value: "responsiveVisibility", label: __( "Responsive Visibility" ) },
+                    { value: "userRole", label: __( "User Role" ) },
+                    { value: "browser", label: __( "Browser" ) },
+                    { value: "os", label: __( "Operting System" ) },
+                ]}
+			/> 
+            { DisplayConditions == "userstate" && 
+                <Fragment>
+                    <ToggleControl
+                        label={ __('Hide From Logged In Users')}
+                        checked={UAGloggedIn}
+                        help={ __( "This settings will only take effect once you are on the live page, and not while you're editing in Gutenberg." ) }
+                        onChange={(value) => setAttributes({ UAGloggedIn: !attributes.UAGloggedIn })}
+                    />
+                    <ToggleControl
+                        label={ __('Hide From Logged Out Users' ) }
+                        checked={UAGloggedOut}
+                        help={ __( "This settings will only take effect once you are on the live page, and not while you're editing in Gutenberg." ) }
+                        onChange={(value) => setAttributes({ UAGloggedOut: !attributes.UAGloggedOut })}
+                    />
+                </Fragment>
+            }
+            { DisplayConditions == "responsiveVisibility" && 
+                <Fragment>
+                    <ToggleControl
+                        label={ __('Hide on Desktop')}
+                        checked={UAGHideDesktop}
+                        help={ __( "This settings will only take effect once you are on the live page, and not while you're editing in Gutenberg." ) }
+                        onChange={(value) => setAttributes({ UAGHideDesktop: !attributes.UAGHideDesktop })}
+                    />
+                    <ToggleControl
+                        label={ __('Hide on Tablet' ) }
+                        checked={UAGHideTab}
+                        help={ __( "This settings will only take effect once you are on the live page, and not while you're editing in Gutenberg." ) }
+                        onChange={(value) => setAttributes({ UAGHideTab: !attributes.UAGHideTab })}
+                    />
+                    <ToggleControl
+                        label={ __('Hide on Mobile' ) }
+                        checked={UAGHideMob}
+                        help={ __( "This settings will only take effect once you are on the live page, and not while you're editing in Gutenberg." ) }
+                        onChange={(value) => setAttributes({ UAGHideMob: !attributes.UAGHideMob })}
+                    />
+                </Fragment>
+            }
+            { DisplayConditions == "os" && 
+                <Fragment>
+                    <SelectControl
+                        label={ __( "Operting System" ) }
+                        value={ UAGSystem }
+                        onChange={ ( value ) => setAttributes( { UAGSystem: value } ) }
+                        options={ [
+                            { value: 'iphone', label: __( "iPhone" ) },
+                            { value: "android", label: __( "Android" ) },
+                            { value: "windows", label: __( "Windows" ) },
+                            { value: "open_bsd", label: __( "OpenBSD" ) },
+                            { value: "sun_os", label: __( "SunOS" ) },
+                            { value: "linux", label: __( "Linux" ) },
+                            { value: "mac_os", label: __( "Mac OS" ) },
+                        ]}
+			        />
+                </Fragment>
+            }
+            { DisplayConditions == "browser" && 
+                <Fragment>
+                    <SelectControl
+                        label={ __( "Browser" ) }
+                        value={ UAGBrowser }
+                        onChange={ ( value ) => setAttributes( { UAGBrowser: value } ) }
+                        options={ [
+                            { value: 'ie', label: __( "Internet Explorer" ) },
+                            { value: "firefox", label: __( "Mozilla Firefox" ) },
+                            { value: "chrome", label: __( "Google Chrome" ) },
+                            { value: "opera_mini", label: __( "Opera Mini" ) },
+                            { value: "opera", label: __( "Opera" ) },
+                            { value: "safari", label: __( "Safari" ) },
+                            { value: "edge", label: __( "Microsoft Edge" ) },
+                        ]}
+			        />
+                </Fragment>
+            }
         </Fragment>
     );
 }
