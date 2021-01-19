3715ef13fe8df4ae40cd8ba8df8789918ad6b040
Abhijit Babasaheb Nage <abhijitn@bsf.io>
2021-01-19 11:00:19 +0530

    Added translatable

--- src/blocks/post/post-meta/save.js
+++ src/blocks/post/post-meta/save.js
@@ -1,22 +1,22 @@
-
+const { __ } = wp.i18n
 export default function save() {
     return <div className='uagb-post-grid-byline'>
             <span className='uagb-post__author'>
                 <span className="dashicons-admin-users dashicons"></span>
-                Author Name
+                { __( "Author Name" ) }
             </span>
 
             <time className='uagb-post__date'>
                 <span className="dashicons-calendar dashicons"></span>
-                Post Date
+                { __( "Post Date" ) }
             </time>
             <span className='uagb-post__comment' >
                 <span className="dashicons-admin-comments dashicons"></span>
-                Comments
+                { __( "Comments" ) }
             </span>
             <span className='uagb-post__taxonomy' >
                 <span className="dashicons-tag dashicons"></span>
-                Category
+                { __( "Category" ) }
             </span>	
         </div>
 
