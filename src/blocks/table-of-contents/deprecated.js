ecca56f1e1585b1f3aea4108df42d54e1317ad13
Abhijit Babasaheb Nage <abhijitn@bsf.io>
2021-01-28 17:41:10 +0530

    Added deprecated

--- src/blocks/table-of-contents/deprecated.js
+++ src/blocks/table-of-contents/deprecated.js
@@ -180,6 +180,66 @@ const deprecated = [
 				</div>
 			)
 		},
+	},
+	{
+		attributes,			
+		save: function( props ) {
+	
+			const { className } = props
+		
+			const {
+				align,
+				block_id,
+				tColumns,
+				heading,
+				headerLinks,
+				mappingHeaders,
+				smoothScroll,
+				makeCollapsible,
+				icon,
+				initialCollapse,
+				smoothScrollOffset,
+				smoothScrollDelay,
+			} = props.attributes
+		
+			let icon_html = ''
+		
+			if ( makeCollapsible && icon ) {
+				icon_html = (
+					<span className="uag-toc__collapsible-wrap">{renderSVG(icon)}</span>
+				)	
+			}
+		
+			return (
+		
+				<div className={ classnames(
+						className,
+						`uagb-toc__align-${align}`,
+						`uagb-toc__columns-${tColumns}`,
+						( initialCollapse ) ? `uagb-toc__collapse` : '',
+						`uagb-block-${ block_id }`
+					) }
+					data-scroll={smoothScroll}
+					data-offset={smoothScrollOffset}
+					data-delay={smoothScrollDelay}
+				>
+					<div className="uagb-toc__wrap">
+						<div className="uagb-toc__title-wrap">
+							<RichText.Content
+								value={ heading }
+								tagName='div'
+								className='uagb-toc__title'
+							/>
+							{icon_html}
+						</div>
+						<TableOfContents
+							mappingHeaders={mappingHeaders}
+							headers={headerLinks && JSON.parse(headerLinks)}
+						/>
+					</div>
+				</div>
+			)
+		},
 	}
 ]
 
