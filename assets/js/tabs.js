b6c39292afa68a36b005f16c99539074e7df0fe3
supriya jadhav <supriyaj@bsf.io>
2021-01-29 09:00:25 +0530

    removed alerts

--- assets/js/tabs.js
+++ assets/js/tabs.js
@@ -4,7 +4,6 @@
     jQuery('.uagb-tabs__wrap').each(function () {
        
         var activeTab = jQuery(this).data('tab-active');
-        var tabPanel = jQuery(this).find('.uagb-tabs-panel');
 
         var tab = jQuery(this).find('li.uagb-tab:not(".uagb-tabs__active")');
         if(jQuery(this).prop('id') !== '') {
@@ -13,23 +12,16 @@
         var tabs = jQuery(this).find('.uagb-tab');
         var bodyHeaders = jQuery(this).find('.uagb-tabs__body-header');
         var bodyContainers = jQuery(this).find('.uagb-tabs__body-container');
-        var bgColor = tab.css('background-color');
-        var borderColor = tab.css('border-color');
-        var borderWidth = tab.css('border-width');
-        var borderStyle = tab.css('border-style');
-        var borderRadius = tab.css('border-radius');
-        var textColor = tab.find('a').css('color');
 
         if(jQuery(this).prop('id') !== '') {
-            alert('dhsassd');
             jQuery( this ).find( ".uagb-tab a:not(.ui-tabs-anchor)" ).unbind( "click" );
             // Render tabs UI
             jQuery( this ).tabs( {
                 active: parseInt( activeTab ),
                 activate: function ( e, ui ) {
                     var newIdx = ui.newTab.index();
-                    bodyHeaders.removeClass( 'header-active' );
-                    bodyHeaders.eq( newIdx ).addClass( 'header-active' );
+                    bodyHeaders.removeClass( 'uagb-tabs__active' );
+                    bodyHeaders.eq( newIdx ).addClass( 'uagb-tabs__active' );
                 }
             } );
         } else {
@@ -51,25 +43,6 @@
         }
 
         bodyHeaders.eq(activeTab).addClass('header-active');
-        bodyHeaders.css({
-            backgroundColor: bgColor,
-            color: textColor,
-            borderColor: borderColor,
-            borderWidth: borderWidth,
-            borderStyle: borderStyle,
-            borderRadius: borderRadius
-        })
     });
 
-    jQuery('.uagb-tabs__body-header').click(function () {
-        var bodyContainer = jQuery(this).closest('.uagb-tabs__body-container');
-        var bodyWrapper = jQuery(this).closest('.uagb-tabs__body-wrapper');
-        var tabsWrapper = jQuery(this).closest('.uagb-tabs-wrapper');
-        var tabsPanel = tabsWrapper.find('.uagb-tabs-panel');
-        var idx = bodyContainer.index();
-
-        bodyWrapper.find('.uagb-tabs__body-header').removeClass('header-active');
-        jQuery(this).addClass('header-active');
-        tabsPanel.find('.uagb-tab').eq(idx).find('a').trigger('click');
-    })
 })( jQuery )
