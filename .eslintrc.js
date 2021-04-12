dcae59d43dc039125e948030c70bcde1e8331904
Rohit Patil <rohitp@bsf.io>
2021-04-12 17:38:42 +0530

    fix: JS linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,26 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "indent": [
-            "error",
-            "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "windows"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "never"
-        ]
-    }
+	env: {
+		browser: true,
+		commonjs: true,
+	},
+	"extends": [
+		"eslint:recommended",
+		"plugin:react/recommended"
+	],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 2018,
+		sourceType: 'module',
+	},
+	plugins: [ 'react' ],
+	rules: {
+		"react/react-in-jsx-scope": "off",
+		indent: [ 'error', 'tab' ],
+		'linebreak-style': [ 'error', 'windows' ],
+		quotes: [ 'error', 'double' ],
+		semi: [ 'error', 'never' ],
+	},
+	"globals": { "wp": true },
 };
