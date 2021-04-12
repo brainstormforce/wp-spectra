68ec721ac302588baf65464ef3b8cf37f19ad060
Sandesh <sandeshj@bsf.io>
2021-04-12 16:05:34 +0530

    build zip script added

--- bin/build-zip.sh
+++ bin/build-zip.sh
@@ -10,7 +10,7 @@ echo "BUILD_NAME=$BUILD_NAME"  >> $GITHUB_ENV
 echo "BUILD_VERSION=$BUILD_VERSION"  >> $GITHUB_ENV
 
 if [ ! -d "ultimate-addons-for-gutenberg" ]; then
-  mkdir "distultimate-addons-for-gutenberg"
+  mkdir "ultimate-addons-for-gutenberg"
 fi
 
 # if [ ! -d "artifact" ]; then
@@ -22,4 +22,8 @@ rsync -rc --exclude-from ".distignore" "./" "ultimate-addons-for-gutenberg"
 # cd ultimate-addons-for-gutenberg
 zip -r "ultimate-addons-for-gutenberg.zip" "./ultimate-addons-for-gutenberg"
 # cd -
+
+rm -rf ./ultimate-addons-for-gutenberg
+
+success "Done. Your UAG zip is ready..! 🎉"
 
