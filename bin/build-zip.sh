5bb4b57443a50b77b94fb18f18bb95ad16e83028
Sandesh <sandeshj@bsf.io>
2021-04-12 15:48:30 +0530

    build zip script added

---
+++ bin/build-zip.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+BUILD_VERSION=$(node -pe "require('./package.json').version")
+export BUILD_VERSION
+BUILD_NAME=$(node -pe "require('./package.json').name")
+export BUILD_NAME
+
+
+echo "BUILD_NAME=$BUILD_NAME"  >> $GITHUB_ENV
+echo "BUILD_VERSION=$BUILD_VERSION"  >> $GITHUB_ENV
+
+if [ ! -d "ultimate-addons-for-gutenberg" ]; then
+  mkdir "distultimate-addons-for-gutenberg"
+fi
+
+# if [ ! -d "artifact" ]; then
+#   mkdir "artifact"
+# fi
+
+rsync -rc --exclude-from ".distignore" "./" "ultimate-addons-for-gutenberg"
+
+cd ultimate-addons-for-gutenberg
+zip -r "ultimate-addons-for-gutenberg.zip" "./ultimate-addons-for-gutenberg/"
+cd -
+
