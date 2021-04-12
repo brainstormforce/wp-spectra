3d36f0313d20db9210f4c6166585a85e79ad17dd
Sandesh <sandeshj@bsf.io>
2021-04-12 16:22:11 +0530

    build zip script added

--- bin/build-zip.sh
+++ bin/build-zip.sh
@@ -1,5 +1,30 @@
 #!/usr/bin/env bash
 
+# Exit if any command fails.
+set -e
+
+# Enable nicer messaging for build status.
+BLUE_BOLD='\033[1;34m';
+GREEN_BOLD='\033[1;32m';
+RED_BOLD='\033[1;31m';
+YELLOW_BOLD='\033[1;33m';
+COLOR_RESET='\033[0m';
+error () {
+	echo -e "\n${RED_BOLD}$1${COLOR_RESET}\n"
+}
+status () {
+	echo -e "\n${BLUE_BOLD}$1${COLOR_RESET}\n"
+}
+success () {
+	echo -e "\n${GREEN_BOLD}$1${COLOR_RESET}\n"
+}
+warning () {
+	echo -e "\n${YELLOW_BOLD}$1${COLOR_RESET}\n"
+}
+
+
+status "💃 Time to build the plugin ZIP file 🕺"
+
 BUILD_VERSION=$(node -pe "require('./package.json').version")
 export BUILD_VERSION
 BUILD_NAME=$(node -pe "require('./package.json').name")
