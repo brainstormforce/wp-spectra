4a0e0aace7710fdd7a2a36782964d249ba77c9e4
Sandesh <sandeshj@bsf.io>
2021-04-15 12:19:40 +0530

    Updated strings, removed commented code

--- bin/build-zip.sh
+++ bin/build-zip.sh
@@ -25,30 +25,21 @@ warning () {
 
 status "💃 Time to build the plugin ZIP file 🕺"
 
-BUILD_VERSION=$(node -pe "require('./package.json').version")
-export BUILD_VERSION
-BUILD_NAME=$(node -pe "require('./package.json').name")
-export BUILD_NAME
-
-
-echo "BUILD_NAME=$BUILD_NAME"  >> $GITHUB_ENV
-echo "BUILD_VERSION=$BUILD_VERSION"  >> $GITHUB_ENV
-
-# if [ ! -d "ultimate-addons-for-gutenberg" ]; then
-#   mkdir "ultimate-addons-for-gutenberg"
-# fi
-
 if [ ! -d "artifact" ]; then
   mkdir "artifact"
 fi
 
+
+# Copy files for zip.
 rsync -rc --exclude-from ".distignore" "./" "artifact/ultimate-addons-for-gutenberg"
 
+# Go to directory
 cd artifact
-zip -r ultimate-addons-for-gutenberg.zip "./ultimate-addons-for-gutenberg"
-# cd -
 
+# Create a zip copied files.
+zip -r ultimate-addons-for-gutenberg.zip "./ultimate-addons-for-gutenberg"
+
+# Removed copied files folder.
 rm -rf ultimate-addons-for-gutenberg
 
 success "Done. Your UAG zip is ready..! 🎉"
-
