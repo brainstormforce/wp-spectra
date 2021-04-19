f41a3dc9fb49f87ccfb5d13f96ed01b19238cf84
RajkiranBagal <rajkiranb@bsf.io>
2021-04-19 13:44:00 +0530

    Combined the if conditions

--- assets/js/tabs.js
+++ assets/js/tabs.js
@@ -48,30 +48,28 @@ function tabClickEvent(e , tabName , list , count){
 
 }
 function anchorTabId() {
-	if ( window.location.hash != '' ) {
-		if ( /^#uagb-tabs__tab\d$/.test(window.location.hash)) {
-			var tabId = escape(window.location.hash.substring(1));
-            var tabPanel = document.querySelector('#' + tabId );
-                var tabNum = tabPanel.querySelector('a.uagb-tabs-list').getAttribute( 'data-tab' );
-                var listPanel = tabPanel.closest( '.uagb-tabs__panel' );
+	if ( window.location.hash != '' && /^#uagb-tabs__tab\d$/.test( window.location.hash ) ) {
+    var tabId = escape(window.location.hash.substring(1));
+    var tabPanel = document.querySelector('#' + tabId );
+        var tabNum = tabPanel.querySelector('a.uagb-tabs-list').getAttribute( 'data-tab' );
+        var listPanel = tabPanel.closest( '.uagb-tabs__panel' );
 
-                var oldActiveTab = listPanel.querySelector( '.uagb-tabs__active' );
-                oldActiveTab.classList.remove( 'uagb-tabs__active' );
+        var oldActiveTab = listPanel.querySelector( '.uagb-tabs__active' );
+        oldActiveTab.classList.remove( 'uagb-tabs__active' );
 
-                listPanel.querySelector( 'a.uagb-tabs-list' ).setAttribute( 
-                    'aria-selected', 'false'
-                );
-                tabPanel.classList.add( 'uagb-tabs__active' );
-                tabPanel.setAttribute( 'aria-selected', 'true' );
+        listPanel.querySelector( 'a.uagb-tabs-list' ).setAttribute( 
+            'aria-selected', 'false'
+        );
+        tabPanel.classList.add( 'uagb-tabs__active' );
+        tabPanel.setAttribute( 'aria-selected', 'true' );
 
-                var count = listPanel.getElementsByTagName("li").length;
-                var mainWrap = tabPanel.closest( '.uagb-tabs__wrap' );
-                for ( var idx = 0; idx < count; idx++ ) {
-                    mainWrap.classList.remove( 'uagb-active-tab-'+idx); 
-                }
-                mainWrap.classList.add('uagb-active-tab-'+tabNum)
-                mainWrap.querySelector( '.uagb-tabs__body-wrap .uagb-tabs__body-container:not(.uagb-inner-tab-' + tabNum + ')' ).setAttribute( 'aria-hidden', 'true' );
-                mainWrap.querySelector( '.uagb-tabs__body-wrap .uagb-inner-tab-' + tabNum ).setAttribute( 'aria-hidden', 'false' );
+        var count = listPanel.getElementsByTagName("li").length;
+        var mainWrap = tabPanel.closest( '.uagb-tabs__wrap' );
+        for ( var idx = 0; idx < count; idx++ ) {
+            mainWrap.classList.remove( 'uagb-active-tab-'+idx); 
         }
+        mainWrap.classList.add('uagb-active-tab-'+tabNum)
+        mainWrap.querySelector( '.uagb-tabs__body-wrap .uagb-tabs__body-container:not(.uagb-inner-tab-' + tabNum + ')' ).setAttribute( 'aria-hidden', 'true' );
+        mainWrap.querySelector( '.uagb-tabs__body-wrap .uagb-inner-tab-' + tabNum ).setAttribute( 'aria-hidden', 'false' );
 	}
 }
