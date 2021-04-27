b80e35ecdda53ed891029c7acef12e0304f30487
Rohit Patil <rohitp@bsf.io>
2021-04-27 10:45:12 +0530

    fix: removed unused package files and lintersrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,3 @@
 module.exports = {
-	env: {
-		browser: true,
-		commonjs: true,
-	},
-	"extends": [
-		"plugin:@wordpress/eslint-plugin/recommended",
-		"plugin:react/recommended"
-	],
-	parserOptions: {
-		ecmaFeatures: {
-			jsx: true,
-		},
-		ecmaVersion: 2018,
-		sourceType: 'module',
-	},
-	plugins: [ 'react' ],
-	rules: {
-		"react/react-in-jsx-scope": "off",
-		indent: [ 'error', 'tab' ],
-		"linebreak-style": ["error", "unix"],
-		quotes: [ 'error', 'double' ],
-		semi: [ 'error', 'always' ],
-	},
-	"globals": { "wp": true },
+	extends: [ 'plugin:@wordpress/eslint-plugin/recommended' ],
 };
