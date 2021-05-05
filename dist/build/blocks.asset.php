eaab33160f08e56382eb03cceda0059f5aa19a43
Rohit Patil <rohitp@bsf.io>
2021-05-05 12:55:09 +0530

    refactor: lottie

--- dist/build/blocks.asset.php
+++ dist/build/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '0d04a3a55a7d9fc0f5e549dc6c0a0bef');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '2d4821a75a76456ccb9d4f4f2e5243c8');
