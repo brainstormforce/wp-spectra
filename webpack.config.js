1ddae367659d2b50723f8482f3d092726bcf37be
Rohit Patil <rohitp@bsf.io>
2021-05-10 13:57:16 +0530

    refactor: Removed unused loadah code which was not using exactly

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
 const defaultConfig = require("@wordpress/scripts/config/webpack.config");
 const path = require( "path" );
 const CHUNK_DIR  = '/wp-content' + path.resolve( __dirname, 'dist/build' ).split('wp-content').pop() + '/';
+const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
 
 module.exports = {
 	...defaultConfig,
+	plugins:[
+		...defaultConfig.plugins,
+		new LodashModuleReplacementPlugin,
+		// new BundleAnalyzerPlugin,
+	],
 	entry: {
 		"blocks": path.resolve(
 			__dirname,
