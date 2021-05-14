1f69e41c129eb34049f1489db2e1d4999c537d5c
Rohit Patil <rohitp@bsf.io>
2021-05-14 13:30:21 +0530

    fix: Added alias for the components

--- dist/build/blocks.asset.php
+++ dist/build/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n', 'wp-polyfill'), 'version' => '8d8ea635a72df3a18edd4d3889a7f09b');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n', 'wp-polyfill'), 'version' => '38924b34cf431b4dbfdebb36856b3e53');
