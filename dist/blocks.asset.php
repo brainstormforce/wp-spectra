8b43d752723d3d72fbb909fb94937d44a5adf4fb
supriya jadhav <supriyaj@bsf.io>
2021-05-21 14:27:43 +0530

    Resolved stack  editor issue

--- dist/blocks.asset.php
+++ dist/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '5abe08eb46dc1ba135313243074774d9');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '59fca48ec7f53185661963ff19ae17c2');
