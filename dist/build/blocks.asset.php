851b36769524ea9ab44562534caf515db8ffff66
Rohit Patil <rohitp@bsf.io>
2021-05-24 12:59:59 +0530

    refactor: Removed build folder

--- dist/build/blocks.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-polyfill'), 'version' => 'ec7f175638955a469d596c7063c17d53');
