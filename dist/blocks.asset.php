c8c2eac80a1e83ac4d194259109a93ec3dd28a79
supriya jadhav <supriyaj@bsf.io>
2021-05-25 19:18:09 +0530

    Fixed phpcs issues

--- dist/blocks.asset.php
+++ dist/blocks.asset.php
@@ -1,4 +1,7 @@
-<?php return array(
+<?php
+// @codingStandardsIgnoreStart
+return array(
 	'dependencies' => array( 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill' ),
 	'version'      => 'c2daffef5a55eca8cc0799af6d9c6b21',
 );
+// @codingStandardsIgnoreEnd
