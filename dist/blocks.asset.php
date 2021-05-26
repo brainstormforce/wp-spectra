3c5fe0a09872f8eb16aff970e8bd9f956a7a0b80
supriya jadhav <supriyaj@bsf.io>
2021-05-26 16:51:44 +0530

    Resolved asset size reporter issue

--- dist/blocks.asset.php
+++ dist/blocks.asset.php
@@ -1,4 +1,1 @@
-<?php return array(
-	'dependencies' => array( 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill' ),
-	'version'      => '993736c8630ef1e387e1f055ad10e6ca',
-);
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '993736c8630ef1e387e1f055ad10e6ca');
