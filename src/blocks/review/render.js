64f15e1e4f27cbc69be1ed8a5cbf0b2fe8060180
supriya jadhav <supriyaj@bsf.io>
2021-06-03 15:31:15 +0530

    resolved block_id issue

--- src/blocks/review/render.js
+++ src/blocks/review/render.js
@@ -13,37 +13,35 @@ const Render = (
 	props = props.parentProps;
 	// Setup the attributes
 	const {
-		attributes,
+		attributes: {
+			block_id,
+			authorName,
+			itemName,
+			description,
+			parts,
+			starCount,
+			summaryTitle,
+			summaryDescription,
+			inactiveStarColor,
+			activeStarColor,
+			ctaLink,
+			ctaTarget,
+			rTitle,
+			rContent,
+			rAuthor,
+			headingTag,
+			mainimage,
+			imgSize,
+			showFeature,
+			showAuthor,
+			starOutlineColor,
+			enableDescription,
+			enableImage,
+		},
 		setAttributes,
 		isSelected,
-		className
+		className,
 	} = props;
-
-	const {
-		block_id,
-		authorName,
-		itemName,
-		description,
-		parts,
-		starCount,
-		summaryTitle,
-		summaryDescription,
-		inactiveStarColor,
-		activeStarColor,
-		ctaLink,
-		ctaTarget,
-		rTitle,
-		rContent,
-		rAuthor,
-		headingTag,
-		mainimage,
-		imgSize,
-		showFeature,
-		showAuthor,
-		starOutlineColor,
-		enableDescription,
-		enableImage,
-	} = attributes;
 	
 	let urlChk = '';
 	let title = '';
