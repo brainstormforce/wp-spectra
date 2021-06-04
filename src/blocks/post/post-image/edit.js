d9043629a0086f256a8296e93fa5b51bd303a4b8
Rohit Patil <rohitp@bsf.io>
2021-06-04 14:20:02 +0530

    fix: Linter change

--- src/blocks/post/post-image/edit.js
+++ src/blocks/post/post-image/edit.js
@@ -4,41 +4,45 @@ const { decodeEntities } = wp.htmlEntities;
 import { __ } from '@wordpress/i18n';
 import styles from './editor.lazy.scss';
 
-export const PostImage = (props) =>  {
-
+export const PostImage = ( props ) => {
 	// Add and remove the CSS on the drop and remove of the component.
-	useLayoutEffect(() => {
+	useLayoutEffect( () => {
 		styles.use();
 		return () => {
 			styles.unuse();
 		};
-	}, []);
+	}, [] );
 
-		const { post, attributes } = props
+	const { post, attributes } = props;
 
-		let target = ( attributes.newTab ) ? "_blank" : "_self"
+	const target = attributes.newTab ? '_blank' : '_self';
 
-		if (
-			attributes.displayPostImage &&
-			undefined !== post.uagb_featured_image_src &&
-			attributes.imgSize &&
-			post.uagb_featured_image_src[attributes.imgSize]
-		) {
-			var src = post.uagb_featured_image_src[attributes.imgSize]
+	if (
+		attributes.displayPostImage &&
+		undefined !== post.uagb_featured_image_src &&
+		attributes.imgSize &&
+		post.uagb_featured_image_src[ attributes.imgSize ]
+	) {
+		const src = post.uagb_featured_image_src[ attributes.imgSize ];
 
-			return (
-				<div className='uagb-post__image'>
-					<a href={ post.link } target={ target } rel ="noopener noreferrer">
-						<img
-							src={ src[0] }
-							alt={ decodeEntities( post.title.rendered.trim() ) || __( "(Untitled)" ) }
-						/>
-					</a>
-				</div>
-			)
-		} 
-
-			return null
-		
+		return (
+			<div className="uagb-post__image">
+				<a
+					href={ post.link }
+					target={ target }
+					rel="noopener noreferrer"
+				>
+					<img
+						src={ src[ 0 ] }
+						alt={
+							decodeEntities( post.title.rendered.trim() ) ||
+							__( '(Untitled)' )
+						}
+					/>
+				</a>
+			</div>
+		);
 	}
+
+	return null;
 };
