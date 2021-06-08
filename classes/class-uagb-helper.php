a9681c1e220f8f3e919b7eae97c8509492af923c
Sandesh <sandeshj@bsf.io>
2021-06-08 11:05:59 +0530

    Updated helper functions

--- classes/class-uagb-helper.php
+++ classes/class-uagb-helper.php
@@ -780,11 +780,10 @@ if ( ! class_exists( 'UAGB_Helper' ) ) {
 		 */
 		public static function get_uag_upload_dir_path() {
 
-			$wp_info  = wp_upload_dir( null, false );
-			$dir_name = 'uag-plugin';
+			$wp_info = self::get_upload_dir();
 
 			// Build the paths.
-			return trailingslashit( trailingslashit( $wp_info['basedir'] ) . $dir_name );
+			return $wp_info['path'];
 		}
 
 		/**
@@ -795,11 +794,10 @@ if ( ! class_exists( 'UAGB_Helper' ) ) {
 		 */
 		public static function get_uag_upload_url_path() {
 
-			$wp_info  = wp_upload_dir( null, false );
-			$dir_name = 'uag-plugin';
+			$wp_info = self::get_upload_dir();
 
 			// Build the paths.
-			return trailingslashit( trailingslashit( $wp_info['baseurl'] ) . $dir_name );
+			return $wp_info['url'];
 		}
 
 		/**
@@ -812,14 +810,12 @@ if ( ! class_exists( 'UAGB_Helper' ) ) {
 
 			$dir           = self::get_uag_upload_dir_path();
 			$wp_filesystem = uagb_filesystem();
-			$dir           = trailingslashit( $dir );
 			$filelist      = $wp_filesystem->dirlist( $dir, true );
 			$retval        = true;
 
 			if ( is_array( $filelist ) ) {
 
 				unset( $filelist['index.html'] );
-
 				unset( $filelist['custom-style-blocks.css'] );
 
 				foreach ( $filelist as $filename => $fileinfo ) {
