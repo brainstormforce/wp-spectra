b933ca3b975fda20fbb618a8a66b3894a0984790
Rajkiran Bagal <rajkiranbagal8585@gmail.com>
2021-06-10 16:55:34 +0530

    Fix: Table of Contents: Headings with other tags inside them are not visible on frontend.

--- blocks-config/table-of-content/class-uagb-table-of-content.php
+++ blocks-config/table-of-content/class-uagb-table-of-content.php
@@ -158,7 +158,7 @@ if ( ! class_exists( 'UAGB_Table_Of_Content' ) ) {
 			// Get all non-empty heading elements in the post content.
 			$headings = iterator_to_array(
 				$xpath->query(
-					'//*[self::h1 or self::h2 or self::h3 or self::h4 or self::h5 or self::h6][text()!=""]'
+					'//*[self::h1 or self::h2 or self::h3 or self::h4 or self::h5 or self::h6]'
 				)
 			);
 
