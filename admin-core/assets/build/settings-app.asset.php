6ad0090689acfba7504c6b58ae34930e85cc3911
Rajkiran Bagal <rajkiranbagal8585@gmail.com>
2021-07-01 11:36:01 +0530

    imp: Added SCSS variables for colors & tried using it

--- admin-core/assets/build/settings-app.asset.php
+++ admin-core/assets/build/settings-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'cbb93c01f037b1dd1ccbfa57d567bd45');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'e84b0a1fe123e1a27cb2adc07c835372');
