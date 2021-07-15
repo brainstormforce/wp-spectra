802ab98df0b60a3b1dc641a82c60d956c6853c10
RajkiranBagal <rajkiranb@bsf.io>
2021-07-15 18:31:15 +0530

    fix: Added styling to multibuttons

--- dist/blocks.asset.php
+++ dist/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'e507a5eeb62320194140e2ba176e43d1');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '29100e3587417324f4f1b60e224eedc8');
