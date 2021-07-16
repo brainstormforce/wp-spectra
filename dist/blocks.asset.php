90264a64499e81b6a377f548facd766b08002e4b
RajkiranBagal <rajkiranb@bsf.io>
2021-07-16 12:24:17 +0530

    fix: Toggle align

--- dist/blocks.asset.php
+++ dist/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '9f07334c0df0dd74edeec8b92a8f4c3f');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '3241885f995ec6c5e76f83487e62cd67');
