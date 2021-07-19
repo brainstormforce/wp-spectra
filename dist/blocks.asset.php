3f15fe49f4ba152979498ef5f7b3f14e9497f5e7
AbhijitNage123 <AbhijitNage123@users.noreply.github.com>
2021-07-19 12:29:32 +0000

    Version & build files updated

--- dist/blocks.asset.php
+++ dist/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => 'aa38af21611b3864ba47228a65e4581c');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => '7cc759c1000b18dbffa8a2ad69187dd8');
