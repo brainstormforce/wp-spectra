2245f2630dd9df737b636ccff2019a3fcbc3fce5
AbhijitNage123 <abhiitn@bsf.io>
2021-07-19 15:44:23 +0530

    Updated condition properly

--- dist/blocks.asset.php
+++ dist/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => '821c1cdb08168d7ea3856dc191971a27');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => 'aa38af21611b3864ba47228a65e4581c');
