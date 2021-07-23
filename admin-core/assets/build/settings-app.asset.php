47f143ca28312f5259fa328158f88f659e306815
supriya jadhav <supriyaj@bsf.io>
2021-07-23 09:51:31 +0530

    Updated btn UI

--- admin-core/assets/build/settings-app.asset.php
+++ admin-core/assets/build/settings-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '92efbb8fdbc16b4e6389c6434b16798c');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '163d65af9d03c63ba7e610b7cecf0f14');
