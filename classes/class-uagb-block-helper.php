ed707f4b4a5197caec7ceb7e7e53a02a7e8389af
AbhijitNage123 <abhiitn@bsf.io>
2021-07-27 15:10:28 +0530

    phpcbf

--- classes/class-uagb-block-helper.php
+++ classes/class-uagb-block-helper.php
@@ -925,7 +925,7 @@ if ( ! class_exists( 'UAGB_Block_Helper' ) ) {
 			$t_selectors = array();
 
 			$selectors = array(
-				' .uagb-heading-text'   => array(
+				' .uagb-heading-text' => array(
 					'text-align'    => $attr['headingAlign'],
 					'color'         => $attr['headingColor'],
 					'margin-bottom' => UAGB_Helper::get_css_value( $attr['headSpace'], 'px' ),
@@ -933,7 +933,7 @@ if ( ! class_exists( 'UAGB_Block_Helper' ) ) {
 				'.wp-block-uagb-advanced-heading .uagb-separator-wrap' => array(
 					'text-align' => $attr['headingAlign'],
 				),
-				' .uagb-desc-text'      => array(
+				' .uagb-desc-text'    => array(
 					'text-align' => $attr['headingAlign'],
 					'color'      => $attr['subHeadingColor'],
 				),
