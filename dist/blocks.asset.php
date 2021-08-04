0423bd169b75df738e39722cf67cbe264ce4a23c
RajkiranBagal <rajkiranb@bsf.io>
2021-08-04 15:21:47 +0530

    Added responsive icons styling & new svgs in Range & Spacing control

--- dist/blocks.asset.php
+++ dist/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-polyfill'), 'version' => '5be3f3869d069fb017deb11c8b418518');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-polyfill'), 'version' => '16f42885eaf5e4b79d195fc0df5a779e');
