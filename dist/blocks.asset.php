ecd277db278564abd69172ae870c93e2f05d133f
RajkiranBagal <rajkiranb@bsf.io>
2021-08-05 14:14:52 +0530

    Updated the Responsive control in Resp Slider & Fix CSS in Multibuttons

--- dist/blocks.asset.php
+++ dist/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-polyfill'), 'version' => 'e8c761e2d358e4712e3bac58a2063c82');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-polyfill'), 'version' => '4a7b507f2d61571136178dfc49ad701e');
