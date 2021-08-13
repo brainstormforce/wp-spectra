0e549aa335f30fada8c72ee267c94032fc3466ca
Sandesh <sandeshj@bsf.io>
2021-08-13 12:20:28 +0530

    Fixed sticky issue

--- dist/blocks.asset.php
+++ dist/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-polyfill'), 'version' => 'db26cf25d768644e29c66e00f03cb099');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-polyfill'), 'version' => '9ec55df3292f5fc865b71f4597d91d92');
