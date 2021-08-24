96b0930948623b0ff8c15d90568f27bf7eadb107
AbhijitNage123 <abhiitn@bsf.io>
2021-08-24 12:08:58 +0530

    Updated latest changes

--- dist/blocks.asset.php
+++ dist/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => '843a3d6e81c83aac238269ba9294a815');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => '2e3d92977fd21fe1f47ecf63a9e3b3d4');
