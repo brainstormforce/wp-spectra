14e806cc0fa62f02969db2ba7981b164e7b0ce04
supriya jadhav <supriyaj@bsf.io>
2021-08-27 15:10:56 +0530

    updated menu css

--- admin-core/assets/build/settings-app.asset.php
+++ admin-core/assets/build/settings-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'ad03e96b2e50fa2f0bd975936ab0e46b');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'd8798fad30ce8859c93dc7e1c6ab39af');
