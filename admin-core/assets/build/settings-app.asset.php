4a20ee3b73cdaa63cd2a1a4a6636905f2b5f6dc7
supriya jadhav <supriyaj@bsf.io>
2021-08-30 11:42:25 +0530

    updated font-size

--- admin-core/assets/build/settings-app.asset.php
+++ admin-core/assets/build/settings-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '64fe0e0b132b92af04bf4ec110157e50');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'bd21732f29a87d0b964ab712676fd043');
