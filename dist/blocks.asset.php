8d835a0d9a25195303d77ab94a38c3f5b5d8bd34
RajkiranBagal <rajkiranb@bsf.io>
2021-08-30 12:51:03 +0530

    fix: Styling issues in Controls UI/uX

--- dist/blocks.asset.php
+++ dist/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-polyfill'), 'version' => 'f93f479755501200a5aa7d2602764f4e');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-polyfill'), 'version' => '2667e8857c67bbcf98ae0f85bff2c7eb');
