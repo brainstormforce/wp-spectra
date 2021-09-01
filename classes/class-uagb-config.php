f5381271d92d5d4a14bbe37d461000746183adbc
Sandesh <sandeshj@bsf.io>
2021-09-01 16:14:30 +0530

    block data refactored

--- classes/class-uagb-config.php
+++ classes/class-uagb-config.php
@@ -49,6 +49,7 @@ if ( ! class_exists( 'UAGB_Config' ) ) {
 		public static function get_block_attributes() {
 
 			if ( null === self::$block_attributes ) {
+
 				self::$block_attributes = array(
 					'uagb/demo-control'           => array(
 						'slug'        => '',
@@ -3262,38 +3263,52 @@ if ( ! class_exists( 'UAGB_Config' ) ) {
 							'playOn'           => 'none',
 						),
 					),
-					'uagb/star-rating'            => array(
-						'slug'        => '',
-						'title'       => __( 'Star Rating', 'ultimate-addons-for-gutenberg' ),
-						'description' => __( 'This block helps you add Star Rating and customize it as required.', 'ultimate-addons-for-gutenberg' ),
-						'default'     => true,
-						'js_assets'   => array(),
-						'attributes'  => array(
-							'block_id'         => '',
-							'range'            => '5',
-							'rating'           => '4',
-							'size'             => '',
-							'align'            => 'left',
-							'layout'           => 'inline',
-							'gap'              => '',
-							'unmarkedColor'    => '#ccd6df',
-							'color'            => '#f0ad4e',
-							'fontFamily'       => 'Default',
-							'fontWeight'       => '',
-							'fontSubset'       => '',
-							'fontSizeType'     => 'px',
-							'fontSize'         => '',
-							'fontSizeMobile'   => '',
-							'fontSizeTablet'   => '',
-							'lineHeightType'   => 'em',
-							'lineHeight'       => '',
-							'lineHeightMobile' => '',
-							'lineHeightTablet' => '',
-							'titleColor'       => '',
-							'titleGap'         => '10',
-						),
-					),
+					// 'uagb/star-rating'            => array(
+					// 	'slug'        => '',
+					// 	'title'       => __( 'Star Rating', 'ultimate-addons-for-gutenberg' ),
+					// 	'description' => __( 'This block helps you add Star Rating and customize it as required.', 'ultimate-addons-for-gutenberg' ),
+					// 	'default'     => true,
+					// 	'js_assets'   => array(),
+					// 	'attributes'  => array(
+					// 		'block_id'         => '',
+					// 		'range'            => '5',
+					// 		'rating'           => '4',
+					// 		'size'             => '',
+					// 		'align'            => 'left',
+					// 		'layout'           => 'inline',
+					// 		'gap'              => '',
+					// 		'unmarkedColor'    => '#ccd6df',
+					// 		'color'            => '#f0ad4e',
+					// 		'fontFamily'       => 'Default',
+					// 		'fontWeight'       => '',
+					// 		'fontSubset'       => '',
+					// 		'fontSizeType'     => 'px',
+					// 		'fontSize'         => '',
+					// 		'fontSizeMobile'   => '',
+					// 		'fontSizeTablet'   => '',
+					// 		'lineHeightType'   => 'em',
+					// 		'lineHeight'       => '',
+					// 		'lineHeightMobile' => '',
+					// 		'lineHeightTablet' => '',
+					// 		'titleColor'       => '',
+					// 		'titleGap'         => '10',
+					// 	),
+					// ),
 				);
+
+				$block_files = glob( UAGB_DIR . 'includes/blocks/*/block.php' );
+
+				foreach( $block_files as $block_file ) {
+					$block_slug = '';
+					$block_data = array();
+
+					include( $block_file );
+
+					if ( ! empty( $block_slug ) && ! empty( $block_data ) ) {
+
+						self::$block_attributes[ $block_slug ] = $block_data;
+					}
+				}
 			}
 			return self::$block_attributes;
 		}
