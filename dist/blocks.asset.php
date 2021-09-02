a902f591d281e521bc48a3c9a550d3b167905e5a
RajkiranBagal <rajkiranb@bsf.io>
2021-09-02 19:10:55 +0530

    fix: Styling issues in UAGImage & Gutenberg ToggleControl

--- dist/blocks.asset.php
+++ dist/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-polyfill'), 'version' => 'f1acec47cd00f385ace009e348a3a941');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-polyfill'), 'version' => '96f3214f4e534875e3b3dbf471eb401a');
