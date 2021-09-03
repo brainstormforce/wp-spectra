696e5cdaf612b1b7106f27d03d6ac1df69e9e987
RajkiranBagal <rajkiranb@bsf.io>
2021-09-03 10:38:44 +0530

    fix: Added Tooltip support for Multibuttons control

--- dist/blocks.asset.php
+++ dist/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-polyfill'), 'version' => 'ad9f3d6a19b05e2d70cf425c6d8fad65');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-polyfill'), 'version' => '2e80b820a4aa4890d8bbb127b7125bd6');
