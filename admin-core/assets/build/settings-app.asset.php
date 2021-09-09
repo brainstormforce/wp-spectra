af5e1213fe857acec3f7d9ab665eb66c39db92f3
unknown <supriyaj@bsf.io>
2021-09-09 18:52:45 +0530

    updated button css

--- admin-core/assets/build/settings-app.asset.php
+++ admin-core/assets/build/settings-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '1c5f1914feb4840da0b7fd164b751b61');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '0c6036c016d1ef994894c4160b576d83');
