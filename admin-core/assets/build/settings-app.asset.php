9575cc405704bf0d0da30b715d3a5ef673b46034
RajkiranBagal <rajkiranb@bsf.io>
2021-09-14 15:43:52 +0530

    Fix: Label Spacing

--- admin-core/assets/build/settings-app.asset.php
+++ admin-core/assets/build/settings-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '37051808b65838964ce7a614fe574596');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '7daab64256c3615fc89e4768384f41e3');
