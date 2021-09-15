bcb35a8346f2a30d7445f622c73982d3d1165793
unknown <supriyaj@bsf.io>
2021-09-15 18:45:03 +0530

    updated category

--- classes/class-uagb-config.php
+++ classes/class-uagb-config.php
@@ -98,7 +98,7 @@ if ( ! class_exists( 'UAGB_Config' ) ) {
 						),
 					),
 					'uagb/columns'                => array(
-						'admin_categories' => array( 'extensions' ),
+						'admin_categories' => array( ),
 						'doc'              => 'advanced-columns',
 						'slug'             => '',
 						'link'             => 'advanced-columns',
@@ -2146,7 +2146,7 @@ if ( ! class_exists( 'UAGB_Config' ) ) {
 					'uagb/section'                => array(
 						'doc'              => 'section',
 						'slug'             => '',
-						'admin_categories' => array( 'extensions' ),
+						'admin_categories' => array( ),
 						'link'             => 'sections',
 						'title'            => __( 'Section', 'ultimate-addons-for-gutenberg' ),
 						'description'      => __( 'This block is an outer wrap section that allows you to add other blocks within it.', 'ultimate-addons-for-gutenberg' ),
