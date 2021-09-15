c8efa0c6666e12bce1f87e56b3a893b879b83293
AbhijitNage123 <abhiitn@bsf.io>
2021-09-15 18:26:48 +0530

    js lint post timeline fixes

--- dist/blocks.asset.php
+++ dist/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-polyfill'), 'version' => 'b74eca3eb8870e237fdfa2eeea0a74fe');
+<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-polyfill'), 'version' => '2ca2b5ff620bce7599f5440288b36b79');
