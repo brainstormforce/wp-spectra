78901efc49346eddcba0d5a5c4a8dcb7c1dd41ac
RajkiranBagal <rajkiranb@bsf.io>
2021-09-20 19:03:44 +0530

    fix: Updated Box Shadow to Typo & Box Shadow

--- dist/blocks.asset.php
+++ dist/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-polyfill'), 'version' => 'bee5785bc262b477a0e0d49ce192c182');
+<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-polyfill'), 'version' => '6e57cc2180aacf7994ce0aac6f24d7d8');
