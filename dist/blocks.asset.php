0f0d15fb1566fd1f3df22ab6be48ed55c24e96ab
RajkiranBagal <rajkiranb@bsf.io>
2021-09-21 09:38:12 +0530

    fix: Updated defaults for Blockquote

--- dist/blocks.asset.php
+++ dist/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-polyfill'), 'version' => '25c6b1c188b25ff6a8182fc5391d3088');
+<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-polyfill'), 'version' => '012c9ea9039eaee99ae5fbac62184f5b');
