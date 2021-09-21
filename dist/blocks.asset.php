05f5b1fac36be627444f772b97f6b1e5b3ff3b3c
RajkiranBagal <rajkiranb@bsf.io>
2021-09-21 12:36:18 +0530

    fix: Updated defaults for TOC & Social share

--- dist/blocks.asset.php
+++ dist/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-polyfill'), 'version' => '2f3464a4fa21b6fcfd2bfa367731b292');
+<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-polyfill'), 'version' => '3dc868a840d9a1b64365e43f3af5716e');
