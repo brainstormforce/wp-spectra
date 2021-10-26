f7ece090951a2bc9987f75cc45ddf3f43257162d
Abhijit Nage <47779685+AbhijitNage123@users.noreply.github.com>
2021-10-26 11:48:25 +0530

    Fix: Team block - Deprecation issue (#1104)
    * Added deprecated HTML for team block
    * Margin left/right auto issue in deprecatedion

--- dist/blocks.asset.php
+++ dist/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-polyfill'), 'version' => '9c819fbb1ca95c80303aa696ce37b108');
+<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-polyfill'), 'version' => '3933431841150ad83c0800ca293b1812');
--- src/blocks/team/edit.js
+++ src/blocks/team/edit.js
@@ -60,7 +60,7 @@ const UAGBTeam = ( props ) => {
 				props.setAttributes( { imageBottomMargin: imgBottomMargin } );
 			}
 		}
-
+	
 		if ( imgLeftMargin ) {
 			if ( ! imageLeftMargin ) {
 				props.setAttributes( { imageLeftMargin: imgLeftMargin } );
