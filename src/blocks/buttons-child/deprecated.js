f3561fe9dd32de6ba7f549f01be026f82b783235
Abhijit Nage <47779685+AbhijitNage123@users.noreply.github.com>
2021-12-16 15:26:44 +0530

    renderSVG missing in multi-buttons child deprecated file (#1166)

--- src/blocks/buttons-child/deprecated.js
+++ src/blocks/buttons-child/deprecated.js
@@ -6,6 +6,7 @@ import classnames from 'classnames';
 import attributes from './attributes';
 import { __ } from '@wordpress/i18n';
 import { RichText } from '@wordpress/block-editor';
+import renderSVG from '@Controls/renderIcon';
 
 const deprecated = [
 	{
