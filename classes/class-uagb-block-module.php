bec40fe0d3ed2ebc375d8235787efac427333b9f
RajkiranBagal <rajkiranb@bsf.io>
2022-01-14 18:28:40 +0530

    Added filter to add Pro Blocks in the Frontend Assest Blocks Array & Also Added functionality to set the Lite & Pro DIR files

--- classes/class-uagb-block-module.php
+++ classes/class-uagb-block-module.php
@@ -18,122 +18,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 class UAGB_Block_Module {
 
 	/**
-	 * Blocks
-	 *
-	 * @var array
-	 */
-	private static $blocks = array(
-		'star-rating'        => array(
-			'dir' => 'star-rating',
-		),
-		'advanced-heading'   => array(
-			'dir' => 'advanced-heading',
-		),
-		'columns'            => array(
-			'dir' => 'columns',
-		),
-		'column'             => array(
-			'dir' => 'column',
-		),
-		'blockquote'         => array(
-			'dir' => 'blockquote',
-		),
-		'call-to-action'     => array(
-			'dir' => 'call-to-action',
-		),
-		'cf7-styler'         => array(
-			'dir' => 'cf7-styler',
-		),
-		'content-timeline'   => array(
-			'dir' => 'content-timeline',
-		),
-		'faq'                => array(
-			'dir' => 'faq',
-		),
-		'gf-styler'          => array(
-			'dir' => 'gf-styler',
-		),
-		'how-to'             => array(
-			'dir' => 'how-to',
-		),
-		'how-to-step'        => array(
-			'dir' => 'how-to-step',
-		),
-		'icon-list'          => array(
-			'dir' => 'icon-list',
-		),
-		'icon-list-child'    => array(
-			'dir' => 'icon-list-child',
-		),
-		'info-box'           => array(
-			'dir' => 'info-box',
-		),
-		'inline-notice'      => array(
-			'dir' => 'inline-notice',
-		),
-		'marketing-button'   => array(
-			'dir' => 'marketing-button',
-		),
-		'buttons'            => array(
-			'dir' => 'buttons',
-		),
-		'buttons-child'      => array(
-			'dir' => 'buttons-child',
-		),
-		'post-carousel'      => array(
-			'dir' => 'post-carousel',
-		),
-		'post-grid'          => array(
-			'dir' => 'post-grid',
-		),
-		'post-masonry'       => array(
-			'dir' => 'post-masonry',
-		),
-		'post-timeline'      => array(
-			'dir' => 'post-timeline',
-		),
-		'restaurant-menu'    => array(
-			'dir' => 'restaurant-menu',
-		),
-		'review'             => array(
-			'dir' => 'review',
-		),
-		'section'            => array(
-			'dir' => 'section',
-		),
-		'social-share'       => array(
-			'dir' => 'social-share',
-		),
-		'social-share-child' => array(
-			'dir' => 'social-share-child',
-		),
-		'tabs'               => array(
-			'dir' => 'tabs',
-		),
-		'table-of-contents'  => array(
-			'dir' => 'table-of-contents',
-		),
-		'team'               => array(
-			'dir' => 'team',
-		),
-		'testimonial'        => array(
-			'dir' => 'testimonial',
-		),
-		'wp-search'          => array(
-			'dir' => 'wp-search',
-		),
-		'taxonomy-list'      => array(
-			'dir' => 'taxonomy-list',
-		),
-		'forms'              => array(
-			'dir' => 'forms',
-		),
-		'lottie'             => array(
-			'dir' => 'lottie',
-		),
-	);
-
-	/**
 	 * Get frontend Assets.
 	 *
 	 * @since 2.0.0
@@ -146,15 +30,167 @@ class UAGB_Block_Module {
 	 */
 	public static function get_frontend_assets( $slug, $attr, $id, $type = 'css' ) {
 
+		$blocks = apply_filters(
+			'uag_frontent_assets_blocks',
+			array(
+				'star-rating'        => array(
+					'dir'    => 'star-rating',
+					'is-pro' => false,
+				),
+				'advanced-heading'   => array(
+					'dir'    => 'advanced-heading',
+					'is-pro' => false,
+				),
+				'columns'            => array(
+					'dir'    => 'columns',
+					'is-pro' => false,
+				),
+				'column'             => array(
+					'dir'    => 'column',
+					'is-pro' => false,
+				),
+				'blockquote'         => array(
+					'dir'    => 'blockquote',
+					'is-pro' => false,
+				),
+				'call-to-action'     => array(
+					'dir'    => 'call-to-action',
+					'is-pro' => false,
+				),
+				'cf7-styler'         => array(
+					'dir'    => 'cf7-styler',
+					'is-pro' => false,
+				),
+				'content-timeline'   => array(
+					'dir'    => 'content-timeline',
+					'is-pro' => false,
+				),
+				'faq'                => array(
+					'dir'    => 'faq',
+					'is-pro' => false,
+				),
+				'gf-styler'          => array(
+					'dir'    => 'gf-styler',
+					'is-pro' => false,
+				),
+				'how-to'             => array(
+					'dir'    => 'how-to',
+					'is-pro' => false,
+				),
+				'how-to-step'        => array(
+					'dir'    => 'how-to-step',
+					'is-pro' => false,
+				),
+				'icon-list'          => array(
+					'dir'    => 'icon-list',
+					'is-pro' => false,
+				),
+				'icon-list-child'    => array(
+					'dir'    => 'icon-list-child',
+					'is-pro' => false,
+				),
+				'info-box'           => array(
+					'dir'    => 'info-box',
+					'is-pro' => false,
+				),
+				'inline-notice'      => array(
+					'dir'    => 'inline-notice',
+					'is-pro' => false,
+				),
+				'marketing-button'   => array(
+					'dir'    => 'marketing-button',
+					'is-pro' => false,
+				),
+				'buttons'            => array(
+					'dir'    => 'buttons',
+					'is-pro' => false,
+				),
+				'buttons-child'      => array(
+					'dir'    => 'buttons-child',
+					'is-pro' => false,
+				),
+				'post-carousel'      => array(
+					'dir'    => 'post-carousel',
+					'is-pro' => false,
+				),
+				'post-grid'          => array(
+					'dir'    => 'post-grid',
+					'is-pro' => false,
+				),
+				'post-masonry'       => array(
+					'dir'    => 'post-masonry',
+					'is-pro' => false,
+				),
+				'post-timeline'      => array(
+					'dir'    => 'post-timeline',
+					'is-pro' => false,
+				),
+				'restaurant-menu'    => array(
+					'dir'    => 'restaurant-menu',
+					'is-pro' => false,
+				),
+				'review'             => array(
+					'dir'    => 'review',
+					'is-pro' => false,
+				),
+				'section'            => array(
+					'dir'    => 'section',
+					'is-pro' => false,
+				),
+				'social-share'       => array(
+					'dir'    => 'social-share',
+					'is-pro' => false,
+				),
+				'social-share-child' => array(
+					'dir'    => 'social-share-child',
+					'is-pro' => false,
+				),
+				'tabs'               => array(
+					'dir'    => 'tabs',
+					'is-pro' => false,
+				),
+				'table-of-contents'  => array(
+					'dir'    => 'table-of-contents',
+					'is-pro' => false,
+				),
+				'team'               => array(
+					'dir'    => 'team',
+					'is-pro' => false,
+				),
+				'testimonial'        => array(
+					'dir'    => 'testimonial',
+					'is-pro' => false,
+				),
+				'wp-search'          => array(
+					'dir'    => 'wp-search',
+					'is-pro' => false,
+				),
+				'taxonomy-list'      => array(
+					'dir'    => 'taxonomy-list',
+					'is-pro' => false,
+				),
+				'forms'              => array(
+					'dir'    => 'forms',
+					'is-pro' => false,
+				),
+				'lottie'             => array(
+					'dir'    => 'lottie',
+					'is-pro' => false,
+				),
+			)
+		);
+
 		$assets = array();
 
 		if ( 'js' === $type ) {
 			$assets = '';
 		}
 
-		if ( isset( self::$blocks[ $slug ] ) ) {
+		if ( isset( $blocks[ $slug ] ) ) {
 
-			$block_dir = UAGB_DIR . 'includes/blocks/' . self::$blocks[ $slug ]['dir'];
+			$main_dir = $blocks[ $slug ]['is-pro'] ? UAG_PRO_DIR : UAGB_DIR;
+
+			$block_dir = $main_dir . 'includes/blocks/' . $blocks[ $slug ]['dir'];
 
 			$assets_file = $block_dir . '/frontend.' . $type . '.php';
 
@@ -176,5 +212,6 @@ class UAGB_Block_Module {
 		}
 
 		return $assets;
+
 	}
 }
