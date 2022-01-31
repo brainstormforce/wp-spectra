2ea947044516fa69014da15336082246279d68d8
Abhijit Nage <47779685+AbhijitNage123@users.noreply.github.com>
2022-01-31 12:35:39 +0530

    Fix: Timeline block - timeline not working properly when scroll (#1221)

--- assets/js/timeline.js
+++ assets/js/timeline.js
@@ -31,7 +31,7 @@ function uagbTimelineInit() {
 
 			lineOuter.style.bottom = timelineEndIcon?.offsetTop + 'px';
 			parentTop = lastItemTop - timelineStartIcon?.offsetTop;
-			lastItem = parentTop + timelineEndIcon?.offsetTop;
+			lastItem = parentTop + timelineEndIcon?.getBoundingClientRect().top;
 
 		} else if ( content.classList.contains( 'uagb-timeline__arrow-top' ) ) {
 
@@ -53,11 +53,11 @@ function uagbTimelineInit() {
 		const viewportHeight = document.documentElement.clientHeight;
 
 		const viewportHeightHalf = viewportHeight / 2 + connectorHeight;
-		const elementPos = content.offsetTop;
+		const elementPos = ( content.getBoundingClientRect().top + document.documentElement.scrollTop + content.offsetTop );
 
 		const newElementPos = elementPos + timelineStartIcon?.offsetTop;
 
-		let photoViewportOffsetTop = newElementPos - window.pageYOffset;
+		let photoViewportOffsetTop = newElementPos - document.documentElement.scrollTop;
 
 		if ( photoViewportOffsetTop < 0 ) {
 			photoViewportOffsetTop = Math.abs( photoViewportOffsetTop );
@@ -87,7 +87,7 @@ function uagbTimelineInit() {
 			elementEnd
 		) {
 			if ( 0 > photoViewportOffsetTop ) {
-				lineInner.style.height = ( viewportHeightHalf - Math.abs( photoViewportOffsetTop ) ) + 'px';
+				lineInner.style.height = ( viewportHeightHalf - Math.abs( photoViewportOffsetTop ) ) + 'px';	
 			} else {
 				lineInner.style.height = ( viewportHeightHalf + photoViewportOffsetTop ) + 'px';
 			}
@@ -113,8 +113,8 @@ function uagbTimelineInit() {
 			timelineIconPos = timelineIcon[j].lastElementChild.getBoundingClientRect().top + window.scrollY;
 			timelineCardPos = animateBorder[j].lastElementChild.getBoundingClientRect().top + window.scrollY;
 			
-			timelineIconTop = timelineIconPos - window.pageYOffset;
-			timelineCardTop = timelineCardPos - window.pageYOffset;
+			timelineIconTop = timelineIconPos - document.documentElement.scrollTop;
+			timelineCardTop = timelineCardPos - document.documentElement.scrollTop;
 
 			if ( ( timelineCardTop ) < ( viewportHeightHalf ) ) {
 				animateBorder[j].classList.remove( 'out-view' );
