b5b9ebc8e2c6d4aebb3def27be94b9faf77568db
Rajkiran Gajanan Bagal <47767536+RajkiranBagal@users.noreply.github.com>
2022-02-10 09:58:34 +0530

    improvement: Added the UAG Block categories above all other blocks. (#1281)

--- classes/class-uagb-init-blocks.php
+++ classes/class-uagb-init-blocks.php
@@ -46,9 +46,9 @@ class UAGB_Init_Blocks {
 		add_action( 'enqueue_block_editor_assets', array( $this, 'editor_assets' ) );
 
 		if ( version_compare( get_bloginfo( 'version' ), '5.8', '>=' ) ) {
-			add_filter( 'block_categories_all', array( $this, 'register_block_category' ), 10, 2 );
+			add_filter( 'block_categories_all', array( $this, 'register_block_category' ), 999999, 2 );
 		} else {
-			add_filter( 'block_categories', array( $this, 'register_block_category' ), 10, 2 );
+			add_filter( 'block_categories', array( $this, 'register_block_category' ), 999999, 2 );
 		}
 
 		add_action( 'wp_ajax_uagb_get_taxonomy', array( $this, 'get_taxonomy' ) );
@@ -394,13 +394,13 @@ class UAGB_Init_Blocks {
 	 */
 	public function register_block_category( $categories, $post ) {
 		return array_merge(
-			$categories,
 			array(
 				array(
 					'slug'  => 'uagb',
 					'title' => __( 'Ultimate Addons Blocks', 'ultimate-addons-for-gutenberg' ),
 				),
-			)
+			),
+			$categories
 		);
 	}
 
