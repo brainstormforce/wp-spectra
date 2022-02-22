b0a2126266d4deab40c06966b32ace40d733e16f
unknown <supriyaj@bsf.io>
2022-02-22 14:20:49 +0530

    resolved lint issues

--- blocks-config/uagb-controls/deprecatedRenderIcon.js
+++ blocks-config/uagb-controls/deprecatedRenderIcon.js
@@ -1,23 +1,24 @@
 /**
  * Set inline CSS class.
- * @param {object} props - The block object.
- * @return {array} The inline CSS class.
+ *
+ * @param {Object} props - The block object.
+ * @return {Array} The inline CSS class.
  */
 
- import UAGB_SVG_Icon from "./UAGBIcon"
- import parseSVG from "./parseIcon"
+ import UAGB_SVG_Icon from './UAGBIcon'
+ import parseSVG from './parseIcon'
 
  function renderSVG ( svg ) {
 
 	 svg = parseSVG( svg )
 
-	 var fontAwesome = UAGB_SVG_Icon[svg]
+	 const fontAwesome = UAGB_SVG_Icon[svg]
 
-	 if ( "undefined" != typeof fontAwesome ) {
+	 if ( 'undefined' !== typeof fontAwesome ) {
 
-		 var viewbox_array = ( fontAwesome["svg"].hasOwnProperty("brands") ) ? fontAwesome["svg"]["brands"]["viewBox"] : fontAwesome["svg"]["solid"]["viewBox"]
-		 var path = ( fontAwesome["svg"].hasOwnProperty("brands") ) ? fontAwesome["svg"]["brands"]["path"] : fontAwesome["svg"]["solid"]["path"]
-		 var viewBox = viewbox_array.join( " " )
+		 const viewbox_array = ( fontAwesome.svg.hasOwnProperty( 'brands' ) ) ? fontAwesome.svg.brands.viewBox : fontAwesome.svg.solid.viewBox
+		 const path = ( fontAwesome.svg.hasOwnProperty( 'brands' ) ) ? fontAwesome.svg.brands.path : fontAwesome.svg.solid.path
+		 const viewBox = viewbox_array.join( ' ' )
 
 		 return (
 			 <svg xmlns="http://www.w3.org/2000/svg" viewBox={viewBox}><path d={path}></path></svg>
