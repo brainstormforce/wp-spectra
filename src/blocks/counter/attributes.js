4ba4a28e482f43685fabae1814007d784e7938b3
Tushar Imran <tusharimran9909@gmail.com>
2022-02-28 23:20:01 +0600

    Counter Block - added general settings attribute

--- src/blocks/counter/attributes.js
+++ src/blocks/counter/attributes.js
@@ -2,6 +2,42 @@ const attributes = {
 	block_id: {
 		type: 'string',
 	},
+	startNumber: {
+		type: 'number',
+		default: 0,
+	},
+	endNumber: {
+		type: 'number',
+		default: 100,
+	},
+	numberPrefix: {
+		type: 'string',
+		default: '',
+	},
+	numberSuffix: {
+		type: 'string',
+		default: '',
+	},
+	animationDuration: {
+		type: 'number',
+		default: 2000,
+	},
+	thousandSeparator: {
+		type: 'string',
+		default: '',
+	},
+	heading: {
+		type: 'string',
+		default: '',
+	},
+	headingTag: {
+		type: 'string',
+		default: '',
+	},
+	layout: {
+		type: 'string',
+		default: 'default',
+	},
 };
 
 export default attributes;
--- src/blocks/counter/render.js
+++ src/blocks/counter/render.js
@@ -23,7 +23,18 @@ const Render = ( props ) => {
 
 	return (
 		<React.Fragment>
-			Render Mode
+			<div className={ classnames(
+				props.className,
+				`uagb-block-${ block_id }`,
+				'wp-block-uagb-counter'
+			) }>
+				<div className="wp-block-uagb-counter__number">
+					<span className="uagb-counter-block-prefix">Hello</span>
+					<span className="uagb-counter-block-number" data-duration="2000" data-to-value="10000" data-from-value="0" data-delimiter=".">10.000</span>
+					<span className="uagb-counter-block-suffix">World</span>
+				</div>
+				<div className="wp-block-uagb-counter__title">Cool Number</div>
+			</div>
 		</React.Fragment>
 	);
 }
