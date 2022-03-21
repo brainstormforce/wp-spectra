af986c34eaabe5a2b033b9986679c2e42136b753
unknown <supriyaj@bsf.io>
2022-03-21 19:35:29 +0530

    Resolved merge conflict

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '8a1ef798bab35f877c8385cde044123a');
-=======
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'b5df23b1d3ddc55efe2ef1d9aba1ac07');
->>>>>>> a8ff05819d651550de90136d095036d3982f8de4
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'c742e606ebf9e9075d2a107db5136a06');
