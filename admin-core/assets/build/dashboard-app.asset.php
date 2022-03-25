28b4259036caa33ffb39ef8195e3f4e852b3fdd1
RajkiranBagal <rajkiranb@bsf.io>
2022-03-25 13:21:02 +0530

    Added loading skeletons where necessary

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '394870c5ae6546b63a68befb293788b2');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'cd8b8db417b43639301fdea1f0fcfaa3');
