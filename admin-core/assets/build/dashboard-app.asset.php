995d8a724d4404649d4b208181b67feac8f1acf9
AbhijitNage123 <abhiitn@bsf.io>
2022-03-28 12:04:05 +0530

    Updated icon in admin

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'cb546e08150fa91303a336a3c346fe2c');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '7e321d0ba24c1ad5e7ab315675efea6d');
