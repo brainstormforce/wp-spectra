88d61599e8351f2f0453a17c869172173e362d3c
AbhijitNage123 <abhiitn@bsf.io>
2022-03-28 15:01:39 +0530

    Updated icon for responsive condition extention

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '6677ab53fa65d2fb130751d3154a4e37');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '7725a7fd88e8fbea426a0022c8e6310f');
