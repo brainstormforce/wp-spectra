0a5854c879fdc96a52bc116024130eaa6f23350e
RajkiranBagal <rajkiranb@bsf.io>
2022-03-29 18:29:44 +0530

    Adde hash links

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'a5fa4fb4983dd9458dcc1b946c9d55b4');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'cedb95b8de27dd8497fc86c7372cf0d0');
