869775f52a88458d089a2e3d783336b444f6231d
Tushar Imran <tusharimran9909@gmail.com>
2022-03-31 14:45:39 +0600

    dynamic content - production build

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '9c97c40630d3024201beb3c925bd25fa');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '5acc39cffd1a5c54670612acb54201cb');
