bda694f7b6f55a0358659a15d26a3b6329c17cc1
unknown <supriyaj@bsf.io>
2022-04-13 09:31:56 +0530

    Lint Fixes

--- src/components/responsive-icons/responsive-icons.js
+++ src/components/responsive-icons/responsive-icons.js
@@ -1,31 +1,31 @@
-document.addEventListener('load', spectra_responsive_icons);
-document.addEventListener('DOMContentLoaded', spectra_responsive_icons);
+document.addEventListener( 'load', spectra_responsive_icons );
+document.addEventListener( 'DOMContentLoaded', spectra_responsive_icons );
 import './style.scss'
 import DeviceIcons from './deviceIcons';
 
 function spectra_responsive_icons() {
-	wp.data.subscribe(function () {
-		setTimeout(function () {
+	wp.data.subscribe( function () {
+		setTimeout( function () {
 			spectra_responsive_icon();
-		}, 500);
-	});
+		}, 500 );
+	} );
 }
 
 function spectra_responsive_icon() {
-	if (!document.querySelector('.edit-post-header__settings')) {
+	if ( !document.querySelector( '.edit-post-header__settings' ) ) {
 		return null;
 	}
-	if (document.querySelector('.spectra-responsive-icons__wrap')) {
+	if ( document.querySelector( '.spectra-responsive-icons__wrap' ) ) {
 		return null;
 	}
 
-	let buttonWrapper = document.createElement('div');
-	buttonWrapper.classList.add('spectra-responsive-icons__wrap');
+	const buttonWrapper = document.createElement( 'div' );
+	buttonWrapper.classList.add( 'spectra-responsive-icons__wrap' );
 
-	document.querySelector('.edit-post-header__settings').insertBefore(buttonWrapper,document.querySelector('.edit-post-header__settings').firstChild);
+	document.querySelector( '.edit-post-header__settings' ).insertBefore( buttonWrapper,document.querySelector( '.edit-post-header__settings' ).firstChild );
 	wp.element.render(
 		<DeviceIcons />,
-		document.querySelector('.spectra-responsive-icons__wrap')
+		document.querySelector( '.spectra-responsive-icons__wrap' )
 	);
 }
 
