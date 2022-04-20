201c475aed5bfb2f6a3306e8e911600e73f59271
Zane Vaz <zanev@bsf.io>
2022-04-20 10:21:16 +0530

    Fixed Admin Icon Issues

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'e78db1c273dc9a78f4c973763ced0621');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '3ce75ee3d8268aca3ed2b262ad7b80aa');
