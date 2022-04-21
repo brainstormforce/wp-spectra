1c3f5472086f5a911ddb4a703308703f1e58e825
unknown <supriyaj@bsf.io>
2022-04-21 18:04:05 +0530

    Resolved admin video responsive issue

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '86c19c0340cda484acb6021ec280be3c');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '4509533873059c986b65846f6d1cbced');
