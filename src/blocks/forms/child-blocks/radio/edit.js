1d5645102ee62cbf5691dec0cf4b66382a88c8a9
unknown <supriyaj@bsf.io>
2022-04-25 00:58:23 +0530

    Resolved radio and checkbox issue

--- src/blocks/forms/child-blocks/radio/edit.js
+++ src/blocks/forms/child-blocks/radio/edit.js
@@ -14,17 +14,15 @@ const Render = lazy( () =>
 );
 
 const UAGBFormsRadioEdit = ( props ) => {
-	const [ setState ] = useState( {
-		optionsstate: [
-			{
-				optiontitle: __(
-					'Option Name 1',
-					'ultimate-addons-for-gutenberg'
-				),
-			},
-		],
-	} );
 
+	const [ state, setState ] = useState( { optionsstate: [
+		{
+			optiontitle: __(
+				'Option Name 1',
+				'ultimate-addons-for-gutenberg'
+			),
+		},
+	], } );
 	useEffect( () => {
 		const { setAttributes } = props;
 
