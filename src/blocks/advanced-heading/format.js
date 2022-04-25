421ecbdebc69243f5bebdaa3dd83baee7a1e3806
Tushar Imran <tusharimran9909@gmail.com>
2022-04-25 20:07:57 +0600

    Heading Block - Added highlight toolbar button tooltip

--- src/blocks/advanced-heading/format.js
+++ src/blocks/advanced-heading/format.js
@@ -1,6 +1,7 @@
 import { registerFormatType, toggleFormat } from '@wordpress/rich-text';
 import {BlockControls} from '@wordpress/block-editor';
-import { Toolbar, Button } from '@wordpress/components';
+import {__} from '@wordpress/i18n'
+import { Toolbar, Button, Tooltip } from '@wordpress/components';
 import { useSelect } from '@wordpress/data';
 
 const FORMAT_TYPE = 'uagb/advanced-heading-highlight'
@@ -17,18 +18,20 @@ function HighlightFormat( { isActive, onChange, value } ) {
 			{
 				selectedBlock && selectedBlock.name === 'uagb/advanced-heading' && attributes.headingColorType === 'classic' && (
 					<Toolbar className="uag-highlight-toolbar" label="Options">
-						<Button
-							isPrimary={isActive}
-							onClick={ () => {
-								onChange(
-									toggleFormat( value, {
-										type: FORMAT_TYPE,
-									} )
-								);
-							} }
-						>
-							<span className="dashicons  dashicons-admin-customizer"></span>
-						</Button>
+						<Tooltip text={__('Highlight Text', 'ultimate-addons-for-gutenberg')}>
+							<Button
+								isPrimary={isActive}
+								onClick={ () => {
+									onChange(
+										toggleFormat( value, {
+											type: FORMAT_TYPE,
+										} )
+									);
+								} }
+							>
+								<span className="dashicons  dashicons-admin-customizer"></span>
+							</Button>
+						</Tooltip>
 					</Toolbar>
 				)
 			}
