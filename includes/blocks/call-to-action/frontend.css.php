9288b168e310122fa00a1c336ab8b5f1b43ceda0
unknown <supriyaj@bsf.io>
2022-04-28 11:47:29 +0530

    Remove width on front-end

--- includes/blocks/call-to-action/frontend.css.php
+++ includes/blocks/call-to-action/frontend.css.php
@@ -398,26 +398,17 @@ if ( 'right' === $attr['ctaPosition'] && ( 'text' === $attr['ctaType'] || 'butto
 	$selectors[' .uagb-cta__content-right .uagb-cta__left-right-wrap .uagb-cta__content']      = array(
 		'width' => UAGB_Helper::get_css_value( $attr['contentWidth'], '%' ),
 	);
-	$selectors[' .uagb-cta__content-right .uagb-cta__left-right-wrap .uagb-cta__link-wrapper'] = array(
-		'width' => UAGB_Helper::get_css_value( ( 100 - $attr['contentWidth'] ), '%' ),
-	);
 	$selectors[' .uagb-cta__wrap'] = array(
 		'width' => UAGB_Helper::get_css_value( $attr['contentWidth'], '%' ),
 	);
 	$t_selectors[' .uagb-cta__content-right .uagb-cta__left-right-wrap .uagb-cta__content']      = array(
 		'width' => UAGB_Helper::get_css_value( $attr['contentWidthTablet'], '%' ),
 	);
-	$t_selectors[' .uagb-cta__content-right .uagb-cta__left-right-wrap .uagb-cta__link-wrapper'] = array(
-		'width' => UAGB_Helper::get_css_value( ( 100 - $attr['contentWidthTablet'] ), '%' ),
-	);
 	$t_selectors[' .uagb-cta__wrap'] = array(
 		'width' => UAGB_Helper::get_css_value( $attr['contentWidthTablet'], '%' ),
 	);
 	$m_selectors[' .uagb-cta__content-right .uagb-cta__left-right-wrap .uagb-cta__content']      = array(
 		'width' => UAGB_Helper::get_css_value( $attr['contentWidthMobile'], '%' ),
-	);
-	$m_selectors[' .uagb-cta__content-right .uagb-cta__left-right-wrap .uagb-cta__link-wrapper'] = array(
-		'width' => UAGB_Helper::get_css_value( ( 100 - $attr['contentWidthMobile'] ), '%' ),
 	);
 	$m_selectors[' .uagb-cta__wrap'] = array(
 		'width' => UAGB_Helper::get_css_value( $attr['contentWidthMobile'], '%' ),
@@ -475,15 +466,15 @@ if ( 'desktop' === $attr['stack'] ) {
 
 } elseif ( 'none' === $attr['stack'] ) {
 	$selectors['.wp-block-uagb-call-to-action  ']  = array(
-		'align-items'    => 'top' === $attr['buttonAlign'] ? 'flex-start' : 'center',
+		'align-items' => 'top' === $attr['buttonAlign'] ? 'flex-start' : 'center',
 		'flex-direction' => 'row',
 	);
 	$t_selectors['.wp-block-uagb-call-to-action '] = array(
-		'align-items'    => 'top' === $attr['buttonAlign'] ? 'flex-start' : 'center',
+		'align-items' => 'top' === $attr['buttonAlign'] ? 'flex-start' : 'center',
 		'flex-direction' => 'row',
 	);
 	$m_selectors['.wp-block-uagb-call-to-action '] = array(
-		'align-items'    => 'top' === $attr['buttonAlign'] ? 'flex-start' : 'center',
+		'align-items' => 'top' === $attr['buttonAlign'] ? 'flex-start' : 'center',
 		'flex-direction' => 'row',
 	);
 }
