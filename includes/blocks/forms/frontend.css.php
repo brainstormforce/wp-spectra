daf5d8e432f2001224705937caef7940c8133421
Zane Vaz <zanev@bsf.io>
2022-05-05 12:55:21 +0530

    Manually Fix the Remaining PHPCS Errors

--- includes/blocks/forms/frontend.css.php
+++ includes/blocks/forms/frontend.css.php
@@ -319,7 +319,7 @@ $m_selectors[' .uagb-forms-main-form  .uagb-forms-input'] = array(
 );
 $m_selectors[' .uagb-switch input:checked + .uagb-slider:before'] = array(
 	'transform' => 'translateX(' . UAGB_Helper::get_css_value(
-		(int)( ( ( ( 20 + $attr['toggleWidthSizeMobile'] ) * 2.5 ) - ( 20 + $attr['toggleWidthSizeMobile'] ) ) ),
+		(int) ( ( ( ( 20 + $attr['toggleWidthSizeMobile'] ) * 2.5 ) - ( 20 + $attr['toggleWidthSizeMobile'] ) ) ),
 		'px'
 	) . ')',
 );
@@ -379,20 +379,20 @@ if ( 'underlined' === $attr['formStyle'] ) {
 	);
 	$t_selectors[' .uagb-forms-main-form .uagb-switch']                                  = array(
 		'height' => UAGB_Helper::get_css_value(
-			(int)( 20 + $attr['toggleWidthSizeTablet'] + ( $attr['inputborderWidth'] * 2 ) + ( ( 20 + $attr['toggleWidthSizeTablet'] ) / 3 ) ),
+			(int) ( 20 + $attr['toggleWidthSizeTablet'] + ( $attr['inputborderWidth'] * 2 ) + ( ( 20 + $attr['toggleWidthSizeTablet'] ) / 3 ) ),
 			'px'
 		),
 		'width'  => UAGB_Helper::get_css_value(
-			(int)( ( ( 20 + $attr['toggleWidthSizeTablet'] ) * 2.5 ) + ( $attr['inputborderWidth'] * 2 ) + ( ( 20 + $attr['toggleWidthSizeTablet'] ) / 3 ) ),
+			(int) ( ( ( 20 + $attr['toggleWidthSizeTablet'] ) * 2.5 ) + ( $attr['inputborderWidth'] * 2 ) + ( ( 20 + $attr['toggleWidthSizeTablet'] ) / 3 ) ),
 			'px'
 		),
 	);
 	$t_selectors[' .uagb-forms-main-form .uagb-slider:before']                           = array(
 		'height' => UAGB_Helper::get_css_value( 20 + $attr['toggleWidthSizeTablet'], 'px' ),
 		'width'  => UAGB_Helper::get_css_value( 20 + $attr['toggleWidthSizeTablet'], 'px' ),
-		'top'    => UAGB_Helper::get_css_value( (int)( ( 20 + $attr['toggleWidthSizeTablet'] ) / 6 ), 'px' ),
-		'bottom' => UAGB_Helper::get_css_value( (int)( ( 20 + $attr['toggleWidthSizeTablet'] ) / 6 ), 'px' ),
-		'left'   => UAGB_Helper::get_css_value( (int)( ( 20 + $attr['toggleWidthSizeTablet'] ) / 6 ), 'px' ),
+		'top'    => UAGB_Helper::get_css_value( (int) ( ( 20 + $attr['toggleWidthSizeTablet'] ) / 6 ), 'px' ),
+		'bottom' => UAGB_Helper::get_css_value( (int) ( ( 20 + $attr['toggleWidthSizeTablet'] ) / 6 ), 'px' ),
+		'left'   => UAGB_Helper::get_css_value( (int) ( ( 20 + $attr['toggleWidthSizeTablet'] ) / 6 ), 'px' ),
 	);
 
 
@@ -422,20 +422,20 @@ if ( 'underlined' === $attr['formStyle'] ) {
 	);
 	$m_selectors[' .uagb-forms-main-form .uagb-switch']                                  = array(
 		'height' => UAGB_Helper::get_css_value(
-			(int)( 20 + $attr['toggleWidthSizeMobile'] + ( $attr['inputborderWidth'] * 2 ) + ( ( 20 + $attr['toggleWidthSizeMobile'] ) / 3 ) ),
+			(int) ( 20 + $attr['toggleWidthSizeMobile'] + ( $attr['inputborderWidth'] * 2 ) + ( ( 20 + $attr['toggleWidthSizeMobile'] ) / 3 ) ),
 			'px'
 		),
 		'width'  => UAGB_Helper::get_css_value(
-			(int)( ( ( 20 + $attr['toggleWidthSizeMobile'] ) * 2.5 ) + ( $attr['inputborderWidth'] * 2 ) + ( ( 20 + $attr['toggleWidthSizeMobile'] ) / 3 ) ),
+			(int) ( ( ( 20 + $attr['toggleWidthSizeMobile'] ) * 2.5 ) + ( $attr['inputborderWidth'] * 2 ) + ( ( 20 + $attr['toggleWidthSizeMobile'] ) / 3 ) ),
 			'px'
 		),
 	);
 	$m_selectors[' .uagb-forms-main-form .uagb-slider:before']                           = array(
 		'height' => UAGB_Helper::get_css_value( 20 + $attr['toggleWidthSizeMobile'], 'px' ),
 		'width'  => UAGB_Helper::get_css_value( 20 + $attr['toggleWidthSizeMobile'], 'px' ),
-		'top'    => UAGB_Helper::get_css_value( (int)( ( 20 + $attr['toggleWidthSizeMobile'] ) / 6 ), 'px' ),
-		'bottom' => UAGB_Helper::get_css_value( (int)( ( 20 + $attr['toggleWidthSizeMobile'] ) / 6 ), 'px' ),
-		'left'   => UAGB_Helper::get_css_value( (int)( ( 20 + $attr['toggleWidthSizeMobile'] ) / 6 ), 'px' ),
+		'top'    => UAGB_Helper::get_css_value( (int) ( ( 20 + $attr['toggleWidthSizeMobile'] ) / 6 ), 'px' ),
+		'bottom' => UAGB_Helper::get_css_value( (int) ( ( 20 + $attr['toggleWidthSizeMobile'] ) / 6 ), 'px' ),
+		'left'   => UAGB_Helper::get_css_value( (int) ( ( 20 + $attr['toggleWidthSizeMobile'] ) / 6 ), 'px' ),
 	);
 
 	$combined_selectors = array(
