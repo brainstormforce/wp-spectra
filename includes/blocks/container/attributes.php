849115ea4bad1e93322545faf3d08cf7054c0172
Antariksh Patre <antarikshpatre@gmail.com>
2022-05-06 16:52:53 +0530

    Added textColor attribute in attribute.php

--- includes/blocks/container/attributes.php
+++ includes/blocks/container/attributes.php
@@ -134,4 +134,5 @@ return array(
 	'backgroundVideoOpacity'         => 0.5,
 	'topInvert'                      => false,
 	'bottomInvert'                   => false,
+	'textColor'                      => 'inherit',
 );
