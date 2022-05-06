ddf526835441f103a773ea212a5e3b1e1c509d6c
RajkiranBagal <rajkiranb@bsf.io>
2022-05-06 13:20:47 +0530

    fix: typo tray issue

--- src/components/typography/index.js
+++ src/components/typography/index.js
@@ -33,8 +33,9 @@ const TypographyControl = ( props ) => {
 		window.addEventListener( 'click', function( e ){
 			const typoDiv = document.querySelector( '.uagb-typography-advanced' );
 			const actionsDiv = document.querySelector( '.uag-typography-button' );
+
 			if ( typoDiv ) {
-				if ( ! typoDiv.contains( e.target ) && ! actionsDiv.contains( e.target ) ){
+				if ( ! typoDiv.contains( e.target ) && ! actionsDiv.contains( e.target ) && ! e.target?.parentElement?.parentElement?.classList?.contains('uag-font-family-select__menu') && ! e.target?.classList?.contains('uag-responsive-common-button') ){
 					toggleAdvancedControls( false )
 				}
 			}
