f5b8393f0e0e34b6ddc1fadb7568080c442a37b8
AbhijitNage123 <abhiitn@bsf.io>
2022-05-12 19:28:37 +0530

    phpcbf & phpcs

--- includes/blocks/testimonial/frontend.js.php
+++ includes/blocks/testimonial/frontend.js.php
@@ -53,7 +53,7 @@ $selector      = $base_selector . $id;
 ?>
 jQuery( document ).ready( function() {
 	if( jQuery( '<?php echo esc_html( $selector ); ?>' ).length > 0 ){
-	jQuery( '<?php echo esc_html( $selector ); ?>' ).find( ".is-carousel" ).slick( <?php echo $settings; ?> );
+	jQuery( '<?php echo esc_html( $selector ); ?>' ).find( ".is-carousel" ).slick( <?php echo $settings; //phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?> );
 	}
 	var $scope = jQuery( '.uagb-block-<?php echo esc_html( $id ); ?>' );
 	var enableEqualHeight = ( '<?php echo esc_html( $equal_height ); ?>' );
