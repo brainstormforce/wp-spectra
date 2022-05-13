6711f0a9d06fb61193e437cdb9400add5c224db6
Zane Vaz <zanev@bsf.io>
2022-05-13 14:36:14 +0530

    SPCTR-606: Added Enable Title Toggle

--- src/blocks/star-rating/settings.js
+++ src/blocks/star-rating/settings.js
@@ -4,7 +4,7 @@ import TypographyControl from '@Components/typography';
 import WebfontLoader from '@Components/typography/fontloader';
 import { __ } from '@wordpress/i18n';
 import { InspectorControls } from '@wordpress/block-editor';
-import { Icon } from '@wordpress/components';
+import { Icon, ToggleControl } from '@wordpress/components';
 import InspectorTabs from '@Components/inspector-tabs/InspectorTabs.js';
 import InspectorTab, {
 	UAGTabs,
@@ -57,6 +57,7 @@ const Settings = ( props ) => {
 			fontStyle,
 			fontTransform,
 			fontDecoration,
+			displayTitle,
 		},
 	} = props;
 
@@ -147,6 +148,13 @@ const Settings = ( props ) => {
 
 	const generalSettings = (
 		<UAGAdvancedPanelBody title={ __( 'General', 'ultimate-addons-for-gutenberg' ) }>
+			<ToggleControl
+				label={ __( 'Enable Title', 'ultimate-addons-for-gutenberg' ) }
+				checked={ displayTitle }
+				onChange={ () =>
+					setAttributes( { displayTitle: ! displayTitle } )
+				}
+			/>
 			<MultiButtonsControl
 				setAttributes={ setAttributes }
 				label={ __( 'Range', 'ultimate-addons-for-gutenberg' ) }
@@ -368,7 +376,7 @@ const Settings = ( props ) => {
 					</InspectorTab>
 					<InspectorTab { ...UAGTabs.style }>
 						{ starStyling }
-						{ '' !== title && titleStyling }
+						{ ( displayTitle && '' !== title ) && titleStyling }
 					</InspectorTab>
 					<InspectorTab
 						{ ...UAGTabs.advance }
