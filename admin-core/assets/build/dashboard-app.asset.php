d989690135b468d82f78a07dfc7409815bc6d4bf
Zane Vaz <zanev@bsf.io>
2022-05-30 16:38:35 +0530

    SPCTR-926: Implemented Button Block Icons in Editor and Settings

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '1a5df31ae5bde22e5cd6e87b3d6b1386');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '4f9d29686f7638dff78cb8223a875f20');
