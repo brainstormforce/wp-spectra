f76e8eb78f154b4c10564c482bbe57850219c5e8
AbhijitNage123 <abhiitn@bsf.io>
2022-06-10 18:40:15 +0530

    Updated height to 36 to resolved the issue

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'da8b85bce717a0d4ddf4f9db678bdc29');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'bc31f8f911d7ceb410021648425c11a4');
