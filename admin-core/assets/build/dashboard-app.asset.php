7fb78a5cd0de95a9296ff71dafce70e9fd045a0f
jamyrbsf <jamyr@bsf.io>
2022-06-17 10:31:18 +0600

    SPCTR-1019 Lint fix

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'dbb57060444fb05bd439c9c29a396395');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'bc6ae30ce78a9d9afae103cd5ef0be99');
