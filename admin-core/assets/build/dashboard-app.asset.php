93adb22595e5ca809bef1061ababde9345e157bc
Sushma Kure <sushmak@bsf.io>
2022-06-23 19:28:09 +0530

    Updated Coming soon mode string

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '4321ed39270cf42f404d44df0a4bcc3d');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'bc2a9e3c987eb0841d1922b1aeb75992');
