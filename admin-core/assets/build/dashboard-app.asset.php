bf5e8d0f616f78f68144ce8dfaea6bdfa6a8b627
jamyrbsf <jamyr@bsf.io>
2022-06-27 14:50:56 +0600

    All svg fixed

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '0fbd4a4677c511c816f1e843e445b315');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '273072c23de2c720ab56824237f67d82');
