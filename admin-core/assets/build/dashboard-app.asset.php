a97247fc99b9bd1a62ad335a6604f4a7b330e126
Sushma Kure <sushmak@bsf.io>
2022-06-28 12:53:22 +0530

    Removed bottom border from settings on dashboard pages

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '1ead59dbc1df8a48dbe352cff780effb');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'f49b646a9ca96ee442555e778a4ffc2f');
