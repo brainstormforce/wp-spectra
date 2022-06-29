182fcf7b3e9e8a0db33bdaa00e6e56545c56cd0b
Supriya Jadhav <supriyaj@bsf.io>
2022-06-29 14:22:10 +0530

    updated 10 to 20 default value

--- src/blocks/container/attributes.js
+++ src/blocks/container/attributes.js
@@ -406,28 +406,28 @@ const attributes = {
 
 	topPaddingDesktop: {
 		type: 'number',
-		default: 10,
+		default: 20,
 		UAGCopyPaste: {
 			styleType: 'container-top-padding-desktop'
 		},
 	},
 	bottomPaddingDesktop: {
 		type: 'number',
-		default: 10,
+		default: 20,
 		UAGCopyPaste: {
 			styleType: 'container-bottom-padding-desktop'
 		},
 	},
 	leftPaddingDesktop: {
 		type: 'number',
-		default: 10,
+		default: 20,
 		UAGCopyPaste: {
 			styleType: 'container-left-padding-desktop'
 		},
 	},
 	rightPaddingDesktop: {
 		type: 'number',
-		default: 10,
+		default: 20,
 		UAGCopyPaste: {
 			styleType: 'container-right-padding-desktop'
 		},
