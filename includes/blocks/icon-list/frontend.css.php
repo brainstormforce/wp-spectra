bc74b04c48930e319b62f12b12c44fde39e7e710
jamyrbsf <jamyr@bsf.io>
2022-07-04 20:29:12 +0600

    soctr-1244 fixed margin for last child

--- includes/blocks/icon-list/frontend.css.php
+++ includes/blocks/icon-list/frontend.css.php
@@ -244,10 +244,6 @@ if ( 'horizontal' === $icon_layout ) {
 		$t_selectors[' .uagb-icon-list__wrap']['flex-direction'] = 'column';
 		$t_selectors[' .uagb-icon-list__wrap']['align-items']    = $alignment_tablet;
 
-		$t_selectors[' .uagb-icon-list__wrap .wp-block-uagb-icon-list-child:last-child'] = array(
-			'margin-bottom' => 0,
-		);
-
 	} elseif ( 'mobile' === $attr['stack'] ) {
 
 		$m_selectors[' .uagb-icon-list__wrap .wp-block-uagb-icon-list-child'] = array(
@@ -258,10 +254,6 @@ if ( 'horizontal' === $icon_layout ) {
 
 		$m_selectors[' .uagb-icon-list__wrap']['flex-direction'] = 'column';
 		$m_selectors[' .uagb-icon-list__wrap']['align-items']    = $alignment_mobile;
-
-		$m_selectors[' .uagb-icon-list__wrap .wp-block-uagb-icon-list-child:last-child'] = array(
-			'margin-bottom' => 0,
-		);
 	}
 	$selectors['.wp-block-uagb-icon-list .wp-block-uagb-icon-list-child'] = array(
 		'margin-left'  => UAGB_Helper::get_css_value( ( $gap_fallback / 2 ), $attr['gapType'] ),
@@ -272,9 +264,6 @@ if ( 'horizontal' === $icon_layout ) {
 	$selectors['.wp-block-uagb-icon-list .wp-block-uagb-icon-list-child:first-child'] = array(
 		'margin-left' => 0,
 	);
-	$selectors['.wp-block-uagb-icon-list .wp-block-uagb-icon-list-child:last-child']  = array(
-		'margin-right' => 0,
-	);
 } elseif ( 'vertical' === $icon_layout ) {
 	$selectors[' .uagb-icon-list__wrap']['flex-direction'] = 'column';
 
@@ -282,9 +271,6 @@ if ( 'horizontal' === $icon_layout ) {
 		'margin-left'   => 0,
 		'margin-right'  => 0,
 		'margin-bottom' => UAGB_Helper::get_css_value( $gap_fallback, $attr['gapType'] ),
-	);
-	$selectors['.wp-block-uagb-icon-list .wp-block-uagb-icon-list-child:last-child'] = array(
-		'margin-bottom' => 0,
 	);
 }
 
