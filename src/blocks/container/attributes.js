bccb71766d80a6ba6b9ccee8238ad638ae3b1b7a
Sushma Kure <sushmak@bsf.io>
2022-07-21 17:30:54 +0530

    Added support to Copy Paste general style controls in container

--- src/blocks/container/attributes.js
+++ src/blocks/container/attributes.js
@@ -150,13 +150,22 @@ const attributes = {
 	wrapDesktop: {
 		type: 'string',
 		default: 'nowrap',
+		UAGCopyPaste: {
+			styleType: 'container-wrap-desktop'
+		},
 	},
 	wrapTablet: {
 		type: 'string',
+		UAGCopyPaste: {
+			styleType: 'container-wrap-tablet'
+		},
 	},
 	wrapMobile: {
 		type: 'string',
 		default: 'wrap',
+		UAGCopyPaste: {
+			styleType: 'container-wrap-mobile'
+		},
 	},
 	alignContentDesktop: {
 		type: 'string',
