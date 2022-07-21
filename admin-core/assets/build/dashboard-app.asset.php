59decdd7ff595352c42a7a7be4e675108f46326a
Tushar Imran <tusharimran9909@gmail.com>
2022-07-21 13:39:39 +0600

    Dynamic Content - production build admin settings

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '002aacadfd848083c12321b651f00048');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '5fdf421049ddd154e1d17fd967a9aa03');
