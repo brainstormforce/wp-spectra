d037d7adae5df778f421cbd0da4b99bd31f9464a
RajkiranBagal <rajkiranb@bsf.io>
2022-07-22 12:21:26 +0530

    resolved conflicts lint

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '002aacadfd848083c12321b651f00048');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '06a3d63e8c05daec8b1a84f7801bbc36');
