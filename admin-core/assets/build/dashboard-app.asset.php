845b4df2112ce9e0f4a504060f340048303b8f33
Tushar Imran <tusharimran9909@gmail.com>
2022-07-26 17:32:38 +0600

    Resolved Admin Conflicts

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '5fdf421049ddd154e1d17fd967a9aa03');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '2821375408d0f3d21e4f68b3e4cc666b');
