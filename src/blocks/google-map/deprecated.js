72f942df5b9de3d77cc207793efb94c55e99d0f6
jamyrbsf <jamyr@bsf.io>
2022-08-03 14:23:14 +0600

    spctr-1509 fix google maps issue

--- src/blocks/google-map/deprecated.js
+++ src/blocks/google-map/deprecated.js
@@ -130,6 +130,56 @@ const deprecated = [
 			);
 		},
 	},
+	{
+		attributes: {
+			block_id: {
+				type: "string"
+			},
+			address: {
+				type: "string",
+				default: "Brainstorm Force"
+			},
+			height: {
+				type: "number",
+				default: 300
+			},
+			zoom: {
+				type: "number",
+				default: 12
+			},
+			language: {
+				type: "string",
+				default: "en",
+			},
+		},
+		save ( props ) {
+
+			const {
+				block_id,
+				height,
+				zoom,
+				address,
+				language
+			} = props.attributes
+
+			let encoded_address = encodeURI( address )
+
+			var lang_par = (language) ? language : "en";
+
+			let url = `https://www.google.com/maps/embed/v1/place?key=${wp.uagb_google_api_key}&q=${encoded_address}&zoom=${zoom}&language=${lang_par}`
+
+			return (
+				<div className={ classnames( props.className, "uagb-google-map__wrap", `uagb-block-${block_id}` ) }>
+					<iframe
+						className="uagb-google-map__iframe"
+						title = { __( "Google Map for " + address ) }
+						src={url}
+						style={{height: height}}></iframe>
+				</div>
+			)
+		}
+
+	},
 ];
 
 export default deprecated;
