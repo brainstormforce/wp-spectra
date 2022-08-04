280bea800ec739fbf3b031bf00dbbaabfe79920a
Zane Vaz <zanev@bsf.io>
2022-08-04 14:18:52 +0530

    SPCTR-1538: Updated Company Name Placeholder Generation

--- src/blocks/testimonial/settings.js
+++ src/blocks/testimonial/settings.js
@@ -1211,10 +1211,7 @@ const Settings = ( props ) => {
 												description:
 													'I have been working with these guys since years now! With lots of hard work and timely communication they made sure they delivered the best to me. Highly recommended!',
 												name: 'John Doe',
-												company:
-													'Company' +
-													( cloneTest_block.length +
-														1 ),
+												company: 'Company Name',
 												image: '',
 											} );
 										}
