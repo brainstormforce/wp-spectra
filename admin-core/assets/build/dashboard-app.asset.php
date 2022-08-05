8e48f2a1bbfeb772aa752bff09867f2e592bd7c0
Zane Vaz <zanev@bsf.io>
2022-08-05 17:31:40 +0530

    SPCTR-1285: Updated Rollback Popup

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'ffcc0f96a1ac8280277355e1872acf7f');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'a6f1933affa465213e9e1c9ca32c8147');
