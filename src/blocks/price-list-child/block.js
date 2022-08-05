51888da6d5d35f7607ba2bcf0363b8b522b585b3
Zane Vaz <zanev@bsf.io>
2022-08-05 22:34:16 +0530

    SPCTR-1532: Added and Implemented Child Block Icons

--- src/blocks/price-list-child/block.js
+++ src/blocks/price-list-child/block.js
@@ -16,7 +16,7 @@ registerBlockType( 'uagb/restaurant-menu-child', {
 	title: __( 'Price List-Child', 'ultimate-addons-for-gutenberg' ), // Block title.
 	description:
 	__( 'This block allows you to add attractive Price List.', 'ultimate-addons-for-gutenberg' ), // Block description.
-	icon: UAGB_Block_Icons.restaurant_menu, // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
+	icon: UAGB_Block_Icons.restaurant_menu_child, // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
 	keywords: [ __( 'pricelist' ), __( 'menu' ), __( 'uag' ) ],
 	supports: {
 		anchor: true,
