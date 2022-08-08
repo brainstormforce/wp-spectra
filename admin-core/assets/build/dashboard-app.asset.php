72079acd968c67eefe07e1700bf3f7aafdcfd199
RajkiranBagal <rajkiranb@bsf.io>
2022-08-08 22:26:45 +0530

    resolved conflicts

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '202c5d870e0fe243346d68a474de642f');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '63d97c28d4a620ad831fd889d31ce9c3');
