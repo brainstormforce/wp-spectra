43af8a8c25c5fb59f616c92717992b083377485a
Zane Vaz <zanev@bsf.io>
2022-08-08 13:23:04 +0530

    SPCTR-1532: Linted

--- blocks-config/uagb-controls/block-icons.js
+++ blocks-config/uagb-controls/block-icons.js
@@ -424,7 +424,7 @@ const UAGB_Block_Icons = {
 			strokeLinejoin: 'round',
 		} ),
 	),
-	'columns': el(
+	columns: el(
 		'svg',
 		{ width: 24, height: 24, viewBox: '0 0 24 24', fill: 'none', className: 'uagb-editor-icons' },
 		el( 'path', {
