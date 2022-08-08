b2133236c3a90985afa762649e45c5750d2bad92
Zane Vaz <zanev@bsf.io>
2022-08-08 12:03:58 +0530

    SPCTR-1532: Updated Deprecated Block Icons

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '469f32bda1412004c0ed2fd1333bb914');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '152fb23c874937cba4ee79b2e7787e35');
