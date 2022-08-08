bc9f5c2127eba04afacc3d5c409fc533849d4f9a
Antariksh Patre <antarikshpatre@gmail.com>
2022-08-08 17:42:46 +0530

    SPCTR-1581 Price List Child- Added code to retrieve showImage value from parent-block and set it for the child block

--- src/blocks/price-list-child/edit.js
+++ src/blocks/price-list-child/edit.js
@@ -4,6 +4,7 @@
 
 import React, { lazy, Suspense, useEffect } from 'react';
 import lazyLoader from '@Controls/lazy-loader';
+import { select } from '@wordpress/data';
 
 const Settings = lazy( () =>
 	import(
@@ -20,6 +21,18 @@ const UAGBRestaurantMenuChild = ( props ) => {
 		// Assigning block_id in the attribute.
 		props.setAttributes( { block_id: props.clientId.substr( 0, 8 ) } );
 	}, [] );
+
+	useEffect( () => {
+		const parentClientId = select(
+			'core/block-editor'
+		).getBlockHierarchyRootClientId( props.clientId );
+		const parentAttributes = select( 'core/block-editor' ).getBlockAttributes(
+			parentClientId
+		);
+
+		const showImg = ( parentAttributes ) ? parentAttributes.showImage : props.attributes.showImage;
+		props.setAttributes ( { showImage: showImg } );
+	}, [props] );
 
 	return (
 		<>
