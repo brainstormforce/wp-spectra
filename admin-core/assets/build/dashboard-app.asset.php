a664c746ce0e87ea1a38c56e042493974784a0fb
Supriya Jadhav <supriyaj@bsf.io>
2022-08-09 20:29:44 +0530

    Updated ui/ux in recaptcha

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '63d97c28d4a620ad831fd889d31ce9c3');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '54078951f23c9595af3df814c168b69e');
