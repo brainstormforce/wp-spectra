9d1e2795c3df80a5cdc01ccb35cc14491d0b44b2
Antariksh Patre <antarikshpatre@gmail.com>
2022-08-10 09:20:57 +0530

    SPCTR-1601 Admin-Core build files

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '63d97c28d4a620ad831fd889d31ce9c3');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '63183f1fab7f77abd7ed368be6cdd925');
