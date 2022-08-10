bbdebc6de09570b4c21dc1e3a40f8a78e5b33c04
AbhijitNage123 <abhiitn@bsf.io>
2022-08-10 14:59:13 +0530

    added dot at the last of desciption

--- includes/blocks/forms-accept/block.php
+++ includes/blocks/forms-accept/block.php
@@ -12,7 +12,7 @@ $block_data = array(
 	'slug'        => '',
 	'link'        => '',
 	'title'       => __( 'Accept', 'ultimate-addons-for-gutenberg' ),
-	'description' => __( 'Add a consent statement with a checkbox in your form', 'ultimate-addons-for-gutenberg' ),
+	'description' => __( 'Add a consent statement with a checkbox in your form.', 'ultimate-addons-for-gutenberg' ),
 	'default'     => true,
 	'deprecated'  => false,
 );
