9ae2b9058e322e4b3b484cbbcc83eb49d0ec9b75
RajkiranBagal <rajkiranb@bsf.io>
2022-08-12 16:02:58 +0530

    fix: Issues reported by QA in Shapedivider in editor

--- src/blocks/container/render.js
+++ src/blocks/container/render.js
@@ -98,44 +98,44 @@ const Render = ( props ) => {
 
 	return (
 		<>
-			{ topDividerHtml }
-				<CustomTag
-					{ ...blockProps }
-					key = { block_id }
-					{...customTagLinkAttributes}
-				>
-					{ 'video' === backgroundType && (
-						<div className="uagb-container__video-wrap">
-							{ backgroundVideo && (
-								<video autoPlay loop muted playsinline>
-									<source
-										src={ backgroundVideo.url }
-										type="video/mp4"
-									/>
-								</video>
-							) }
-						</div>
-					) }
-					{ isBlockRootParent && 'alignfull' === contentWidth && 'alignwide' === innerContentWidth
-					?  (
-						<div className='uagb-container-inner-blocks-wrap'>
-							<InnerBlocks
-								__experimentalMoverDirection={ moverDirection }
-								renderAppender = { hasChildBlocks
-								? undefined
-								: InnerBlocks.ButtonBlockAppender }
-							/>
-						</div>
-					)
-					: <InnerBlocks
+			<CustomTag
+				{ ...blockProps }
+				key = { block_id }
+				{...customTagLinkAttributes}
+			>
+				{ topDividerHtml }
+				{ 'video' === backgroundType && (
+					<div className="uagb-container__video-wrap">
+						{ backgroundVideo && (
+							<video autoPlay loop muted playsinline>
+								<source
+									src={ backgroundVideo.url }
+									type="video/mp4"
+								/>
+							</video>
+						) }
+					</div>
+				) }
+				{ isBlockRootParent && 'alignfull' === contentWidth && 'alignwide' === innerContentWidth
+				?  (
+					<div className='uagb-container-inner-blocks-wrap'>
+						<InnerBlocks
 							__experimentalMoverDirection={ moverDirection }
 							renderAppender = { hasChildBlocks
 							? undefined
 							: InnerBlocks.ButtonBlockAppender }
 						/>
-					}
-				</CustomTag>
-			{ bottomDividerHtml }
+					</div>
+				)
+				: <InnerBlocks
+						__experimentalMoverDirection={ moverDirection }
+						renderAppender = { hasChildBlocks
+						? undefined
+						: InnerBlocks.ButtonBlockAppender }
+					/>
+				}
+				{ bottomDividerHtml }
+			</CustomTag>
 		</>
 	);
 };
