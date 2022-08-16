ba5918040ec7f9db155937a21deb726690172307
Supriya Jadhav <supriyaj@bsf.io>
2022-08-16 17:31:31 +0530

    Updated admin changes

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'c5c2f4c685f4f9affabc57a8fcade62b');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'aa73fdd1d224bae0855fa22ce8c63f61');
