58e8db5737107d036d9295cfcea8066c4a6f7d01
Zane Vaz <zanev@bsf.io>
2022-08-17 12:29:27 +0530

    Updated Admin Block Icons when Disabled

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'aa73fdd1d224bae0855fa22ce8c63f61');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '0d001f02738e1aac0086598a2871d3e8');
