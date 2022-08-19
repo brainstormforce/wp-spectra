8a05ffc7706c54dd906bf494f1ad32f974379f2f
AbhijitNage123 <abhiitn@bsf.io>
2022-08-19 16:42:03 +0530

    updated deprectaed label to legacy

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'b441fc45e1307abc1a5ff7394c2fc0bd');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '7c5ff14c523750b8f08eb833f1fb3cf5');
