f7f96d53a36528e784997f6144a5a8f8e50b1c7d
Supriya Jadhav <supriyaj@bsf.io>
2022-08-30 11:33:32 +0530

    Resolved poprup issue

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '3973bfd3737bb101445a197d683233bc');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '43de5e7696b010d7bf3174dacf810c6d');
