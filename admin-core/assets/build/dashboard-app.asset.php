ba0ae9581ba9c5727b98711933589a84f613234d
Supriya Jadhav <supriyaj@bsf.io>
2022-09-02 12:47:28 +0530

    Added msg

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '193ec0906375613041887c931d2d2cfc');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'd59471749e3c58f1e698ec738a45f239');
