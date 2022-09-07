3f96d4a6bbb93dcf2d4fec99a9caef5da7fde2b1
Tushar Imran <tusharimran9909@gmail.com>
2022-09-07 16:43:01 +0600

    Dynamic Content - popover border color not updating issue fixing

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '9ef43f3935323928b258f4faf9e7bd21');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '18b5361d1cb77275c3a2930f3bdf6855');
