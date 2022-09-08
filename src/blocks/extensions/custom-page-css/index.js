7a49196c30eacda609da98724dcd3eda2b9441a2
RajkiranBagal <rajkiranb@bsf.io>
2022-09-08 12:48:51 +0530

    wip

--- src/blocks/extensions/custom-page-css/index.js
+++ src/blocks/extensions/custom-page-css/index.js
@@ -1,6 +1,19 @@
+import CodeMirror from 'react-codemirror';
+import { select, dispatch } from '@wordpress/data';
+
 const PageCustomCSS = (props) => {
 
-	return "Hello";
+	return (
+		<CodeMirror 
+			options={{
+				mode: 'css',
+			}} 
+			value={select('core/editor').getEditedPostAttribute('meta')['_uag_custom_page_level_css']}
+			onChange={(newValue) => {
+				dispatch('core/editor').editPost({meta: {_uag_custom_page_level_css: newValue}})
+			}}
+		/>
+	);
 }
 
 export default PageCustomCSS;
