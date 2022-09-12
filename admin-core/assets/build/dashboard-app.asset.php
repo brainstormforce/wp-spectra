93644c234bfc31e4d8469a160b7a107efcc0bf5d
Tushar Imran <tusharimran9909@gmail.com>
2022-09-12 19:51:05 +0600

    improve login register block settings

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '27cada67034716b1cf876db2a6ff2e09');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'd0021df34f6bc90037a95c2c76949387');
--- admin-core/assets/src/dashboard-app/pages/settings/block-settings/Login.js
+++ admin-core/assets/src/dashboard-app/pages/settings/block-settings/Login.js
@@ -49,72 +49,92 @@ export default function Login(props) {
     };
 	return (
 		<React.Fragment>
-			<section>
-				<section className='flex mt-8'>
-					<div className='pr-16 pb-8 w-[78%]'>
-						<h3 className="text-lg leading-6 font-medium text-gray-900">
-							{__( 'Social Login/Register', 'ultimate-addons-for-gutenberg' )}
-						</h3>
-						<p className="mt-[0.6rem] text-sm ">
-							{ __( 'Enable Social Login Auto Register if user not registered.', 'ultimate-addons-for-gutenberg' ) }
-						</p>
-					</div>
-					<div>
-						<Switch
-							checked={socialRegister}
-							onChange={(e) => savedFormData('UPDATE_SOCIAL_REGISTER', !socialRegister)}
-							className={classNames(
-								socialRegister ? 'bg-spectra' : 'bg-gray-200',
-								'relative inline-flex flex-shrink-0 h-5 w-[2.4rem] items-center border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none'
-							)}
-							>
-							<span
-								aria-hidden="true"
-								className={classNames(
-									socialRegister ? 'translate-x-5' : 'translate-x-0',
-								'pointer-events-none inline-block h-3.5 w-3.5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200'
-								)}
-							/>
-						</Switch>
-					</div>
-				</section>
-				<div className='border-b pt-4'>
-					<div className='pb-4'>
-						<h3 className='text-lg leading-6 font-medium text-gray-900'>
-							{ __( 'Social Login/Register - Google Client ID', 'ultimate-addons-for-gutenberg' ) }
-						</h3>
-						<p className='mt-[0.6rem] text-sm'>
-							{ __( 'Note: This setting is required if you wish to use Login with Google in your website. Read', 'ultimate-addons-for-gutenberg' ) }
-							{' '}
-							<a href='https://ultimateelementor.com/docs/create-google-client-id-for-login-form-widget/?utm_source=uael-pro-dashboard&utm_medium=uael-menu-page&utm_campaign=uael-pro-plugin'>{__('this article', 'ultimate-addons-for-gutenberg')}</a>.
-						</p>
-					</div>
-					<div className='pb-8'>
-						<label className='block'>{__('Google Client ID', 'ultimate-addons-for-gutenberg')}</label>
-						<input className='block' type="text" name="google-client-id" value={googleClientId || ''} onChange={(e) => savedFormData('UPDATE_GOOGLE_CLIENT_ID', e.target.value)} />
-					</div>
+			<section className='flex border-b border-solid border-slate-200 px-12 py-8 justify-between'>
+				<div className='pr-16 w-[78%]'>
+					<h3 className="p-0 flex-1 justify-right inline-flex text-lg leading-8 font-medium text-gray-900">
+						{__( 'Social Login/Register', 'ultimate-addons-for-gutenberg' )}
+					</h3>
+					<p className="mt-2 text-sm text-slate-500">
+						{ __( 'Enable Social Login Auto Register if user not registered.', 'ultimate-addons-for-gutenberg' ) }
+					</p>
 				</div>
-				<div className='border-b pt-4'>
-					<div className='pb-4'>
-						<h3 className='text-lg leading-6 font-medium text-gray-900'>
-							{ __( 'Login Login/Register - Facebook App Details', 'ultimate-addons-for-gutenberg' ) }
-						</h3>
-						<p className='mt-[0.6rem] text-sm'>
-							{ __( 'Note: This setting is required if you wish to use Login with Facebook in your website. Need help to get Facebook App Details? Read', 'ultimate-addons-for-gutenberg' ) }
-							{' '}
-							<a href='https://ultimateelementor.com/docs/create-facebook-app-id-for-login-form-widget/?utm_source=uael-pro-dashboard&utm_medium=uael-menu-page&utm_campaign=uael-pro-plugin'>{__('this article', 'ultimate-addons-for-gutenberg')}</a>.
-						</p>
-					</div>
-					<div className='flex'>
-						<div className='pb-4'>
-							<label className='block'>{__('Facebook App ID', 'ultimate-addons-for-gutenberg')}</label>
-							<input className='block' type="text" name="facebook-app-id" value={facebookAppId || ''} onChange={(e) => savedFormData('UPDATE_FACEBOOK_APP_ID', e.target.value)} />
-						</div>
-						<div className='pb-4 ml-4'>
-							<label className='block'>{__('Facebook App Secret', 'ultimate-addons-for-gutenberg')}</label>
-							<input className='block' type="text" name="facebook-app-secret" value={facebookAppSecret || ''} onChange={(e) => savedFormData('UPDATE_FACEBOOK_APP_SECRET', e.target.value)} />
-						</div>
-					</div>
+				<div>
+					<Switch
+						checked={socialRegister}
+						onChange={(e) => savedFormData('UPDATE_SOCIAL_REGISTER', !socialRegister)}
+						className={classNames(
+							socialRegister ? 'bg-spectra' : 'bg-gray-200',
+							'relative inline-flex flex-shrink-0 h-5 w-[2.4rem] items-center border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none'
+						)}
+						>
+						<span
+							aria-hidden="true"
+							className={classNames(
+								socialRegister ? 'translate-x-5' : 'translate-x-0',
+							'pointer-events-none inline-block h-3.5 w-3.5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200'
+							)}
+						/>
+					</Switch>
+				</div>
+			</section>
+			<section className='block border-b border-solid border-slate-200 px-12 py-8'>
+				<div className='pb-4'>
+					<h3 className='p-0 flex-1 justify-right inline-flex text-lg leading-8 font-medium text-gray-900'>
+						{ __( 'Social Login/Register - Google Client ID', 'ultimate-addons-for-gutenberg' ) }
+					</h3>
+					<p className='mt-2 text-sm text-slate-500'>
+						{ __( 'Note: This setting is required if you wish to use Login with Google in your website. Read', 'ultimate-addons-for-gutenberg' ) }
+						{' '}
+						<a href=''>{__('this article', 'ultimate-addons-for-gutenberg')}</a>.
+					</p>
+					<p className="mt-2 w-9/12 text-sm">
+						<a className="text-spectra focus:text-spectra-hover active:text-spectra-hover hover:text-spectra-hover" href="https://ultimateelementor.com/docs/create-google-client-id-for-login-form-widget/?utm_source=uael-pro-dashboard&utm_medium=uael-menu-page&utm_campaign=uael-pro-plugin" target="_blank" rel="noreferrer">
+							{ __( 'Get Keys', 'ultimate-addons-for-gutenberg' ) }
+						</a>
+						{ __( ' | ', 'ultimate-addons-for-gutenberg' ) }
+						<a className="text-spectra focus:text-spectra-hover active:text-spectra-hover hover:text-spectra-hover" href="https://ultimateelementor.com/docs/create-google-client-id-for-login-form-widget/?utm_source=uael-pro-dashboard&utm_medium=uael-menu-page&utm_campaign=uael-pro-plugin" target="_blank" rel="noreferrer">
+							{ __(
+								' Documentation',
+								'ultimate-addons-for-gutenberg'
+							) }
+						</a>
+					</p>
+				</div>
+				<div>
+					<label className='block text-gray-900 text-sm font-medium'>{__('Google Client ID', 'ultimate-addons-for-gutenberg')}</label>
+					<input className='mt-3 w-9/12 h-10 text-sm placeholder-slate-400 transition spectra-admin__input-field' type="text" name="google-client-id" value={googleClientId || ''} onChange={(e) => savedFormData('UPDATE_GOOGLE_CLIENT_ID', e.target.value)} />
+				</div>
+			</section>
+			<section className='block border-b border-solid border-slate-200 px-12 py-8'>
+				<div className='pb-4'>
+					<h3 className='p-0 flex-1 justify-right inline-flex text-lg leading-8 font-medium text-gray-900'>
+						{ __( 'Login Login/Register - Facebook App Details', 'ultimate-addons-for-gutenberg' ) }
+					</h3>
+					<p className='mt-2 text-sm text-slate-500'>
+						{ __( 'Note: This setting is required if you wish to use Login with Facebook in your website. Need help to get Facebook App Details? Read', 'ultimate-addons-for-gutenberg' ) }
+						{' '}
+						<a href='https://ultimateelementor.com/docs/create-facebook-app-id-for-login-form-widget/?utm_source=uael-pro-dashboard&utm_medium=uael-menu-page&utm_campaign=uael-pro-plugin'>{__('this article', 'ultimate-addons-for-gutenberg')}</a>.
+					</p>
+					<p className="mt-2 w-9/12 text-sm">
+						<a className="text-spectra focus:text-spectra-hover active:text-spectra-hover hover:text-spectra-hover" href="https://ultimateelementor.com/docs/create-facebook-app-id-for-login-form-widget/?utm_source=uael-pro-dashboard&utm_medium=uael-menu-page&utm_campaign=uael-pro-plugin" target="_blank" rel="noreferrer">
+							{ __( 'Get Keys', 'ultimate-addons-for-gutenberg' ) }
+						</a>
+						{ __( ' | ', 'ultimate-addons-for-gutenberg' ) }
+						<a className="text-spectra focus:text-spectra-hover active:text-spectra-hover hover:text-spectra-hover" href="https://ultimateelementor.com/docs/create-facebook-app-id-for-login-form-widget/?utm_source=uael-pro-dashboard&utm_medium=uael-menu-page&utm_campaign=uael-pro-plugin" target="_blank" rel="noreferrer">
+							{ __(
+								' Documentation',
+								'ultimate-addons-for-gutenberg'
+							) }
+						</a>
+					</p>
+				</div>
+				<div className='pb-4'>
+					<label className='block text-gray-900 text-sm font-medium'>{__('Facebook App Details', 'ultimate-addons-for-gutenberg')}</label>
+					<input className='mt-3 w-9/12 h-10 text-sm placeholder-slate-400 transition spectra-admin__input-field' type="text" name="facebook-app-id" value={facebookAppId || ''} onChange={(e) => savedFormData('UPDATE_FACEBOOK_APP_ID', e.target.value)} />
+				</div>
+				<div className='pb-4'>
+					<label className='block text-gray-900 text-sm font-medium'>{__('Facebook App Secret Key', 'ultimate-addons-for-gutenberg')}</label>
+					<input className='mt-3 w-9/12 h-10 text-sm placeholder-slate-400 transition spectra-admin__input-field' type="text" name="facebook-app-secret" value={facebookAppSecret || ''} onChange={(e) => savedFormData('UPDATE_FACEBOOK_APP_SECRET', e.target.value)} />
 				</div>
 			</section>
 		</React.Fragment>
