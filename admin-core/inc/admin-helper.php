68405852b0fe99a86cadba02089d07117ae0a0ea
Sushma Kure <sushmak@bsf.io>
2022-09-14 13:10:54 +0530

    Fixed undefined var error

--- admin-core/inc/admin-helper.php
+++ admin-core/inc/admin-helper.php
@@ -38,8 +38,6 @@ class Admin_Helper {
 	 */
 	public static function get_common_settings() {
 
-		$general_settings = self::get_settings_page_data();
-
 		$uag_versions   = self::get_rollback_versions_options();
 		$changelog_data = self::get_changelog_feed_data();
 
