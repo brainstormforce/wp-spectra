fd16e8e30b6ba3816d5de96e40a287ad4368edf9
Supriya Jadhav <supriyaj@bsf.io>
2022-09-22 11:02:50 +0530

    Added backward compatability

--- src/blocks/icon-list-child/deprecated.js
+++ src/blocks/icon-list-child/deprecated.js
@@ -225,6 +225,80 @@ const deprecated = [
 			);
 		},
 	},
+	{
+		attributes,
+		save( props ) {
+			const { attributes, className } = props;
+
+			const {
+				label,
+				image_icon,
+				icon,
+				image,
+				block_id,
+				link,
+				target,
+				disableLink,
+				hideLabel,
+				fromParentIcon
+			} = attributes;
+		
+			const defaultedAlt = ( image && image?.alt ) ? image?.alt : '';
+		
+			let imageIconHtml = '';
+		
+			if ( image_icon === 'icon' ) {
+				if( icon || fromParentIcon ){
+					imageIconHtml = icon ? renderSVG( icon ) : renderSVG( fromParentIcon );
+				}
+			} else if ( image && image.url && image_icon !== 'none' ) {
+				imageIconHtml = (
+					<img
+						className="uagb-icon-list__source-image"
+						src={ image.url }
+						alt={ defaultedAlt }
+					/>
+				);
+			}
+		
+			const targetVal = target ? '_blank' : '_self';
+			const linkUrl = disableLink ? link : '/';
+
+			return (
+				<div
+					className={ classnames(
+						className,
+						`uagb-block-${ block_id }`
+					) }
+				>
+					{ disableLink && (
+						<a
+							target={ targetVal }
+							aria-label={ label.replace( /(<([^>]+)>)/ig, '' ) }
+							rel="noopener noreferrer"
+							href={ linkUrl }
+						>
+							{ ' ' }
+						</a>
+					) }
+					{
+						imageIconHtml && (
+							<span className="uagb-icon-list__source-wrap">
+								{ imageIconHtml }
+							</span>
+						)
+					}
+					{ ! hideLabel && '' !== label && (
+						<RichText.Content
+							tagName="span"
+							value={ label }
+							className="uagb-icon-list__label"
+						/>
+					) }
+				</div>
+			);
+		},
+	},
 ];
 
 export default deprecated;
