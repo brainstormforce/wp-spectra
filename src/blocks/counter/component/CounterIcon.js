4597962278451d41b4d392787a35078d06542b38
Antariksh Patre <antarikshpatre@gmail.com>
2022-09-27 13:02:03 +0530

    SPCTR-1835 Counter- CounterIcon Component- Changed div to span

--- src/blocks/counter/component/CounterIcon.js
+++ src/blocks/counter/component/CounterIcon.js
@@ -5,9 +5,9 @@ const CounterIcon = ( props ) => {
     const { attributes } = props;
 
     return (
-        <div className='wp-block-uagb-counter__icon'>
+        <span className='wp-block-uagb-counter__icon'>
             {renderSVG( attributes.icon )}
-        </div>
+        </span>
     );
 };
 
