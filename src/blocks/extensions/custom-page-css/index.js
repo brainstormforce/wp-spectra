e1c4f3ce2c3dc5dba48fcaee687f0a3ffa19931f
RajkiranBagal <rajkiranb@bsf.io>
2022-09-29 09:50:25 +0530

    wip: Added Different approach for the CUstom Code editor, Syntax Highliting Done, Functionality needs to be worked on.

--- src/blocks/extensions/custom-page-css/index.js
+++ src/blocks/extensions/custom-page-css/index.js
@@ -1,24 +1,26 @@
 import CodeMirror from 'react-codemirror';
 import { select, dispatch } from '@wordpress/data';
+import { useRef, useEffect, useState } from '@wordpress/element';
 
 const PageCustomCSS = (props) => {
 
+	const [customCSS, setCustomCSS] = useState(select('core/editor').getEditedPostAttribute('meta')['_uag_custom_page_level_css']);
+
+	const tabRef = useRef( null );
+	useEffect( () => {
+		console.log(tabRef.current);
+		wp.codeEditor.initialize(tabRef.current);
+    }, [tabRef] );
 	return (
-		<CodeMirror
-			options={{
-				mode: 'css',
-				lineNumbers: true,
-				indentUnit: 2,
-				tabSize: 2,
-				height: 'auto',
-				lineWrapping: true,
-				scrollbarStyle: 'null',
+		<textarea
+			ref={tabRef}
+			value={customCSS}
+			onChange={(event) => {
+				console.log(event);
+				setCustomCSS(event.target.value)
+				dispatch('core/editor').editPost({meta: {_uag_custom_page_level_css: event.target.value}})
 			}}
-			value={select('core/editor').getEditedPostAttribute('meta')['_uag_custom_page_level_css']}
-			onChange={(newValue) => {
-				dispatch('core/editor').editPost({meta: {_uag_custom_page_level_css: newValue}})
-			}}
-		/>
+		></textarea>
 	);
 }
 
