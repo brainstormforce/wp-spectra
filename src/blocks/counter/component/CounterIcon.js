27bac2d58c9d54ac264c7e4a5aa643ba7226793d
Antariksh Patre <antarikshpatre@gmail.com>
2022-09-29 17:19:47 +0530

    SPCTR-548 Counter- CounterIcon Component- Updated component to support images

--- src/blocks/counter/component/CounterIcon.js
+++ src/blocks/counter/component/CounterIcon.js
@@ -4,11 +4,54 @@ const CounterIcon = ( props ) => {
 
     const { attributes } = props;
 
-    return (
-        <span className='wp-block-uagb-counter__icon'>
-            {renderSVG( attributes.icon )}
-        </span>
-    );
+    if( attributes.sourceType === 'icon' ) {
+        return (
+            <span className='wp-block-uagb-counter__icon'>
+                {renderSVG( attributes.icon )}
+            </span>
+        );
+    }
+
+    if( attributes.sourceType === 'image' ) {
+
+        let urlCheck = '';
+
+        if (
+            typeof attributes.iconImage !== 'undefined' &&
+            attributes.iconImage !== null &&
+            attributes.iconImage !== ''
+        ) {
+            urlCheck = attributes.iconImage.url;
+        }
+
+        let url = '';
+
+        if ( urlCheck !== '' ) {
+            const size = attributes.iconImage.sizes;
+            const imageSize = attributes.imageSize;
+    
+            if (
+                typeof size !== 'undefined' &&
+                typeof size[ imageSize ] !== 'undefined'
+            ) {
+                url = size[ imageSize ].url;
+            } else {
+                url = urlCheck;
+            }
+            return (
+                <span className='wp-block-uagb-counter__image'>
+                    <img
+                        className=""
+                        src={ url }
+                        alt={ attributes.iconImage.alt }
+                    />
+                </span>
+            );
+        }
+    }
+
+    return null;
+
 };
 
 export default CounterIcon;
