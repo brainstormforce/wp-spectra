a3012804c171d138907f5f896b6044bcecfc5bff
RajkiranBagal <rajkiranb@bsf.io>
2022-10-03 10:38:40 +0530

    Completed functionality, syntax highlighting & css attr guessing

--- src/blocks/extensions/custom-page-css/index.js
+++ src/blocks/extensions/custom-page-css/index.js
@@ -1,26 +1,26 @@
-import CodeMirror from 'react-codemirror';
 import { select, dispatch } from '@wordpress/data';
-import { useRef, useEffect, useState } from '@wordpress/element';
+import { useRef, useEffect } from '@wordpress/element';
 
 const PageCustomCSS = (props) => {
 
-	const [customCSS, setCustomCSS] = useState(select('core/editor').getEditedPostAttribute('meta')['_uag_custom_page_level_css']);
-
 	const tabRef = useRef( null );
+
 	useEffect( () => {
-		console.log(tabRef.current);
-		wp.codeEditor.initialize(tabRef.current);
+		const editor = wp?.codeEditor?.initialize(tabRef?.current);
+		const codeMirrorEditor = document.querySelector('.CodeMirror-code');
+
+		if ( codeMirrorEditor ) {
+			codeMirrorEditor?.addEventListener( 'keyup', function () {
+				editor?.codemirror?.save();
+				const value = editor?.codemirror?.getValue();
+				dispatch('core/editor').editPost({meta: {_uag_custom_page_level_css: value}})
+
+			});
+		}
     }, [tabRef] );
+
 	return (
-		<textarea
-			ref={tabRef}
-			value={customCSS}
-			onChange={(event) => {
-				console.log(event);
-				setCustomCSS(event.target.value)
-				dispatch('core/editor').editPost({meta: {_uag_custom_page_level_css: event.target.value}})
-			}}
-		></textarea>
+		<textarea value={select('core/editor').getEditedPostAttribute('meta')['_uag_custom_page_level_css']} ref={tabRef}></textarea>
 	);
 }
 
