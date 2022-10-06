2fead3f8a30e7a63459f82e1a8150a56ac2d3d74
Rajkiran Bagal <rajkiranb@bsf.io>
2022-10-06 11:39:50 +0530

    fix: Resolved PHPCS & Linting Errors

--- classes/class-uagb-block-helper.php
+++ classes/class-uagb-block-helper.php
@@ -142,7 +142,7 @@ if ( ! class_exists( 'UAGB_Block_Helper' ) ) {
 			$selectors[ $wrapper . '.wp-block-button__link' ] = $border_css;
 			$m_selectors[ $wrapper ]                          = $border_css_mobile;
 			$t_selectors[ $wrapper ]                          = $border_css_tablet;
-			$selectors[ $wrapper . ':hover' ]                = array(
+			$selectors[ $wrapper . ':hover' ]                 = array(
 				'border-color' => ! empty( $attr['btnBorderHColor'] ) ? $attr['btnBorderHColor'] : $attr['borderHColor'],
 			);
 			$selectors[ $wrapper . ' .uagb-button__link' ]    = array(
