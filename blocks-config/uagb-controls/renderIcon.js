12be3f34fac6b058deedccf66efff7389ea9a475
AbhijitNage123 <abhiitn@bsf.io>
2022-10-07 18:32:39 +0530

    Added a condition check for getting FA icon

--- blocks-config/uagb-controls/renderIcon.js
+++ blocks-config/uagb-controls/renderIcon.js
@@ -29,8 +29,8 @@ function renderSVG( svg ) {
 	let fontAwesome;
 
 	// Load Polyfiller Array if needed.
-	if ( 'disabled' !== uagb_blocks_info.load_font_awesome_5 ) {
-		fontAwesome = uagb_blocks_info.uagb_svg_icons[ uagb_blocks_info.font_awesome_5_polyfill.data[ svg ] ];
+	if ( 'disabled' !== uagb_blocks_info.load_font_awesome_5 && 0 !== uagb_blocks_info.font_awesome_5_polyfill.length ) {
+		fontAwesome = uagb_blocks_info.uagb_svg_icons[ uagb_blocks_info.font_awesome_5_polyfill?.data[ svg ] ];
 		if ( ! fontAwesome ) {
 			fontAwesome = uagb_blocks_info.uagb_svg_icons[ svg ];
 		}
