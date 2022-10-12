a3281fa7aa16f0481cdbca1dd0a8a17caf590e56
Sushma Kure <sushmak@bsf.io>
2022-10-12 13:18:21 +0530

    Added modal block html

--- src/blocks/modal/save.js
+++ src/blocks/modal/save.js
@@ -20,6 +20,14 @@ export default function Save( props ) {
 				tagName="p"
 				className="uagb-modal-text"
 			/>
+			<div class="uagb-modal-popup active" data-hover="">
+				<div class="uagb-modal-popup-wrap">
+					<div class="uagb-modal-popup-close">
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="26" height="26"><path d="M13 11.8l6.1-6.3-1-1-6.1 6.2-6.1-6.2-1 1 6.1 6.3-6.5 6.7 1 1 6.5-6.6 6.5 6.6 1-1z"></path>
+						</svg>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
