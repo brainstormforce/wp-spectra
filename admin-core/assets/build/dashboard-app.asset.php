023980ba6c46fbaa8eaa617ab5c9e59d3f6f459b
Tushar Imran <tusharimran9909@gmail.com>
2022-10-13 18:56:11 +0600

    Production Build

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '079fb042e6aa24f2ed59795f883d69f2');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'ca4257d499ef012ed4a6216891c13cc9');
