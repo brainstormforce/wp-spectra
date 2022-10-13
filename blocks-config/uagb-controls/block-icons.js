99c8d934017485ea87f590ffd8ffcc28b278da53
Tushar Imran <tusharimran9909@gmail.com>
2022-10-13 19:21:14 +0600

    Added password & re enter password Icon

--- blocks-config/uagb-controls/block-icons.js
+++ blocks-config/uagb-controls/block-icons.js
@@ -843,6 +843,30 @@ const UAGB_Block_Icons = {
 			strokeLinejoin: 'round',
 		} ),
 	),
+	password: el(
+		'svg',
+		{ width: 24, height: 24, viewBox: '0 0 24 24', fill: 'none', className: 'uagb-editor-icons' },
+		el( 'path', {
+			d: 'M15 7C16.1046 7 17 7.89543 17 9M21 9C21 12.3137 18.3137 15 15 15C14.3938 15 13.8087 14.9101 13.2571 14.7429L11 17H9V19H7V21H4C3.44772 21 3 20.5523 3 20V17.4142C3 17.149 3.10536 16.8946 3.29289 16.7071L9.25707 10.7429C9.08989 10.1914 9 9.60617 9 9C9 5.68629 11.6863 3 15 3C18.3137 3 21 5.68629 21 9Z',
+			fill: noColor,
+			stroke: iconColor,
+			strokeWidth: 1.4,
+			strokeLinecap: 'round',
+			strokeLinejoin: 'round',
+		} ),
+	),
+	rePassword: el(
+		'svg',
+		{ width: 24, height: 24, viewBox: '0 0 24 24', fill: 'none', className: 'uagb-editor-icons' },
+		el( 'path', {
+			d: 'M13 9C13 7.9 13.8954 7 15 7C16.1046 7 17 7.89543 17 9C17 10.1 16.1 11 15 11M15 11V10M15 11L16 11.5M21 9C21 12.3137 18.3137 15 15 15C14.3938 15 13.8087 14.9101 13.2571 14.7429L11 17H9V19H7V21H4C3.44772 21 3 20.5523 3 20V17.4142C3 17.149 3.10536 16.8946 3.29289 16.7071L9.25707 10.7429C9.08989 10.1914 9 9.60617 9 9C9 5.68629 11.6863 3 15 3C18.3137 3 21 5.68629 21 9Z',
+			fill: noColor,
+			stroke: iconColor,
+			strokeWidth: 1.4,
+			strokeLinecap: 'round',
+			strokeLinejoin: 'round',
+		} ),
+	),
 
 	// ------------------------.
 	// All Miscellaneous Icons .
