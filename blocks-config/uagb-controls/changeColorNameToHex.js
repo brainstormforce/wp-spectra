99ff9201970f59bc80f46bf2a2feba4df554e4cd
AbhijitNage123 <abhiitn@bsf.io>
2022-10-17 17:25:15 +0530

    Added theme color compatibility

--- blocks-config/uagb-controls/changeColorNameToHex.js
+++ blocks-config/uagb-controls/changeColorNameToHex.js
@@ -1,4 +1,6 @@
+import maybeGetColorForVariable from '@Controls/maybeGetColorForVariable';
 function colourNameToHex( colour ) {
+
 	const colours = {'black': '#000000','cyan-bluish-gray':'#abb8c3', 'white':'#ffffff', 'pale-pink':'#f78da7',
 			'luminous-vivid-orange':'#ff6900','luminous-vivid-amber':'#fcb900','light-green-cyan':'#7bdcb5','vivid-green-cyan':'#00d084',
 			'pale-cyan-blue': '#8ed1fc','vivid-cyan-blue': '#0693e3','vivid-purple': '#9b51e0'}
@@ -6,7 +8,8 @@ function colourNameToHex( colour ) {
 		if ( typeof colours[colour?.toLowerCase()] !== 'undefined' ){
 			return colours[colour?.toLowerCase()];
 		}
+			const colorVal = `var(--${colour})`;
+			return maybeGetColorForVariable( colorVal );
 
-    return false;
 }
 export default colourNameToHex
