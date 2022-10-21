de8b4af8e8de82b999662d00c7700b311a440eff
Supriya Jadhav <supriyaj@bsf.io>
2022-10-21 12:47:28 +0530

    Fix: RTL issues in admin dshboard

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'a32718e8fa657e8468ecf3910f27c4fd');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '92c2de8ee0498a61193e03085b5d81b4');
