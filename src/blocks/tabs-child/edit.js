0bed16bf6eb050abc038e99e92bb3805e380d225
Zane Vaz <zanev@bsf.io>
2022-10-27 12:45:05 +0530

    SPCTR-1854: Fixed and Tested Tab Child Preview UseEffect Block Break

--- src/blocks/tabs-child/edit.js
+++ src/blocks/tabs-child/edit.js
@@ -2,27 +2,29 @@
  * BLOCK: Tabs Child Block
  */
 
-import React, { useEffect,    } from 'react';
+import React, { useEffect } from 'react';
 
 import Render from './render';
 import { select } from '@wordpress/data';
 
 const UAGBTabsChildEdit = ( props ) => {
 	useEffect( () => {
-		const { attributes, setAttributes, clientId } = props;
-		const { getBlockRootClientId, getBlockAttributes } = ! wp.blockEditor
-			? select( 'core/editor' )
-			: select( 'core/block-editor' );
-		const rootBlockId = getBlockRootClientId( clientId );
-		const rootBlockAttrs = getBlockAttributes( rootBlockId );
-		setAttributes( { block_id: props.clientId.substr( 0, 8 ) } );
-		setAttributes( { tabActive: rootBlockAttrs.tabActiveFrontend } );
+		if ( ! props.attributes.isPreview ) {
+			const { attributes, setAttributes, clientId } = props;
+			const { getBlockRootClientId, getBlockAttributes } = ! wp.blockEditor
+				? select( 'core/editor' )
+				: select( 'core/block-editor' );
+			const rootBlockId = getBlockRootClientId( clientId );
+			const rootBlockAttrs = getBlockAttributes( rootBlockId );
+			setAttributes( { block_id: props.clientId.substr( 0, 8 ) } );
+			setAttributes( { tabActive: rootBlockAttrs.tabActiveFrontend } );
 
-		// Apply parent style if newly inserted
-		if ( rootBlockAttrs !== null && rootBlockAttrs.needUpdate !== false ) {
-			Object.keys( rootBlockAttrs ).map( ( attribute ) =>
-				attributes[ attribute ] = rootBlockAttrs[ attribute ]
-			)
+			// Apply parent style if newly inserted
+			if ( rootBlockAttrs !== null && rootBlockAttrs.needUpdate !== false ) {
+				Object.keys( rootBlockAttrs ).map( ( attribute ) =>
+					attributes[ attribute ] = rootBlockAttrs[ attribute ]
+				)
+			}
 		}
 	}, [] );
 
