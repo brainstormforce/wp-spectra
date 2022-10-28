e0cc9c9759ec17a5cd67a1d256f904aea234e4c1
jamyrbsf <jamyr@bsf.io>
2022-10-28 13:31:14 +0600

    post time line rich text added

--- src/blocks/timeline/post-timeline/components/CtaLink.js
+++ src/blocks/timeline/post-timeline/components/CtaLink.js
@@ -2,32 +2,28 @@ import React from 'react';
 import { RichText } from '@wordpress/block-editor';
 
 const CtaLink = ( props ) => {
-	const { post, attributes, setAttributes} = props;
+	const { post, attributes, setAttributes } = props;
 	let target = '_self';
 	if ( attributes.linkTarget ) {
 		target = '_blank';
 	}
-	{console.log('hekaslsa')}
 
 	if ( attributes.displayPostLink ) {
 
 		if ( setAttributes !== 'not_set' ) {
 			return (
 				<div className="uagb-timeline__link_parent wp-block-button"	>
-					<a
-						className="uagb-timeline__link wp-block-button__link"
-						href={ post.link }
-						target={ target }
-						rel="noopener noreferrer"
-					>
-						{console.log('hekaslsa 2')}
 						<RichText
-							// value={ attributes.readMoreText.replace( /(<([^>]+)>)/ig, '' ) }
+							value={ attributes.readMoreText.replace( /(<([^>]+)>)/ig, '' ) }
 							onChange={ ( value ) => {
 								setAttributes( { readMoreText: value } );
 							} }
-						/>
-					</a>
+							tagName='a'
+							className="uagb-timeline__link wp-block-button__link"
+							href={ post.link }
+							target={ target }
+							rel="noopener noreferrer"
+					/>
 				</div>
 			);
 		}
@@ -40,14 +36,13 @@ const CtaLink = ( props ) => {
 					target={ target }
 					rel="noopener noreferrer"
 				>
-						{console.log('hekaslsa 3')}
-
 					<RichText.Content
 						value={ attributes.readMoreText.replace( /(<([^>]+)>)/ig, '' ) }
 					/>
 				</a>
 			</div>
 		);
+
 	}
 	return null;
 };
