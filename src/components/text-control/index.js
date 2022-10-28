077e8f53f74735c6c59f339c43b153ae1637c3d0
Tushar Imran <tusharimran9909@gmail.com>
2022-10-28 20:39:53 +0600

    Dynamic Content - Update dynamic content text field components

--- src/components/text-control/index.js
+++ src/components/text-control/index.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {useLayoutEffect} from 'react';
+import styles from './editor.lazy.scss';
 import { TextControl } from '@wordpress/components';
+import Separator from '@Components/separator';
 import { useSelect } from '@wordpress/data';
 import PropTypes from 'prop-types';
 
@@ -14,36 +16,52 @@ const defaultProps = {
 };
 
 export default function UAGTextControl(props) {
+	// Add and remove the CSS on the drop and remove of the component.
+	useLayoutEffect( () => {
+		styles.use();
+		return () => {
+			styles.unuse();
+		};
+	}, [] );
+
 	const {label, name, type, value, setAttributes, disableDynamicContent} = props
 	const selectedBlock = useSelect( ( select ) => {
 		return select( 'core/block-editor' ).getSelectedBlock();
 	}, [] );
 
-	let registerImageExtender = disableDynamicContent ? null : wp.hooks.applyFilters('uagb.registerTextExtender', '', selectedBlock?.name, name, type)
+	let registerTextExtender = disableDynamicContent ? null : wp.hooks.applyFilters('uagb.registerTextExtender', '', selectedBlock?.name, name, type)
 
-	const isShowImageUploader = () => {
+	const isEnableDynamicContent = () => {
 		if(disableDynamicContent){
-			return true;
+			return false;
 		}
 		const dynamicContent = selectedBlock?.attributes?.dynamicContent
 		if(dynamicContent && dynamicContent?.[name]?.enable === true) {
-			return false
+			return true
 		}
-		return true;
+		return false;
 	}
 
 	return (
 		<React.Fragment>
-			<TextControl
-				label={label}
-				value={ value }
-				onChange={ ( value ) =>
-					setAttributes( { [name]: value } )
+			<div className={`uagb-text-control${isEnableDynamicContent() ? ' uagb-text-control--open-dynamic-content' : ''}`}>
+				<TextControl
+					label={label}
+					value={ value }
+					onChange={ ( value ) =>
+						setAttributes( { [name]: value } )
+					}
+					readOnly={isEnableDynamicContent()}
+				/>
+				{
+					registerTextExtender
 				}
-			/>
-			{
-				registerImageExtender
-			}
+				{
+					isEnableDynamicContent() && (
+						<Separator />
+					)
+				}
+			</div>
 		</React.Fragment>
 	);
 }
