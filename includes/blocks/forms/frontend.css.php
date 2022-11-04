a26e9c72d62f9b8e340287f6d0f01244dde035b8
jamyrbsf <jamyr@bsf.io>
2022-11-04 23:42:18 +0600

    lint fix

--- includes/blocks/forms/frontend.css.php
+++ includes/blocks/forms/frontend.css.php
@@ -271,7 +271,7 @@ $t_selectors = array(
 	' .uagb-forms-failed-message'                    => $failed_message_border_tablet,
 	// Drop icon position css.
 	' .uagb-form-phone-country'                      => array(
-		'background-position' => ' top 50% right ' . UAGB_Helper::get_css_value( $attr['paddingFieldRightTablet'] ? $attr['paddingFieldRightTablet'] : 12 , $attr['paddingFieldUnitTablet'] ),
+		'background-position' => 'top 50% right ' . UAGB_Helper::get_css_value( $attr['paddingFieldRightTablet'] ? $attr['paddingFieldRightTablet'] : 12, $attr['paddingFieldUnitTablet'] ),
 		'padding-right'       => UAGB_Helper::get_css_value( $forms_padding_right_tablet_fallback, $attr['paddingFieldUnitTablet'] ) . ' !important',
 	),
 );
@@ -309,7 +309,7 @@ $m_selectors = array(
 
 	// Drop icon position css.
 	' .uagb-form-phone-country'                      => array(
-		'background-position' => 'top 50% right ' . UAGB_Helper::get_css_value( $attr['paddingFieldRightMobile'] ? $attr['paddingFieldRightMobile'] : 6 , $attr['paddingFieldUnitmobile'] ),
+		'background-position' => 'top 50% right ' . UAGB_Helper::get_css_value( $attr['paddingFieldRightMobile'] ? $attr['paddingFieldRightMobile'] : 6, $attr['paddingFieldUnitmobile'] ),
 		'padding-right'       => UAGB_Helper::get_css_value( $forms_padding_right_mobile_fallback, $attr['paddingFieldUnitmobile'] ) . ' !important',
 	),
 );
