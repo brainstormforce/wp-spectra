0016466cc911ff7e5961dff878c6d9f11e6056fe
Antariksh Patre <antarikshpatre@gmail.com>
2022-11-11 10:57:30 +0530

    SPCTR-548 Counter - Updated block preview address

--- src/blocks/counter/edit.js
+++ src/blocks/counter/edit.js
@@ -25,13 +25,13 @@ export default function UAGBCounterEdit( props ) {
         addBlockEditorDynamicStyles( 'uagb-counter-style-' + props.clientId.substr( 0, 8 ), blockStyling );
 	}, [ props ] );
 
-	const previewImageData = `${ uagb_blocks_info.uagb_url }/admin/assets/preview-images/counter.svg`;
+	const previewImageData = `${ uagb_blocks_info.uagb_url }/assets/images/block-previews/counter.svg`;
 
 	return (
 		props.attributes.isPreview ? <img width='100%' src={previewImageData} alt=''/> :
-		<React.Fragment>
+		<>
 			<Settings parentProps={ props } />
 			<Render parentProps={ props } />
-		</React.Fragment>
+		</>
 	);
 }
