cfc508762724f9defd8e653fe878fc398d0b5077
Zane Vaz <zanev@bsf.io>
2022-11-19 11:29:52 +0530

    SPCTR-2050: Updated UI of Nav Stats as per Style Guide

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '5d4c75e232f1022688dc');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '30121b18b929c259b420');
--- admin-core/assets/src/dashboard-app/PluginStats.js
+++ admin-core/assets/src/dashboard-app/PluginStats.js
@@ -7,8 +7,15 @@ import { __ } from '@wordpress/i18n';
 const PluginStats = () => {
 
 	const renderDivider = () => (
-		<svg width="1" height="16" viewBox="0 0 1 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-			<line x1="0.5" y1="2.18557e-08" x2="0.499999" y2="16" stroke="#E2E8F0"/>
+		<svg
+			width='1'
+			height='16'
+			viewBox='0 0 1 16'
+			fill='none'
+			xmlns='http://www.w3.org/2000/svg'
+			className='mr-4'
+		>
+			<line x1='0.5' y1='2.18557e-08' x2='0.499999' y2='16' stroke='#E2E8F0'/>
 		</svg>
 	);
 
@@ -16,15 +23,23 @@ const PluginStats = () => {
 		uag_react.spectra_pro_installed ? (
 			uag_react.spectra_pro_status ? (
 				uag_react.license_status ? (
-					__( 'License activated', 'ultimate-addons-for-gutenberg' )
+					<div className='mr-8 text-sm text-emerald-500'>
+						{ __( 'License activated', 'ultimate-addons-for-gutenberg' ) }
+					</div>					
 				) : (
-					__( 'License not activated', 'ultimate-addons-for-gutenberg' )
+					<div className='mr-8 text-sm text-slate-400'>
+						{ __( 'License not activated', 'ultimate-addons-for-gutenberg' ) }
+					</div>
 				)
 			) : (
-				__( 'Enable Spectra Pro', 'ultimate-addons-for-gutenberg' )
+				<div className='mr-8 text-sm text-spectra'>
+					{ __( 'Enable Spectra Pro', 'ultimate-addons-for-gutenberg' ) }
+				</div>
 			)
 		) : (
-			__( 'Upgrade to Pro', 'ultimate-addons-for-gutenberg' )
+			<div className='mr-8 text-sm text-spectra'>
+				{ __( 'Upgrade to Pro', 'ultimate-addons-for-gutenberg' ) }
+			</div>
 		)
 	);
 
@@ -32,19 +47,22 @@ const PluginStats = () => {
 
 	return (
 		<>
-			<div>
+			<div className='mr-4 text-sm text-slate-400'>
 				{ uag_react.plugin_ver }
 			</div>
+			{ renderDivider() }
 			{ ( uag_react.spectra_pro_ver ) && (
 				<>
-					{ renderDivider() }
-					<div>
+					<div className='mr-2 text-sm text-slate-500'>
 						{ uag_react.spectra_pro_ver }
 					</div>
+					<div className='mr-4 px-1 py-0.5 text-[10px] leading-[10px] text-slate-500 border border-slate-400 rounded'>
+						{ __( 'Pro', 'ultimate-addons-for-gutenberg' ) }
+					</div>
 					{ renderDivider() }
-					{ renderLicenseStatus() }
 				</>
 			) }
+			{ renderLicenseStatus() }
 		</>
 	);
 
