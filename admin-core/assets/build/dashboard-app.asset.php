b902f2d28fc58d37f26dde32fcb74f467e3a6627
Zane Vaz <zanev@bsf.io>
2022-11-21 08:33:36 +0530

    SPCTR-2050: Updated Dynamic Content Extension Tag Status when Pro is Disabled

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '351acecaedb95dc7fcfc');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '2831ea431ab294f069ff');
