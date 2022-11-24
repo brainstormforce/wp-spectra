7bf2356ce3f7864e926c1d4141c4a90b52700650
Zane Vaz <zanev@bsf.io>
2022-11-24 08:21:41 +0530

    SPCTR-2088: Resolved Remaining PHPCS Warnings

--- includes/blocks/faq/block.php
+++ includes/blocks/faq/block.php
@@ -9,18 +9,18 @@
 
 $block_slug = 'uagb/faq';
 $block_data = array(
-	'doc'              => 'faq-schema-or-accordion',
-	'slug'             => '',
-	'admin_categories' => array( 'seo' ),
-	'link'             => 'faq-schema',
-	'title'            => __( 'FAQ', 'ultimate-addons-for-gutenberg' ),
-	'description'      => __( 'Add accordions/FAQ schema to your page.', 'ultimate-addons-for-gutenberg' ),
-	'default'          => true,
-	'extension'        => false,
-	'js_assets'        => array( 'uagb-faq-js' ),
-	'priority'         => Spectra_Block_Prioritization::get_block_priority( 'faq' ),
-	'deprecated'       => false,
-	'dynamic_assets'   => array(
+	'doc'                 => 'faq-schema-or-accordion',
+	'slug'                => '',
+	'admin_categories'    => array( 'seo' ),
+	'link'                => 'faq-schema',
+	'title'               => __( 'FAQ', 'ultimate-addons-for-gutenberg' ),
+	'description'         => __( 'Add accordions/FAQ schema to your page.', 'ultimate-addons-for-gutenberg' ),
+	'default'             => true,
+	'extension'           => false,
+	'js_assets'           => array( 'uagb-faq-js' ),
+	'priority'            => Spectra_Block_Prioritization::get_block_priority( 'faq' ),
+	'deprecated'          => false,
+	'dynamic_assets'      => array(
 		'dir' => 'faq',
 	),
 	'static_dependencies' => array(
