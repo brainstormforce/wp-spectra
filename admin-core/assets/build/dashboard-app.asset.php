a94374c0b057fd1561713bad9d628c6d16a21d16
Rajkiran Bagal <rajkiranb@bsf.io>
2022-11-24 15:49:09 +0530

    Fix: The Default Content Width value is changed to default

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'e0f5676391a5ff2287b5');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '6536e3949ad572fa040eeac8ff5e50db');
