7085e33833a3aaa0272b4b0851bc840a36e03b6b
Tushar Imran <tusharimran9909@gmail.com>
2022-11-24 20:03:01 +0600

    Added Hooks for modify editor css

--- src/blocks/advanced-heading/styling.js
+++ src/blocks/advanced-heading/styling.js
@@ -150,8 +150,8 @@ function styling( props ) {
 		highLightPaddingUnitMobile,
 	} = props.attributes;
 
-	const tablet_selectors = {};
-	const mobile_selectors = {};
+	let tablet_selectors = {};
+	let mobile_selectors = {};
 
 	let gradientLinkColor = {}
 	let gradientLinkHoverColor = {}
@@ -169,7 +169,7 @@ function styling( props ) {
 	const highLightBorderCSSMobile = generateBorderCSS( props.attributes, 'highLight', 'mobile' )
 
 
-	const selectors = {
+	let selectors = {
 		'.wp-block-uagb-advanced-heading ':{
 			'background': 	'classic' === blockBackgroundType ? blockBackground : blockGradientBackground,
 			'text-align': headingAlign,
@@ -540,6 +540,10 @@ function styling( props ) {
 		8
 	) }`;
 
+	selectors = wp.hooks.applyFilters( `spectra.${blockName}.styling`, selectors, props.attributes );
+	tablet_selectors = wp.hooks.applyFilters( `spectra.${blockName}.tabletStyling`, tablet_selectors, props.attributes );
+	mobile_selectors = wp.hooks.applyFilters( `spectra.${blockName}.mobileStyling`, mobile_selectors, props.attributes );
+
 	let styling_css = generateCSS( selectors, base_selector );
 
 	styling_css += generateCSS(
@@ -555,7 +559,6 @@ function styling( props ) {
 		true,
 		'mobile'
 	);
-
 	return styling_css;
 }
 
