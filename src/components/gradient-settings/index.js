5d1d501f450ea27bb16d9778d83209401c2294f4
Tushar Imran <tusharimran9909@gmail.com>
2022-11-28 21:47:39 +0600

    Added Gradient Settings Components Before/After hook

--- src/components/gradient-settings/index.js
+++ src/components/gradient-settings/index.js
@@ -1,9 +1,10 @@
 import styles from './editor.lazy.scss';
 import { GradientPicker } from '@wordpress/components';
 import React, { useLayoutEffect } from 'react';
+import { select } from '@wordpress/data'
 
 const GradientSettings = ( props ) => {
-	
+
 	// Add and remove the CSS on the drop and remove of the component.
 	useLayoutEffect( () => {
 		styles.use();
@@ -11,6 +12,7 @@ const GradientSettings = ( props ) => {
 			styles.unuse();
 		};
 	}, [] );
+	const { getSelectedBlock } = select( 'core/block-editor' );
 
 	const { setAttributes, backgroundGradient } = props;
 
@@ -18,12 +20,25 @@ const GradientSettings = ( props ) => {
 		setAttributes( { [ backgroundGradient.label ]: value } );
 	};
 
+	const blockNameForHook = getSelectedBlock()?.name.split( '/' ).pop(); // eslint-disable-line @wordpress/no-unused-vars-before-return
+	const controlName = 'gradient-settings'; //
+	const controlBeforeDomElement = wp.hooks.applyFilters( `spectra.${blockNameForHook}.gradient-settings.${controlName}.before`, '', blockNameForHook );
+	const controlAfterDomElement = wp.hooks.applyFilters( `spectra.${blockNameForHook}.gradient-settings.${controlName}`, '', blockNameForHook );
+
 	return (
-		<GradientPicker
-			value={ backgroundGradient.value }
-			onChange={ onGradientChange }
-			className="uagb-gradient-picker"
-		/>
+		<>
+			{
+				controlBeforeDomElement
+			}
+			<GradientPicker
+				value={ backgroundGradient.value }
+				onChange={ onGradientChange }
+				className="uagb-gradient-picker"
+			/>
+			{
+				controlAfterDomElement
+			}
+		</>
 	);
 }
 
