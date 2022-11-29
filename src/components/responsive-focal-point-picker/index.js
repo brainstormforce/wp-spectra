e2f277761016399ea95ca43ed6fc2a4b3e9f0bdb
Tushar Imran <tusharimran9909@gmail.com>
2022-11-29 21:47:18 +0600

    Added responsive-focal-point-picker component before/after hook

--- src/components/responsive-focal-point-picker/index.js
+++ src/components/responsive-focal-point-picker/index.js
@@ -6,10 +6,13 @@
  import ResponsiveToggle from '../responsive-toggle';
  import { __ } from '@wordpress/i18n';
  import { FocalPointPicker } from '@wordpress/components';
+ import { select } from '@wordpress/data';
 
  const ResponsiveUAGFocalPointPicker = ( props ) => {
 
 	const { backgroundPosition, backgroundImage, setAttributes } = props;
+
+	const { getSelectedBlock } = select( 'core/block-editor' );
 
 	const responsive = true;
 
@@ -37,8 +40,8 @@
 				setAttributes( { [ backgroundPosition[device]?.label ]: focalPoint } );
 			} }
 		/>
-	 );
-	 output.Mobile = (
+	);
+	output.Mobile = (
 		<FocalPointPicker
 			url={ url }
 			value={ value }
@@ -46,24 +49,33 @@
 				setAttributes( { [ backgroundPosition[device]?.label ]: focalPoint } );
 			} }
 		/>
-	 );
+	);
 
-	 return (
-		 <div className="components-base-control uagb-responsive-select-control">
-			 <div className="uagb-size-type-field-tabs">
-				 <div className="uagb-control__header">
-					 <ResponsiveToggle
-						 label= { __( 'Position', 'ultimate-addons-for-gutenberg' ) }
-						 responsive= { responsive }
-					 />
-				 </div>
-				 { output[ deviceType ] ? output[ deviceType ] : output.Desktop }
-			 </div>
-			 { props.help && (
-				 <p className="uag-control-help-notice">{ props.help }</p>
-			 ) }
-		 </div>
-	 );
- };
+	const blockNameForHook = getSelectedBlock()?.name.split( '/' ).pop(); // eslint-disable-line @wordpress/no-unused-vars-before-return
+	const controlName = 'position'; // There is no label props that's why keep hard coded label
+	const controlBeforeDomElement = wp.hooks.applyFilters( `spectra.${blockNameForHook}.responsive-focal-point-picker.${controlName}.before`, '', blockNameForHook );
+	const controlAfterDomElement = wp.hooks.applyFilters( `spectra.${blockNameForHook}.responsive-focal-point-picker.${controlName}`, '', blockNameForHook );
 
- export default ResponsiveUAGFocalPointPicker;
+	return (
+		<>
+			{controlBeforeDomElement}
+			<div className="components-base-control uagb-responsive-select-control">
+				<div className="uagb-size-type-field-tabs">
+					<div className="uagb-control__header">
+						<ResponsiveToggle
+							label= { __( 'Position', 'ultimate-addons-for-gutenberg' ) }
+							responsive= { responsive }
+						/>
+					</div>
+					{ output[ deviceType ] ? output[ deviceType ] : output.Desktop }
+				</div>
+				{ props.help && (
+					<p className="uag-control-help-notice">{ props.help }</p>
+				) }
+			</div>
+			{controlAfterDomElement}
+		</>
+	);
+};
+
+export default ResponsiveUAGFocalPointPicker;
