0f5959742b1b95c009d9a24f04d384f802180a61
Zane Vaz <zanev@bsf.io>
2022-11-30 15:58:12 +0530

    SPCTR-2093: Commented Upgrade to Pro Indication in Admin Dashboard

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'ddb8be1162afc623f1ce');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '5a00a76c5ea6422aeb44');
