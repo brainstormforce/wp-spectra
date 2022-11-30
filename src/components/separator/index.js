cfe8b794dc451cd0faf3d032fa40b46154730263
Tushar Imran <tusharimran9909@gmail.com>
2022-11-30 10:48:14 +0600

    Added separator before/after hook

--- src/components/separator/index.js
+++ src/components/separator/index.js
@@ -3,6 +3,7 @@
  */
 import styles from './editor.lazy.scss';
 import React, { useLayoutEffect } from 'react';
+import { select } from '@wordpress/data';
 import PropTypes from 'prop-types';
 
 const propTypes = {
@@ -21,7 +22,22 @@ export default function Separator ( { disabledTopSpace } ) {
 		};
 	}, [] );
 
-	return <div className={`spectra-separator ${disabledTopSpace ? 'spectra-separator--removed-top-space' : ''}`} />;
+	const { getSelectedBlock } = select( 'core/block-editor' );
+	const blockNameForHook = getSelectedBlock()?.name.split( '/' ).pop(); // eslint-disable-line @wordpress/no-unused-vars-before-return
+	const controlName = 'separator'; // there is no label props that's why keep hard coded label
+	const controlBeforeDomElement = wp.hooks.applyFilters( `spectra.${blockNameForHook}.separator.${controlName}.before`, '', blockNameForHook );
+	const controlAfterDomElement = wp.hooks.applyFilters( `spectra.${blockNameForHook}.separator.${controlName}`, '', blockNameForHook );
+	return (
+		<>
+			{
+				controlBeforeDomElement
+			}
+			<div className={`spectra-separator ${disabledTopSpace ? 'spectra-separator--removed-top-space' : ''}`} />
+			{
+				controlAfterDomElement
+			}
+		</>
+	);
 };
 
 Separator.propTypes = propTypes;
