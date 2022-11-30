46b36d14de0041e210fda135bd68be778f183070
Tushar Imran <tusharimran9909@gmail.com>
2022-11-30 12:25:03 +0600

    Added typography components before/after hook

--- src/components/typography/index.js
+++ src/components/typography/index.js
@@ -14,7 +14,8 @@ import RangeTypographyControl from './range-typography';
 import TypographyStyles from './inline-styles';
 import styles from './editor.lazy.scss';
 import React, { useLayoutEffect } from 'react';
-import { select } from '@wordpress/data'
+import { select } from '@wordpress/data';
+import { getIdFromString } from '@Utils/Helpers';
 import getUAGEditorStateLocalStorage from '@Controls/getUAGEditorStateLocalStorage';
 import { blocksAttributes } from '@Attributes/getBlocksDefaultAttributes';
 
@@ -420,17 +421,32 @@ const TypographyControl = ( props ) => {
 		);
 	}
 
+
+	const blockNameForHook = getSelectedBlock()?.name.split( '/' ).pop(); // eslint-disable-line @wordpress/no-unused-vars-before-return
+	const controlName = getIdFromString(props.label);
+	const controlBeforeDomElement = wp.hooks.applyFilters( `spectra.${blockNameForHook}.typography.${controlName}.before`, '', blockNameForHook );
+	const controlAfterDomElement = wp.hooks.applyFilters( `spectra.${blockNameForHook}.typography.${controlName}`, '', blockNameForHook );
+
+
 	return (
-		<div
-			className={ `components-base-control uag-typography-options spectra-control-popup__options popup-${props?.attributes?.block_id} ${ activeClass }` }
-		>
-			{ ! disableAdvancedOptions && (
-				<>
-					{ fontTypoAdvancedControls }
-					{ showAdvancedFontControls }
-				</>
-			) }
-		</div>
+		<>
+			{
+				controlBeforeDomElement
+			}
+			<div
+				className={ `components-base-control uag-typography-options spectra-control-popup__options popup-${props?.attributes?.block_id} ${ activeClass }` }
+			>
+				{ ! disableAdvancedOptions && (
+					<>
+						{ fontTypoAdvancedControls }
+						{ showAdvancedFontControls }
+					</>
+				) }
+			</div>
+			{
+				controlAfterDomElement
+			}
+		</>
 	);
 };
 
