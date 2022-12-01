900174628f0bbf6cb10e9e2b6fc738bfb7178d5c
Zane Vaz <zanev@bsf.io>
2022-12-01 11:48:25 +0530

    SPCTR-2093: Commented All Traces of Pro

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '5a00a76c5ea6422aeb44');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'c5ac770202baee40724f');
