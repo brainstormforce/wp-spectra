3a50193b148b3256c4d885c38cf11ad5e09e9129
Tushar Imran <tusharimran9909@gmail.com>
2022-12-12 10:13:54 +0600

    Gradient Settings Component - Added Panel Name inside freemium hook

--- src/components/gradient-settings/index.js
+++ src/components/gradient-settings/index.js
@@ -1,10 +1,13 @@
 import styles from './editor.lazy.scss';
 import { GradientPicker } from '@wordpress/components';
-import React, { useLayoutEffect } from 'react';
+import React, { useLayoutEffect, useEffect, useState, useRef } from 'react';
+import { getPanelIdFromRef } from '@Utils/Helpers';
 import { select } from '@wordpress/data'
 
 const GradientSettings = ( props ) => {
-
+	const [panelNameForHook, setPanelNameForHook] = useState(null);
+	const panelRef = useRef( null );
+	const { getSelectedBlock } = select( 'core/block-editor' );
 	// Add and remove the CSS on the drop and remove of the component.
 	useLayoutEffect( () => {
 		styles.use();
@@ -12,7 +15,13 @@ const GradientSettings = ( props ) => {
 			styles.unuse();
 		};
 	}, [] );
-	const { getSelectedBlock } = select( 'core/block-editor' );
+
+	const blockNameForHook = getSelectedBlock()?.name.split( '/' ).pop(); // eslint-disable-line @wordpress/no-unused-vars-before-return
+	useEffect(() => {
+		setPanelNameForHook( getPanelIdFromRef(panelRef))
+	}, [blockNameForHook])
+
+
 
 	const { setAttributes, backgroundGradient } = props;
 
@@ -20,13 +29,14 @@ const GradientSettings = ( props ) => {
 		setAttributes( { [ backgroundGradient.label ]: value } );
 	};
 
-	const blockNameForHook = getSelectedBlock()?.name.split( '/' ).pop(); // eslint-disable-line @wordpress/no-unused-vars-before-return
-	const controlName = 'gradient-settings'; //
-	const controlBeforeDomElement = wp.hooks.applyFilters( `spectra.${blockNameForHook}.gradient-settings.${controlName}.before`, '', blockNameForHook );
-	const controlAfterDomElement = wp.hooks.applyFilters( `spectra.${blockNameForHook}.gradient-settings.${controlName}`, '', blockNameForHook );
+	const controlName = 'gradient-settings'; // there is no label props that's why keep hard coded label
+	const controlBeforeDomElement = wp.hooks.applyFilters( `spectra.${blockNameForHook}.${panelNameForHook}.${controlName}.before`, '', blockNameForHook );
+	const controlAfterDomElement = wp.hooks.applyFilters( `spectra.${blockNameForHook}.${panelNameForHook}.${controlName}`, '', blockNameForHook );
 
 	return (
-		<>
+		<div
+			ref={panelRef}
+		>
 			{
 				controlBeforeDomElement
 			}
@@ -38,7 +48,7 @@ const GradientSettings = ( props ) => {
 			{
 				controlAfterDomElement
 			}
-		</>
+		</div>
 	);
 }
 
