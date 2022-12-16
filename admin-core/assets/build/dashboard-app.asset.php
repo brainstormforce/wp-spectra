c788ff22e4ae257564a6eb490e5b3d29826af3d7
Zane Vaz <zanev@bsf.io>
2022-12-16 09:02:56 +0530

    SPCTR-2039: Linted

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'cf863b67d07eaa912902');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'e9e37d77e0a800f24928');
