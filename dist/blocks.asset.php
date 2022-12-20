dd35d5fcc4828c1c4c304e39298544c3296da7f3
Vikas Patil <patilvikasj@gmail.com>
2022-12-20 12:00:38 +0530

    npm run build run

--- dist/blocks.asset.php
+++ dist/blocks.asset.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-edit-post', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-keyboard-shortcuts', 'wp-plugins', 'wp-polyfill', 'wp-primitives', 'wp-rich-text', 'wp-url'), 'version' => '7edc9591c2e73dc8571eef03b6eb068f');
-=======
 <?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-edit-post', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-keyboard-shortcuts', 'wp-plugins', 'wp-polyfill', 'wp-primitives', 'wp-rich-text', 'wp-url'), 'version' => '712b8e26af349f4f209842f21fb303fb');
->>>>>>> 426a764246c81078aa30f07d8adce2d4eabd976b
