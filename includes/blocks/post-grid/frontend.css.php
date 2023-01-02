122dd6cdf853305938937cf22d09f3b2e5890c07
AbhijitNage123 <abhiitn@bsf.io>
2023-01-02 18:31:41 +0530

    Fixed undefined array key paddingLeft and paddingRight for grid

--- includes/blocks/post-grid/frontend.css.php
+++ includes/blocks/post-grid/frontend.css.php
@@ -17,6 +17,8 @@ $paddingLeftMobile                 = isset( $attr['paddingLeftMobile'] ) ? $attr
 $paddingRightMobile                = isset( $attr['paddingRightMobile'] ) ? $attr['paddingRightMobile'] : $attr['contentPaddingMobile'];
 $paddingLeftTablet                 = isset( $attr['paddingLeftTablet'] ) ? $attr['paddingLeftTablet'] : $attr['contentPadding'];
 $paddingRightTablet                = isset( $attr['paddingRightTablet'] ) ? $attr['paddingRightTablet'] : $attr['contentPadding'];
+$paddingLeft                       = isset( $attr['paddingLeft'] ) ? $attr['paddingLeft'] : $attr['contentPadding'];
+$paddingRight                      = isset( $attr['paddingRight'] ) ? $attr['paddingRight'] : $attr['contentPadding'];
 
 $selectors = UAGB_Block_Helper::get_post_selectors( $attr );
 // Pagination CSS.
@@ -67,12 +69,12 @@ $t_selectors = UAGB_Block_Helper::get_post_tablet_selectors( $attr );
 
 if ( 'top' === $attr['imgPosition'] ) {
 	$selectors['.uagb-equal_height_inline-read-more-buttons .uagb-post__inner-wrap .uagb-post__text:last-child']   = array(
-		'left'  => UAGB_Helper::get_css_value( $attr['paddingLeft'], 'px' ),
-		'right' => UAGB_Helper::get_css_value( $attr['paddingRight'], 'px' ),
+		'left'  => UAGB_Helper::get_css_value( $paddingLeft, 'px' ),
+		'right' => UAGB_Helper::get_css_value( $paddingRight, 'px' ),
 	);
 	$m_selectors['.uagb-equal_height_inline-read-more-buttons .uagb-post__inner-wrap .uagb-post__text:last-child'] = array(
 		'left'  => UAGB_Helper::get_css_value( $paddingLeftMobile, 'px' ),
-		'right' => UAGB_Helper::get_css_value( $paddingLeftMobile, 'px' ),
+		'right' => UAGB_Helper::get_css_value( $paddingRightMobile, 'px' ),
 	);
 	$m_selectors['.uagb-equal_height_inline-read-more-buttons .uagb-post__inner-wrap .uagb-post__text:last-child'] = array(
 		'left'  => UAGB_Helper::get_css_value( $paddingLeftTablet, 'px' ),
@@ -80,12 +82,12 @@ if ( 'top' === $attr['imgPosition'] ) {
 	);
 } else {
 	$selectors['.uagb-equal_height_inline-read-more-buttons .uagb-post__inner-wrap .uagb-post__text:nth-last-child(2)']   = array(
-		'left'  => UAGB_Helper::get_css_value( $attr['paddingLeft'], 'px' ),
-		'right' => UAGB_Helper::get_css_value( $attr['paddingRight'], 'px' ),
+		'left'  => UAGB_Helper::get_css_value( $paddingLeft, 'px' ),
+		'right' => UAGB_Helper::get_css_value( $paddingRight, 'px' ),
 	);
 	$m_selectors['.uagb-equal_height_inline-read-more-buttons .uagb-post__inner-wrap .uagb-post__text:nth-last-child(2)'] = array(
 		'left'  => UAGB_Helper::get_css_value( $paddingLeftMobile, 'px' ),
-		'right' => UAGB_Helper::get_css_value( $paddingLeftMobile, 'px' ),
+		'right' => UAGB_Helper::get_css_value( $paddingRightMobile, 'px' ),
 	);
 	$m_selectors['.uagb-equal_height_inline-read-more-buttons .uagb-post__inner-wrap .uagb-post__text:nth-last-child(2)'] = array(
 		'left'  => UAGB_Helper::get_css_value( $paddingLeftTablet, 'px' ),
