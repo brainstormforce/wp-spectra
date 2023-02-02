0afc7d7549cdf181afdd4bd607c8faa0345fee72
AbhijitNage123 <abhiitn@bsf.io>
2023-02-02 09:59:42 +0530

    file name change same as folder for v2.3.4 and comment for function change to respective function name

--- src/blocks/advanced-heading/deprecated.js
+++ src/blocks/advanced-heading/deprecated.js
@@ -2,8 +2,8 @@
  * BLOCK: Advanced Heading - Deprecated Block
  */
  import classnames from 'classnames';
- import attributes_v2_3_4 from './v2.3.5/attributes';
- import save_v2_3_4 from './v2.3.5/save';
+ import attributes_v2_3_4 from './v2.3.4/attributes';
+ import save_v2_3_4 from './v2.3.4/save';
 import { RichText } from '@wordpress/block-editor';
 const attributes = {
 	block_id: {
