9f8484a3427b4e4a333fc6119228a39be5916a57
Antariksh Patre <antarikshpatre@gmail.com>
2023-03-02 10:25:08 +0530

    SPCTR-380 Spectra Animations - Admin Dashboard - Added Icon

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '67e589d874d51e44a46c31ab53f9487f');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '39f4cf2e6ef063aa59ce1d858ec1e8b6');
