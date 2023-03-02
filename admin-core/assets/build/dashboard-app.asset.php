91ee2c41cfbaf135eb8d10fb397556b15d076aa7
Rajkiran Bagal <rajkiranb@bsf.io>
2023-03-02 20:52:22 +0530

    Updated necessary files

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'ab26cd243d993b01cf4e');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '8e2fb1bb3adcacc152ff');
