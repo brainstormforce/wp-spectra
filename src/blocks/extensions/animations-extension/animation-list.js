2155e10fdb845b9e9862fdc95e9c798f63c9a292
Antariksh Patre <antarikshpatre@gmail.com>
2023-03-13 20:32:43 +0530

    SPCTR-380 Spectra Animations - Animations List - Added missing i18n text-domains

--- src/blocks/extensions/animations-extension/animation-list.js
+++ src/blocks/extensions/animations-extension/animation-list.js
@@ -2,42 +2,42 @@ import { __ } from '@wordpress/i18n';
 
 export const AnimationList = [
     // None.
-    { value: '', label: __( 'None' ) },
+    { value: '', label: __( 'None', 'ultimate-addons-for-gutenberg' ) },
 
     // Flip.
-    { value: 'flip-down', label: __( 'Flip Down' ) },
-    { value: 'flip-up', label: __( 'Flip Up' ) },
-    { value: 'flip-left', label: __( 'Flip Left' ) },
-    { value: 'flip-right', label: __( 'Flip Right' ) },
+    { value: 'flip-down', label: __( 'Flip Down', 'ultimate-addons-for-gutenberg' ) },
+    { value: 'flip-up', label: __( 'Flip Up', 'ultimate-addons-for-gutenberg' ) },
+    { value: 'flip-left', label: __( 'Flip Left', 'ultimate-addons-for-gutenberg' ) },
+    { value: 'flip-right', label: __( 'Flip Right', 'ultimate-addons-for-gutenberg' ) },
 
     // Slide.
-    { value: 'slide-down', label: __( 'Slide Down' ) },
-    { value: 'slide-up', label: __( 'Slide Up' ) },
-    { value: 'slide-left', label: __( 'Slide Left' ) },
-    { value: 'slide-right', label: __( 'Slide Right' ) },
+    { value: 'slide-down', label: __( 'Slide Down', 'ultimate-addons-for-gutenberg' ) },
+    { value: 'slide-up', label: __( 'Slide Up', 'ultimate-addons-for-gutenberg' ) },
+    { value: 'slide-left', label: __( 'Slide Left', 'ultimate-addons-for-gutenberg' ) },
+    { value: 'slide-right', label: __( 'Slide Right', 'ultimate-addons-for-gutenberg' ) },
 
     // Fade.
-    { value: 'fade', label: __( 'Fade' ) },
-    { value: 'fade-down', label: __( 'Fade Down' ) },
-    { value: 'fade-up', label: __( 'Fade Up' ) },
-    { value: 'fade-left', label: __( 'Fade Left' ) },
-    { value: 'fade-right', label: __( 'Fade Right' ) },
-    { value: 'fade-up-right', label: __( 'Fade-Up Right' ) },
-    { value: 'fade-up-left', label: __( 'Fade-Up Left' ) },
-    { value: 'fade-down-left', label: __( 'Fade-Out Left' ) },
-    { value: 'fade-down-right', label: __( 'Fade-Out Right' ) },
+    { value: 'fade', label: __( 'Fade', 'ultimate-addons-for-gutenberg' ) },
+    { value: 'fade-down', label: __( 'Fade Down', 'ultimate-addons-for-gutenberg' ) },
+    { value: 'fade-up', label: __( 'Fade Up', 'ultimate-addons-for-gutenberg' ) },
+    { value: 'fade-left', label: __( 'Fade Left', 'ultimate-addons-for-gutenberg' ) },
+    { value: 'fade-right', label: __( 'Fade Right', 'ultimate-addons-for-gutenberg' ) },
+    { value: 'fade-up-right', label: __( 'Fade-Up Right', 'ultimate-addons-for-gutenberg' ) },
+    { value: 'fade-up-left', label: __( 'Fade-Up Left', 'ultimate-addons-for-gutenberg' ) },
+    { value: 'fade-down-left', label: __( 'Fade-Out Left', 'ultimate-addons-for-gutenberg' ) },
+    { value: 'fade-down-right', label: __( 'Fade-Out Right', 'ultimate-addons-for-gutenberg' ) },
 
     // Zoom.
-    { value: 'zoom-in', label: __( 'Zoom-In' ) },
-    { value: 'zoom-in-down', label: __( 'Zoom-In Down' ) },
-    { value: 'zoom-in-up', label: __( 'Zoom-In Up' ) },
-    { value: 'zoom-in-left', label: __( 'Zoom-In Left' ) },
-    { value: 'zoom-in-right', label: __( 'Zoom-In Right' ) },
-    { value: 'zoom-out', label: __( 'Zoom-Out' ) },
-    { value: 'zoom-out-down', label: __( 'Zoom-Out Down' ) },
-    { value: 'zoom-out-up', label: __( 'Zoom-Out Up' ) },
-    { value: 'zoom-out-left', label: __( 'Zoom-Out Left' ) },
-    { value: 'zoom-out-right', label: __( 'Zoom-Out Right' ) },
+    { value: 'zoom-in', label: __( 'Zoom-In', 'ultimate-addons-for-gutenberg' ) },
+    { value: 'zoom-in-down', label: __( 'Zoom-In Down', 'ultimate-addons-for-gutenberg' ) },
+    { value: 'zoom-in-up', label: __( 'Zoom-In Up', 'ultimate-addons-for-gutenberg' ) },
+    { value: 'zoom-in-left', label: __( 'Zoom-In Left', 'ultimate-addons-for-gutenberg' ) },
+    { value: 'zoom-in-right', label: __( 'Zoom-In Right', 'ultimate-addons-for-gutenberg' ) },
+    { value: 'zoom-out', label: __( 'Zoom-Out', 'ultimate-addons-for-gutenberg' ) },
+    { value: 'zoom-out-down', label: __( 'Zoom-Out Down', 'ultimate-addons-for-gutenberg' ) },
+    { value: 'zoom-out-up', label: __( 'Zoom-Out Up', 'ultimate-addons-for-gutenberg' ) },
+    { value: 'zoom-out-left', label: __( 'Zoom-Out Left', 'ultimate-addons-for-gutenberg' ) },
+    { value: 'zoom-out-right', label: __( 'Zoom-Out Right', 'ultimate-addons-for-gutenberg' ) },
 
 ];
 
