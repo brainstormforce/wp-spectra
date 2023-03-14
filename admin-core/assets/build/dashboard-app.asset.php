ce1ed640263449d2e8e01a6b40ffdb6965248857
Antariksh Patre <antarikshpatre@gmail.com>
2023-03-14 11:23:42 +0530

    SPCTR-380 Spectra Animations - Admin Core Icon - used variable for fill attribute

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '743e7de8965ad2e760b73526497d04e7');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '647aa2b1875108eceed4234999872778');
