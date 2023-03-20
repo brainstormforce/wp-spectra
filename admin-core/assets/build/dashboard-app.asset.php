699eb8401625fdbe56656de938d5998ccd1ebad1
Zane Vaz <94819993+bsf-zanev@users.noreply.github.com>
2023-03-20 17:13:45 +0530

    Imrpovement: Added Warning and Failed Settings Saved Notifications to the Admin Dashboard (#2835)
    * Added Warning and Failed Settings Saved Notifications
    
    * Updated Conditional Switch Case
    
    * Removed Default Case
    
    ---------
    
    Co-authored-by: Rajkiran Gajanan Bagal <47767536+RajkiranBagal@users.noreply.github.com>

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'f0635ac8b26047f1dcbe');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '5c24bc3b1c4a250f7b3f');
