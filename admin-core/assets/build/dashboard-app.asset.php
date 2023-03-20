d879addf32bd73e9cf211d7535a5718d3f1a579f
Zane Vaz <94819993+bsf-zanev@users.noreply.github.com>
2023-03-20 18:02:21 +0530

    Improvement: Reworked Admin Settings Saved Notification for Dynamic Values (#2837)
    * Reworked Admin Settings Saved Notification for Dynamic Values
    
    * Replaced Settings Saved Notification Array with Object

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '5c24bc3b1c4a250f7b3f');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'b5c3b97f8d15f25053fb');
