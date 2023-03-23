14a0737b4e714da669c477436eaa4ed3312580f1
AbhijitNage123 <abhiitn@bsf.io>
2023-03-24 01:47:41 +0530

    updated condition to site-editor

--- blocks-config/uagb-controls/addCommonDataToSpectraBlocks.js
+++ blocks-config/uagb-controls/addCommonDataToSpectraBlocks.js
@@ -7,7 +7,7 @@ function addCommonDataToSpectraBlocks( configData = {} ) {
 		},
 	};
 
-	if ( uagb_blocks_info.is_site_editor ) {
+	if ( 'site-editor' === uagb_blocks_info.is_site_editor ) {
 		data = {};
 	}
 	return {...configData, ...data };
