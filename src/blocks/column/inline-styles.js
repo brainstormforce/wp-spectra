48b54452a3d6f0f466706f011818e02a5e1ecb73
Raj Mohan <68589327+rajmohanreddy774@users.noreply.github.com>
2023-03-23 12:16:26 +0530

    formatting column block using prettier

--- src/blocks/column/inline-styles.js
+++ src/blocks/column/inline-styles.js
@@ -4,14 +4,8 @@
 import generateBorderCSS from '@Controls/generateBorderCSS';
 
 function inlineStyles( props ) {
-	const {
-		backgroundColor,
-		backgroundType,
-		backgroundImageColor,
-		overlayType,
-		gradientValue,
-		backgroundOpacity
-	} = props.attributes;
+	const { backgroundColor, backgroundType, backgroundImageColor, overlayType, gradientValue, backgroundOpacity } =
+		props.attributes;
 
 	const style = {};
 
@@ -20,23 +14,34 @@ function inlineStyles( props ) {
 	switch ( backgroundType ) {
 		case 'image':
 			if ( 'color' === overlayType ) {
-				style.opacity = ( typeof backgroundOpacity !== 'undefined' && 101 !== backgroundOpacity && 0 !== backgroundOpacity ) ? backgroundOpacity/100 : '';
+				style.opacity =
+					typeof backgroundOpacity !== 'undefined' && 101 !== backgroundOpacity && 0 !== backgroundOpacity
+						? backgroundOpacity / 100
+						: '';
 				style[ 'background-color' ] = backgroundImageColor;
 			} else {
-				style[	'background-image'	] = gradientValue;
+				style[ 'background-image' ] = gradientValue;
 			}
 			break;
 		case 'gradient':
-				style[ 'background-image' ] = gradientValue;
-				style.opacity =  ( typeof backgroundOpacity !== 'undefined' && 101 !== backgroundOpacity && 0 !== backgroundOpacity ) ? backgroundOpacity / 100 : '';
+			style[ 'background-image' ] = gradientValue;
+			style.opacity =
+				typeof backgroundOpacity !== 'undefined' && 101 !== backgroundOpacity && 0 !== backgroundOpacity
+					? backgroundOpacity / 100
+					: '';
 			break;
 		case 'color':
 			style[ 'background-color' ] = backgroundColor;
-			style.opacity = ( typeof backgroundOpacity !== 'undefined' && 101 !== backgroundOpacity && 0 !== backgroundOpacity ) ? backgroundOpacity/100 : '';
+			style.opacity =
+				typeof backgroundOpacity !== 'undefined' && 101 !== backgroundOpacity && 0 !== backgroundOpacity
+					? backgroundOpacity / 100
+					: '';
 			break;
 	}
 
-	style[ 'border-radius' ] = `${borderCSS['border-top-left-radius']} ${borderCSS['border-top-right-radius']} ${borderCSS['border-bottom-right-radius']} ${ borderCSS['border-bottom-left-radius']}`;
+	style[
+		'border-radius'
+	] = `${ borderCSS[ 'border-top-left-radius' ] } ${ borderCSS[ 'border-top-right-radius' ] } ${ borderCSS[ 'border-bottom-right-radius' ] } ${ borderCSS[ 'border-bottom-left-radius' ] }`;
 
 	return style;
 }
