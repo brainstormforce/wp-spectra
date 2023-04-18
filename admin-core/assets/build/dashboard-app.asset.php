da7b0c409767202f08b270c6829dccd108fa6357
Abhishek Rijal <avee.ws@gmail.com>
2023-04-18 13:24:39 +0545

    Added Settings for DC Extension (#3000)

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-i18n'), 'version' => '5ca5a844f2bcb7f31bbc');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-i18n'), 'version' => 'a22d7b9f249b3d227e8f');
--- admin-core/assets/src/dashboard-app/pages/blocks/Extensions.js
+++ admin-core/assets/src/dashboard-app/pages/blocks/Extensions.js
@@ -1,16 +1,16 @@
 import DisplayConditionsExtension from '@DashboardApp/pages/blocks/DisplayConditionsExtension';
 import MasonryGalleryExtension from '@DashboardApp/pages/blocks/MasonryGalleryExtension';
 import ResponsiveConditionsExtention from '@DashboardApp/pages/blocks/ResponsiveConditionsExtention';
-// import DynamicContentExtension from '@DashboardApp/pages/blocks/DynamicContentExtension';
+import DynamicContentExtension from '@DashboardApp/pages/blocks/DynamicContentExtension';
 
 const Extensions = ( { currentTab } ) => {
-	// All extensions should be sorted in Alphebetical Order of their labels.
+	// All extensions should be sorted in Alphabetical Order of their labels.
 	// Render all Extensions when required, render Pro Extensions on the Pro Tab.
 	if ( 'extensions' === currentTab || 'all' === currentTab ) {
 		return (
 			<>
 				<DisplayConditionsExtension/>
-				{/* <DynamicContentExtension/> */}
+				<DynamicContentExtension/>
 				<MasonryGalleryExtension/>
 				<ResponsiveConditionsExtention/>
 			</>
@@ -18,7 +18,7 @@ const Extensions = ( { currentTab } ) => {
 	} else if ( 'pro' === currentTab ) {
 		return (
 			<>
-				{/* <DynamicContentExtension/> */}
+				<DynamicContentExtension/>
 			</>
 		);
 	}
