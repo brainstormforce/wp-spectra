bd42ba64f1069d3a847b819748c51c2cd5c837fa
Supriya Jadhav <supriyaj@bsf.io>
2023-05-08 23:55:09 +0530

    Fix: Resolved infobox issues (#3099)

--- src/blocks/container/styling.js
+++ src/blocks/container/styling.js
@@ -315,7 +315,6 @@ function styling( attributes, clientId, name ) {
 	}
 
 	selectors[ containerFlexSelector ] = {
-		'min-height': generateCSSUnit( minHeightDesktop, minHeightType ),
 		'flex-direction': directionDesktop,
 		'align-items': alignItemsDesktop,
 		'justify-content': justifyContentDesktop,
