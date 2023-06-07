7055cc46b5e24a596bdda7f4b449d8491e212e5d
Zane Vaz <94819993+bsf-zanev@users.noreply.github.com>
2023-06-07 08:39:42 +0530

    SPCTR-2943 - Improvement: Admin Submenu Upgrade (#3237)
    * SPCTR-2943: Added Base of Admin Submenu Divisions
    
    * SPCTR-2943: Rearranged Menu Items and Improved the Menu Layout
    
    * SPCTR-2943: Enqueued Common CSS for Spectra Submenus and Fixed Popup Submenu Divider Overflow Issue
    
    * SPCTR-2943: Added Admin Submenu Switcher Based on Current Spectra Dashboard Page
    
    * SPCTR-2943: Added Translation to Default Render on Admin Dashboard
    
    * SPCTR-2943: Linted
    
    * SPCTR-2943: Updated Menu Position

--- admin-core/assets/src/dashboard-app/SettingsRoute.js
+++ admin-core/assets/src/dashboard-app/SettingsRoute.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import { __ } from '@wordpress/i18n';
 import Welcome from './pages/welcome/Welcome';
 import Blocks from '@DashboardApp/pages/blocks/Blocks';
 import Settings from '@DashboardApp/pages/settings/Settings';
@@ -10,9 +11,24 @@ function SettingsRoute() {
 	const path = query.get( 'path' );
 	const currentEvent = query.get( 'event' );
 
-	let routePage = <p>Default route fallback</p>;
+	let routePage = <p>{ __( 'Default route fallback', 'ultimate-addons-for-gutenberg' ) }</p>;
 
 	if ( uag_react.home_slug === page ) {
+
+		// Get the Admin Sidebar Element from Wordpress.
+		const adminMenu = document.getElementById( 'toplevel_page_spectra' )?.querySelector( '.wp-submenu' );
+
+		// Remove the 'current' class from the old active elements.
+		const oldCurrent = adminMenu?.querySelectorAll( '.current' );
+		oldCurrent?.forEach( currentElement => {
+			currentElement.classList.remove( 'current' );
+		} );
+
+		// Add the 'current' class to the new active elements.
+		const newCurrent = adminMenu?.querySelector( `[href="admin.php?page=${ page }${ path ? `&path=${ path }` : ''}"]` );
+		newCurrent?.classList?.add( 'current' );
+		newCurrent?.parentElement?.classList?.add( 'current' );
+
 		if ( 'getting-started' === currentEvent ) {
 			routePage = <Welcome/>;
 		} else {
