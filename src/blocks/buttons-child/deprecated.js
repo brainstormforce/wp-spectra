8c55c89d1e58c1257910cda2b8effeda2b8065d7
Abhishek Rijal <avee.ws@gmail.com>
2023-07-04 18:52:56 +0545

    SPCTR-3057[ Dynamic Content ] - Improvement Added Dynamic Content support to buttons label (#3289)
    * button text dc support
    
    * deprecation added for button
    
    ---------
    
    Co-authored-by: Aditya Jain <adi3890@gmail.com>

--- src/blocks/buttons-child/deprecated.js
+++ src/blocks/buttons-child/deprecated.js
@@ -14,6 +14,7 @@ import newAttributesV2_1_1 from './deprecated/v2.1.1/attributes';
 import newSaveV2_1_1 from './deprecated/v2.1.1/save';
 import saveV2_6_2 from './deprecated/v2.6.2/save';
 import saveV2_6_7 from './deprecated/v2.6.7/save';
+import saveV2_6_9 from './deprecated/v2.6.9/save';
 import attributesV2_6_2 from './attributes';
 
 const borderAttributes = getBorderAttributes( 'btn' );
@@ -256,6 +257,10 @@ const deprecated = [
 	{
 		attributes: attributesV2_6_2,
 		save: saveV2_6_7,
+	},
+	{
+		attributes: attributesV2_6_2,
+		save: saveV2_6_9,
 	}
 ];
 
