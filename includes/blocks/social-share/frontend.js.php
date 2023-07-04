0001ba11281d2a924d96fe15abd410e506b03582
AbhijitNage123 <abhijitn@bsf.io>
2023-07-04 17:14:59 +0530

    resolved precision alignment of 2 spaces

--- includes/blocks/social-share/frontend.js.php
+++ includes/blocks/social-share/frontend.js.php
@@ -20,34 +20,33 @@ if ( has_post_thumbnail() ) {
 ob_start();
 ?>
 var ssLinksParent = document.querySelector( '<?php echo esc_attr( $selector ); ?>' );
-
 ssLinksParent.addEventListener( 'keyup', function ( e ) {
-  var link = e.target.closest( '.uagb-ss__link' );
-  if ( link && e.keyCode === 13 ) {
+var link = e.target.closest( '.uagb-ss__link' );
+if ( link && e.keyCode === 13 ) {
 	handleSocialLinkClick( link );
-  }
+}
 });
 
 ssLinksParent.addEventListener( 'click', function ( e ) {
-  var link = e.target.closest( '.uagb-ss__link' );
-  if ( link ) {
+var link = e.target.closest( '.uagb-ss__link' );
+if ( link ) {
 	handleSocialLinkClick( link );
-  }
+}
 });
 
 function handleSocialLinkClick( link ) {
-  var social_url = link.dataset.href;
-  var target = "";
-  if ( social_url == "mailto:?body=" ) {
+var social_url = link.dataset.href;
+var target = "";
+if ( social_url == "mailto:?body=" ) {
 	target = "_self";
-  }
-  var request_url = "";
-  if ( social_url.indexOf("/pin/create/link/?url=") !== -1 ) {
+}
+var request_url = "";
+if ( social_url.indexOf("/pin/create/link/?url=") !== -1 ) {
 	request_url = social_url + encodeURIComponent( window.location.href ) + "&media=" + '<?php echo esc_url( $thumbnail ); ?>';
-  } else {
+} else {
 	request_url = social_url + encodeURIComponent( window.location.href );
-  }
-  window.open( request_url, target );
+}
+window.open( request_url, target );
 }
 <?php
 return ob_get_clean();
