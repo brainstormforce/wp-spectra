eda3d59b85d792da0a2190f0cbdca907183457db
Zane Vaz <94819993+bsf-zanev@users.noreply.github.com>
2023-08-14 18:20:42 +0530

    SPCTR-3249: Added Document Links to Pro Blocks in the Admin Dashboard (#3405)
    * SPCTR-3215: Added Document Links to Pro Blocks in the Admin Dashboard
    
    * SPCTR-3215: Built Files
    
    * SPCTR-3249: Reverted Login and Register Links
    
    ---------
    
    Co-authored-by: Aditya Jain <adi3890@gmail.com>

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-i18n'), 'version' => 'cb92ea1a5ddce1d18102');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-i18n'), 'version' => 'ec96788d900773fefb6c');
