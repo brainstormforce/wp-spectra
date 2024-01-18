eb890912f334ed5477e737e5fbbac459f50a5324
Zane Vaz <94819993+bsf-zanev@users.noreply.github.com>
2024-01-18 19:53:07 +0530

    SPCTR-3663: Fixed the reload defaults for the Manage AI Features popup. (#3748)
    * SPCTR-3663: Fixed the reload defaults for the Manage AI Features popup.
    
    * SPCTR-3663: Restructured Manage AI Features popup to work with fixed migration settings.
    
    * SPCTR-3663: Reverted admin helper comment.

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-hooks', 'wp-i18n'), 'version' => 'e70a3f7027b52b0d5ea1');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-hooks', 'wp-i18n'), 'version' => '82ea071142c03b1f2d39');
