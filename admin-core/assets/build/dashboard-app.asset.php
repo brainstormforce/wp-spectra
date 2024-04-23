9e53a079a41c80b3d6a4be0bcf16db08408fb0b2
Yash Tingre <86177587+yash-tingre-10@users.noreply.github.com>
2024-04-23 12:51:05 +0530

    updated string (#3945)

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-hooks', 'wp-i18n'), 'version' => '0b8e3be37a3468fcdfc4');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-hooks', 'wp-i18n'), 'version' => '79e0de00d16e6e56ab1e');
