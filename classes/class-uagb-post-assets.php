1342c5821170321d2a3098695c914cf935af9197
Raj Mohan <68589327+rajmohanreddy774@users.noreply.github.com>
2024-04-26 14:52:15 +0530

    SPE-208: Fix - Countdown - uagb_countdown_data duplicate removal (#3932)
    * removed duplicate uagb_countdown_data from page source
    
    * resolved merge conflicts

--- classes/class-uagb-post-assets.php
+++ classes/class-uagb-post-assets.php
@@ -897,14 +897,6 @@ class UAGB_Post_Assets {
 			)
 		);
 
-		wp_localize_script(
-			'uagb-countdown-js',
-			'uagb_countdown_data',
-			array(
-				'site_name_slug' => sanitize_title( get_bloginfo( 'name' ) ),
-			)
-		);
-
 		do_action( 'spectra_localize_pro_block_ajax' );
 
 	}
