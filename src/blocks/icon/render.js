6451d2d22a5723168b3b18328723f00f2cad3a9a
Abhijit Nage <47779685+AbhijitNage123@users.noreply.github.com>
2024-05-10 13:31:14 +0530

    SPCTR-3980 Refinement of Icon block html to resolve margin issue (#3967)
    * Refine Icon html to resolve margin issue
    
    * optimize html code
    
    * Removed margin css for main araper to resolve the margin getting applied at both wraper

--- src/blocks/icon/render.js
+++ src/blocks/icon/render.js
@@ -7,7 +7,24 @@ import { useBlockProps } from '@wordpress/block-editor';
 
 const Render = ( props ) => {
 	const { attributes, setAttributes, deviceType } = props;
-	const { icon, block_id, iconAccessabilityMode, iconAccessabilityDesc } = attributes;
+	const { 
+		icon,
+		block_id, 
+		iconAccessabilityMode, 
+		iconAccessabilityDesc, 
+		iconBottomMargin, 
+		iconLeftMargin, 
+		iconRightMargin, 
+		iconTopMargin, 
+		iconTopTabletMargin,
+		iconRightTabletMargin,
+		iconLeftTabletMargin,
+		iconBottomTabletMargin,
+		iconTopMobileMargin,
+		iconRightMobileMargin,
+		iconLeftMobileMargin,
+		iconBottomMobileMargin,
+	} = attributes;
 
 	const extraProps = {
 		'role': iconAccessabilityMode === 'svg' ? 'graphics-symbol' : 'image',
@@ -15,16 +32,28 @@ const Render = ( props ) => {
 		'aria-hidden': iconAccessabilityMode === 'presentation',
 	  };
 	  
+	const marginVariables = [ iconBottomMargin, iconLeftMargin, iconRightMargin, iconTopMargin, iconTopTabletMargin, iconRightTabletMargin, iconLeftTabletMargin, iconBottomTabletMargin, iconTopMobileMargin, iconRightMobileMargin, iconLeftMobileMargin, iconBottomMobileMargin ];
+	const hasMargin = marginVariables.some( ( margin ) => typeof margin === 'number' );
 	const iconSvg = icon ? icon : 'circle-check';
 	const iconHtml = renderSVG( iconSvg, setAttributes, extraProps );
+	const marginClass = hasMargin ? 'wp-block-uagb-icon--has-margin' : '';
 
 	const blockProps = useBlockProps( {
-		className: `uagb-block-${ block_id } uagb-icon-wrapper uagb-editor-preview-mode-${ deviceType.toLowerCase() }`,
+		className: `uagb-block-${block_id} uagb-icon-wrapper uagb-editor-preview-mode-${deviceType.toLowerCase()} ${marginClass}`,
 	} );
+
+	const renderSvgWrapper = () => (
+		<span className="uagb-svg-wrapper">{iconHtml}</span>
+	);
 
 	return (
 		<div { ...blockProps }>
-			<span className="uagb-svg-wrapper">{ iconHtml }</span>
+			{ hasMargin ? (
+				<div className='uagb-icon-margin-wrapper'>
+					{renderSvgWrapper()}
+				</div>
+				) : renderSvgWrapper()
+			}
 		</div>
 	);
 };
