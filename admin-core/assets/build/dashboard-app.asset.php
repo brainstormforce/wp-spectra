89b6fb2a7ac11dd83f97a97327a691dedc118560
Zane Vaz <zanev@bsf.io>
2024-06-24 11:59:18 +0530

    Resolved conlficts with 2.13.7

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-hooks', 'wp-i18n'), 'version' => '32ea0d67411b9bd53ee9');
-=======
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-hooks', 'wp-i18n'), 'version' => 'd0a8507b890cc19d08a1');
->>>>>>> 04e079f30a909fbc890bb7c6b1f433a34c7cf9e4
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-hooks', 'wp-i18n'), 'version' => '32e21cab1f8c6e90c0e3');
