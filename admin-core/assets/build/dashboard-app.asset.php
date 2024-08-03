042f516689496f96b949a851ed240ba93cc76aa2
Abhijit Nage <47779685+AbhijitNage123@users.noreply.github.com>
2024-08-03 11:53:56 +0530

    Resolved an issue where whats new side bar is hiding the admin top bar (#4119)

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-hooks', 'wp-i18n'), 'version' => 'd3c8754b4b61a30a0105');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-hooks', 'wp-i18n'), 'version' => 'ddae9f46f324e90b30e8');
