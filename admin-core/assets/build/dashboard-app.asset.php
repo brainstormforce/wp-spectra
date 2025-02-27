44f56b6826a8d7c7fe1b41d8b5c8e60f55b2d4c2
Nirzar Bhatt <75498623+nb-0310@users.noreply.github.com>
2025-02-27 12:48:35 +0530

    En: Update the Videos in the Video Tutorials Section in Dashboard (#4395)
    * En: Update the Videos in the Video Tutorials Section in Dashboard
    
    * see more to playlist

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-i18n', 'wp-url'), 'version' => 'a157912bd6fcce284fc2');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-i18n', 'wp-url'), 'version' => '65dce450ed5f7184b6a4');
