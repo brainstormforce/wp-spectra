6b3a525cb9519f0b116ee061397e3872ab24edde
Abhijit Nage <47779685+AbhijitNage123@users.noreply.github.com>
2025-03-07 13:27:06 +0530

    Updated dynamic content description from admin setting (#4406)
    * Updated dynamic content description from admin setting
    
    * Resolved lint issue

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-i18n', 'wp-url'), 'version' => '5425303f53f16259d277');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-i18n', 'wp-url'), 'version' => '9e7b3053e64a777f26b6');
