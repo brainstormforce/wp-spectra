741f8557dd5c69842065f3b3d4eef57e75c01c4e
Shad <53861418+shadofficial@users.noreply.github.com>
2025-04-30 19:11:03 +0530

    SPCTR-4477-Enhance caching mechanism (#4482)
    * fix: improve the api call
    
    * fix: js lint
    
    * fix: js lint
    
    * fix: add build version

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-i18n', 'wp-url'), 'version' => '1f8554e1c177f0bbff24');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-i18n', 'wp-url'), 'version' => 'afffc70cf296899b26dd');
