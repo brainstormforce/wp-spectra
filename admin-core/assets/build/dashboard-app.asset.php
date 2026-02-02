aa6557412ebb3c1637b5267dfbc1fd321d44c9aa
Abhijit Nage <47779685+AbhijitNage123@users.noreply.github.com>
2026-02-02 16:31:34 +0530

    Build (#4903)

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-i18n', 'wp-url'), 'version' => '0dd74a509542a72bc547');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-i18n', 'wp-url'), 'version' => 'e01bf53da588432b7e45');
